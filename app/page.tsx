93fd43e7b29080a816b9fb3694d8adc05faaff3d
VillageR88 <villager88pl@gmail.com>
2023-10-26 00:44:21 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,31 @@ import illustration_mobile from './assets/images/illustration-sign-up-mobile.svg
 import Image from 'next/image';
 
 export default function Home() {
+
   return (
+
     <main className="flex h-screen flex-col items-center justify-center">
       {/* wrapper */}
-      <div className="flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col-reverse md:flex-row text-dark-slate-grey bg-white p-5 rounded-3xl">
         {/* column1 */}
-        <div className="flex flex-col justify-center">
-          <span className="text-base">Stay updated!</span>
-          <span>Join 60,000+ product managers receiving monthly updates on:</span>
-          <div className="flex">
+        <div className="flex flex-col px-20 w-[35em]">
+          <span className="pt-16 text-[3.5rem] font-bold">Stay updated!</span>
+          <span className='pt-6 font-medium'>Join 60,000+ product managers receiving monthly updates on:</span>
+          <div className="flex pt-4 gap-4">
             <Image className="" src={icon_list as string} alt="icon of list" />
             <span>Product discovery and building what matters</span>
           </div>
-          <div className="flex">
+          <div className="flex pt-2 gap-4">
             <Image className="" src={icon_list as string} alt="icon of list" />
             <span>Measuring to ensure updates are a success</span>
           </div>
-          <div className="flex">
+          <div className="flex pt-2 gap-4">
             <Image className="" src={icon_list as string} alt="icon of list" />
             <span>And much more!</span>
           </div>
-          <span>Email address</span>
-          <input type="text" name="emailAddress" id="emailAddress" placeholder="email@company.com" />
+          <span className='text-xs font-semibold pt-8'>Email address</span>
+          <input className='border solid px-6 py-4' type="text" name="emailAddress" id="emailAddress" placeholder="email@company.com" />
           <button>Subscribe to monthly newsletter</button>
-          <div className="font-sans">Ten tekst używa czcionki "Roboto".</div>
         </div>
         {/* column2 */}
         <Image className="hidden md:flex" src={illustration_desktop as string} alt="illustration" />
