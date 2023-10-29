30dbd6ce4b2b5021d88657b6bfd5a07ac6d484d5
VillageR88 <villager88pl@gmail.com>
2023-10-29 15:40:33 +0100

    code errorField update
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,17 @@ import Image from 'next/image';
 import iconArrow from './assets/images/icon-arrow.svg';
 import '@fontsource/poppins';
 import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/400-italic.css';
 import '@fontsource/poppins/800-italic.css';
 
 export default function Home() {
   const [day, setDay] = useState<string>('');
   const [month, setMonth] = useState<string>('');
   const [year, setYear] = useState<string>('');
+  const [error1, setError1] = useState<string | null>('');
+  const [error2, setError2] = useState<string | null>('');
+  const [error3, setError3] = useState<string | null>('');
+
   const [difference, setDifference] = useState<{ years: number; months: number; days: number } | null>(null);
 
   const calculateDifference = () => {
@@ -23,25 +28,37 @@ export default function Home() {
       const yearDifference = yearValue - currentDate.getFullYear();
       const monthDifference = monthValue - 1 - currentDate.getMonth();
       const dayDifference = dayValue - currentDate.getDate();
-
       setDifference({ years: yearDifference, months: monthDifference, days: dayDifference });
+    } else {
+      if (isNaN(dayValue)) setError1('error');
+      if (isNaN(monthValue)) setError2('error');
+      if (isNaN(yearValue)) setError3('error');
     }
   };
 
-  function renderInput(label: string, value: string, setValue: (value: string) => void, placeholder: string) {
+  function renderInput(
+    label: string,
+    valueN: string,
+    error: string,
+    setValue: (parameter: string) => void,
+    setError: (parameter: string | null) => void,
+    placeholder: string,
+  ) {
     return (
       <div className="flex flex-col gap-2">
         <span className="text-sm font-bold tracking-[0.2em] text-smokeyGrey">{label}</span>
         <input
           className="h-14 w-24 rounded-lg border border-solid px-6 font-bold placeholder-grey [appearance:textfield] hover:cursor-pointer md:h-[2.4em] md:w-40 md:text-3xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
           type="number"
-          name={`${label.toLowerCase()}Input`}
+          name={`${label}`}
           placeholder={placeholder}
-          value={value}
+          value={valueN}
           onChange={(e) => {
             setValue(e.target.value);
+            setError(e.target.textContent);
           }}
         />
+        <span className="text-sm font-light italic text-lightRed">{error}</span>
       </div>
     );
   }
@@ -53,10 +70,10 @@ export default function Home() {
   const MONTH = 'MONTH';
   const DAY = 'DAY';
 
-  function renderDifference(label: string, value: number | undefined) {
+  function renderDifference(label: string, valueN: number | undefined) {
     return (
       <div className="flex gap-3">
-        <span className={`text-${'#864CFF'}`}>{value ?? '- -'}</span>
+        <span className={`text-${'#864CFF'}`}>{valueN ?? '- -'}</span>
         <span className="text-black">{label}</span>
       </div>
     );
@@ -66,9 +83,9 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center font-['Poppins']">
       <div className="flex max-w-full flex-col gap-6 rounded-t-3xl rounded-bl-3xl rounded-br-[12em] bg-white p-6 md:gap-1 md:p-14">
         <div className="flex gap-5 md:gap-8">
-          {renderInput(DAY, day, setDay, 'DD')}
-          {renderInput(MONTH, month, setMonth, 'MM')}
-          {renderInput(YEAR, year, setYear, 'YYYY')}
+          {renderInput(DAY, day, error1 ?? '', setDay, setError1, 'DD')}
+          {renderInput(MONTH, month, error2 ?? '', setMonth, setError2, 'MM')}
+          {renderInput(YEAR, year, error3 ?? '', setYear, setError3, 'YYYY')}
         </div>
         <div className="flex justify-between">
           <hr className="flex w-full self-center text-[#864CFF] md:w-[40em]"></hr>
