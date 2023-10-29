c00e795db911f5a494202936560fac67c716c29c
VillageR88 <villager88pl@gmail.com>
2023-10-29 19:48:11 +0100

    codeReview2
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
   const [error1, setError1] = useState<string | null>('');
   const [error2, setError2] = useState<string | null>('');
   const [error3, setError3] = useState<string | null>('');
+  const [hasErrors1, setToggle1] = useState<boolean>(false);
+  const [hasErrors2, setToggle2] = useState<boolean>(false);
+  const [hasErrors3, setToggle3] = useState<boolean>(false);
 
   const [difference, setDifference] = useState<{ years: number; months: number; days: number } | null>(null);
 
@@ -32,40 +35,43 @@ export default function Home() {
     } else {
       if (isNaN(dayValue)) {
         setError1(errorField);
-        document.getElementsByClassName('input')[0].style.borderColor = ' red';
+        setToggle1(true);
       }
       if (isNaN(monthValue)) {
         setError2(errorField);
-        document.getElementsByClassName('input')[1].style.borderColor = ' red';
+        setToggle2(true);
       }
       if (isNaN(yearValue)) {
         setError3(errorField);
-        document.getElementsByClassName('input')[2].style.borderColor = ' red';
+        setToggle3(true);
       }
     }
   };
 
   function renderInput(
+    placeholder: string,
     label: string,
-    valueN: string,
     error: string,
+    hasErrors: boolean,
     setValue: (parameter: string) => void,
     setError: (parameter: string | null) => void,
-    placeholder: string,
+    setHasErrors: (parameter: boolean) => void,
   ) {
     return (
       <div className="flex flex-col gap-2">
-        <span className="text-sm font-bold tracking-[0.2em] text-smokeyGrey">{label}</span>
+        <label className={`${hasErrors ? 'text-red-500' : ''} text-sm font-bold tracking-[0.2em] text-smokeyGrey`}>
+          {label}
+        </label>
         <input
-          id="inputs"
-          className="input h-14 w-24 rounded-lg border border-solid px-6 font-bold placeholder-grey [appearance:textfield] hover:cursor-pointer md:h-[2.4em] md:w-40 md:text-3xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          className={`${
+            hasErrors ? 'border-red-500' : ''
+          } input h-14 w-24 rounded-lg border border-solid px-6 font-bold placeholder-grey [appearance:textfield] hover:cursor-pointer md:h-[2.4em] md:w-40 md:text-3xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none`}
           type="number"
-          name={`${label}`}
           placeholder={placeholder}
-          value={valueN}
           onChange={(e) => {
             setValue(e.target.value);
             setError(e.target.textContent);
+            setHasErrors(false);
           }}
         />
         <span className="text-sm font-light italic text-lightRed">{error}</span>
@@ -76,6 +82,9 @@ export default function Home() {
   const years = 'years';
   const months = 'months';
   const days = 'days';
+  const DD = 'DD';
+  const MM = 'MM';
+  const YYYY = 'YYYY';
   const YEAR = 'YEAR';
   const MONTH = 'MONTH';
   const DAY = 'DAY';
@@ -93,9 +102,9 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center font-['Poppins']">
       <div className="flex max-w-full flex-col gap-6 rounded-t-3xl rounded-bl-3xl rounded-br-[12em] bg-white p-6 md:gap-1 md:p-14">
         <div className="flex gap-5 md:gap-8">
-          {renderInput(DAY, day, error1 ?? '', setDay, setError1, 'DD')}
-          {renderInput(MONTH, month, error2 ?? '', setMonth, setError2, 'MM')}
-          {renderInput(YEAR, year, error3 ?? '', setYear, setError3, 'YYYY')}
+          {renderInput(DD, DAY, error1 ?? '', hasErrors1, setDay, setError1, setToggle1)}
+          {renderInput(MM, MONTH, error2 ?? '', hasErrors2, setMonth, setError2, setToggle2)}
+          {renderInput(YYYY, YEAR, error3 ?? '', hasErrors3, setYear, setError3, setToggle3)}
         </div>
         <div className="flex justify-between">
           <hr className="flex w-full self-center text-[#864CFF] md:w-[40em]"></hr>
