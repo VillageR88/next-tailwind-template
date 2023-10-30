deb07d8e6830718f37f31dbf749a3bfdcae13af9
VillageR88 <villager88pl@gmail.com>
2023-10-30 17:55:04 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
-    </main>
+    <div>
+      <nav>
+        <Image src="\app\assets\images\logo.svg"></Image>
+      </nav>
+      <main className="flex h-screen flex-col items-center justify-center">
+        <p className="text-lg">Hello</p>
+        <p className="font-mono">World 🌍</p>
+      </main>
+    </div>
   );
 }
