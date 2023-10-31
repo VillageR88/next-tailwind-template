0112b8f38884033604b4b1a8b12dd3ea2038462b
VillageR88 <villager88pl@gmail.com>
2023-10-31 23:37:48 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/plus-jakarta-sans';
+import '@fontsource/plus-jakarta-sans/500.css';
+import '@fontsource/plus-jakarta-sans/800.css';
+
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
