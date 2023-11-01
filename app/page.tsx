838ef6b4dde42aeb8710894dfda5b66e5c7442f3
VillageR88 <villager88pl@gmail.com>
2023-11-01 22:33:22 +0100

    top and first comment
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
 import '@fontsource/plus-jakarta-sans';
 import '@fontsource/plus-jakarta-sans/500.css';
 import '@fontsource/plus-jakarta-sans/800.css';
+import Image from 'next/image';
+import markWebber from './assets/images/avatar-mark-webber.webp';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="font-plusJakartaSans flex h-screen max-w-full flex-col items-center py-[4em]">
+      <div className="flex h-full max-w-full flex-col items-center rounded-xl bg-white px-8 ">
+        <div className="flex w-[42em] max-w-full justify-between py-7">
+          <span className="text-[1.5rem] font-bold text-veryDarkBlue">
+            Notifications{' '}
+            <span className="ml-1.5 rounded-md bg-blue px-[0.7em] py-0.5 align-[3px] text-[1rem] font-semibold text-white">
+              3
+            </span>
+          </span>
+          <button className="font-medium text-darkGrayishBlue">Mark all as read</button>
+        </div>
+        <div className="flex w-[42em] max-w-full gap-2.5 rounded-xl bg-veryLightGrayishBlue px-5 py-4">
+          <Image className="h-11 w-auto" src={markWebber} alt="Image of Mark Webber" />
+          <div className="ml-2 flex flex-col">
+            <div className="flex items-center">
+              <div className="flex">
+                <span className="font-semibold">
+                  Mark Webber
+                  <span className="ml-1.5 font-medium text-darkGrayishBlue">
+                    reacted to your recent post
+                    <span className="ml-1.5 font-semibold text-darkGrayishBlue">My first tournament today!</span>
+                  </span>
+                </span>
+              </div>
+              <div className="ml-1.5 flex h-1 w-1 rounded-full bg-red p-1"></div>
+            </div>
+            <span className="font-medium text-grayishBlue">1m ago</span>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        plusJakartaSans: ['Plus Jakarta Sans'],
       },
       colors: {
         red: 'hsl(1, 90%, 64%)',
