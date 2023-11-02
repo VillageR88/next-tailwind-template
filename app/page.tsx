b413990e2bd0b6f8f49f8f86487f5893876aa70f
VillageR88 <villager88pl@gmail.com>
2023-11-02 21:34:26 +0100

    all feauters added
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,13 @@ interface Message {
   text4: string;
   pictureRated: string;
 }
-const startTime = new Date().getTime();
 
 function MyComponent() {
   const [messages, setMessages] = useState<Message[] | null>(null);
   const [initialCount, setInitialCount] = useState<number | null>(null);
   const [selectedMessageIndex, setSelectedMessageIndex] = useState<number | null>(null);
+  const [showFirstMessage, setShowFirstMessage] = useState(false); // Nowy stan
   let dummyCount = initialCount ?? null;
-
   const dataJson = './data.json';
 
   useState(() => {
@@ -34,21 +33,22 @@ function MyComponent() {
       .then((jsonData: { messages: Message[] }) => {
         setMessages(jsonData.messages);
         const count = jsonData.messages.filter((message) => message.boolean1).length;
-        setInitialCount(count);
+        setInitialCount(count - 1);
         console.log('test');
       })
       .catch((error) => {
         console.error('Error loading JSON: ', error);
       });
   });
+
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const currentTime = new Date().getTime();
-      console.log(currentTime - startTime);
-    }, 1000);
+    const timerId = setTimeout(() => {
+      setShowFirstMessage(true);
+      setInitialCount((parameter) => (parameter !== null ? parameter + 1 : 0));
+    }, 2000);
 
     return () => {
-      clearInterval(intervalId);
+      clearTimeout(timerId);
     };
   }, []);
 
@@ -62,7 +62,7 @@ function MyComponent() {
         ? (days || null)?.toString().concat(days === 1 ? ' day ago' : ' days ago')
         : (Math.floor(days / 7) || null)?.toString().concat(Math.floor(days / 7) === 1 ? ' week ago' : ' weeks ago'),
       hours < 24 ? (hours || null)?.toString().concat('h ago') : null,
-      hours < 1 && minutes < 60 ? (minutes || null)?.toString().concat('m ago') : null,
+      minutes < 1 ? 'now' : hours < 1 && minutes < 60 ? (minutes || null)?.toString().concat('m ago') : null,
     ];
   }
 
@@ -80,7 +80,7 @@ function MyComponent() {
           <button
             onClick={() => {
               setInitialCount((dummyCount = 0));
-              messages.map((message) => (message.boolean1 = false));
+              messages.map((message, index) => (!showFirstMessage ? index > 0 : true) && (message.boolean1 = false));
             }}
             className="font-[600] text-darkGrayishBlue hover:text-blue"
           >
@@ -88,83 +88,90 @@ function MyComponent() {
           </button>
         </div>
         {/*comments section*/}
-        {messages.map((message, index) => (
-          <div
-            onClick={() => {
-              if (message.boolean1) {
-                setInitialCount(dummyCount ? (dummyCount -= 1) : 0);
-                setSelectedMessageIndex(index);
-              }
-            }}
-            key={index}
-            className={`${message.boolean1 ? 'cursor-pointer bg-lightGrayishBlue1 md:bg-veryLightGrayishBlue' : null}
+        {messages.map(
+          (message, index) =>
+            (index > 0 || showFirstMessage) && (
+              <div
+                onClick={() => {
+                  if (message.boolean1) {
+                    setInitialCount(dummyCount ? (dummyCount -= 1) : 0);
+                    setSelectedMessageIndex(index);
+                  }
+                }}
+                key={index}
+                className={`${
+                  message.boolean1 ? 'cursor-pointer bg-lightGrayishBlue1 md:bg-veryLightGrayishBlue' : null
+                }
               ${selectedMessageIndex === index ? (message.boolean1 = false) : null}
             } flex gap-2.5 rounded-[0.5em] px-2 py-4 md:w-[42em] md:px-5`}
-          >
-            <Image
-              className="h-11 w-auto"
-              src={`./images/${message.person}`}
-              alt={`Image of ${message.person}`}
-              width={45}
-              height={10}
-            />
+              >
+                <Image
+                  className="h-11 w-auto"
+                  src={`./images/${message.person}`}
+                  alt={`Image of ${message.person}`}
+                  width={45}
+                  height={10}
+                />
 
-            <div className="ml-1 flex w-full flex-col md:ml-2">
-              <div className="flex justify-between">
-                <div className="flex flex-col">
-                  <div className="flex items-center">
-                    <div className="w-full flex-grow">
-                      <span className="font-semibold hover:cursor-pointer hover:text-blue">{message.text1}</span>
+                <div className="ml-1 flex w-full flex-col md:ml-2">
+                  <div className="flex justify-between">
+                    <div className="flex flex-col">
+                      <div className="flex items-center">
+                        <div className="w-full flex-grow">
+                          <span className="font-semibold hover:cursor-pointer hover:text-blue">{message.text1}</span>
+                          <span
+                            className={`ml-1.5 font-medium text-darkGrayishBlue ${
+                              message.boolean1 ? 'hover:cursor-pointer' : 'hover:cursor-default'
+                            }`}
+                          >
+                            {message.text2}
+                          </span>
+                          <span
+                            className={`${message.alternateText1 && 'text-blue'} ${
+                              message.text3 &&
+                              'ml-1.5 font-semibold text-darkGrayishBlue hover:cursor-pointer hover:text-blue'
+                            }`}
+                          >
+                            {message.text3}
+                          </span>
+                          <span
+                            className={`${
+                              message.boolean1 && 'ml-1.5 rounded-full bg-red px-1 align-middle text-[6.5px]'
+                            }`}
+                          ></span>
+                        </div>
+                      </div>
                       <span
-                        className={`ml-1.5 font-medium text-darkGrayishBlue ${
+                        className={`${
                           message.boolean1 ? 'hover:cursor-pointer' : 'hover:cursor-default'
-                        }`}
+                        } 'font-medium ' text-grayishBlue`}
                       >
-                        {message.text2}
+                        {timeDiff(message.timestamp1)}
                       </span>
-                      <span
-                        className={`${message.alternateText1 && 'text-blue'} ${
-                          message.text3 &&
-                          'ml-1.5 font-semibold text-darkGrayishBlue hover:cursor-pointer hover:text-blue'
-                        }`}
-                      >
-                        {message.text3}
-                      </span>
-                      <span
-                        className={`${message.boolean1 && 'ml-1.5 rounded-full bg-red px-1 align-middle text-[6.5px]'}`}
-                      ></span>
                     </div>
+                    {message.pictureRated && (
+                      <Image
+                        className="ml-6 flex h-14 w-auto cursor-pointer rounded-xl border-4 border-solid border-transparent hover:border-lightGrayishBlue1"
+                        src={`./images/${message.pictureRated}`}
+                        alt={`Image of ${message.pictureRated}`}
+                        width={45}
+                        height={10}
+                      />
+                    )}
                   </div>
+
                   <span
                     className={`${
-                      message.boolean1 ? 'hover:cursor-pointer' : 'hover:cursor-default'
-                    } 'font-medium ' text-grayishBlue`}
+                      message.text4 &&
+                      'mb-3 mt-[0.6em] rounded-md border border-solid px-5 pb-4 pt-4 font-medium leading-5 text-darkGrayishBlue'
+                    } hover:cursor-pointer hover:bg-lightGrayishBlue1`}
                   >
-                    {timeDiff(message.timestamp1)}
+                    {message.text4}
                   </span>
                 </div>
-                {message.pictureRated && (
-                  <Image
-                    className="ml-6 flex h-14 w-auto cursor-pointer rounded-xl border-4 border-solid border-transparent hover:border-lightGrayishBlue1"
-                    src={`./images/${message.pictureRated}`}
-                    alt={`Image of ${message.pictureRated}`}
-                    width={45}
-                    height={10}
-                  />
-                )}
               </div>
-
-              <span
-                className={`${
-                  message.text4 &&
-                  'mb-3 mt-[0.6em] rounded-md border border-solid px-5 pb-4 pt-4 font-medium leading-5 text-darkGrayishBlue'
-                } hover:cursor-pointer hover:bg-lightGrayishBlue1`}
-              >
-                {message.text4}
-              </span>
-            </div>
-          </div>
-        ))}
+            ),
+        )}
       </div>
     );
   } else {
