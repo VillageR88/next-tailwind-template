856e8db471c401ba7a8cbee3f3664fb3db83c397
VillageR88 <villager88pl@gmail.com>
2023-11-03 00:16:44 +0100

    time fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -54,16 +54,24 @@ function MyComponent() {
 
   const prepDate = new Date('2023-11-01T10:00:00Z').getTime();
   function timeDiff(parameter: string | number | Date) {
-    const days = Math.floor((prepDate - new Date(parameter).getTime()) / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((prepDate - new Date(parameter).getTime()) / 3600000);
-    const minutes = ((prepDate - new Date(parameter).getTime()) % (1000 * 60 * 60)) / (1000 * 60);
-    return [
-      days < 7
-        ? (days || null)?.toString().concat(days === 1 ? ' day ago' : ' days ago')
-        : (Math.floor(days / 7) || null)?.toString().concat(Math.floor(days / 7) === 1 ? ' week ago' : ' weeks ago'),
-      hours < 24 ? (hours || null)?.toString().concat('h ago') : null,
-      minutes < 1 ? 'now' : hours < 1 && minutes < 60 ? (minutes || null)?.toString().concat('m ago') : null,
-    ];
+    const timeDifference = prepDate - new Date(parameter).getTime();
+    const seconds = Math.floor(timeDifference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if (days >= 7) {
+      const weeks = Math.floor(days / 7);
+      return `${weeks} week${weeks === 1 ? '' : 's'} ago`;
+    } else if (days >= 1) {
+      return `${days} day${days === 1 ? '' : 's'} ago`;
+    } else if (hours >= 1) {
+      return `${hours}h ago`;
+    } else if (minutes >= 1) {
+      return `${minutes}m ago`;
+    } else {
+      return 'now';
+    }
   }
 
   if (messages) {
