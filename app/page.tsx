3055fca2359861a90fb69aea9c59f3dc6ea5edd9
VillageR88 <villager88pl@gmail.com>
2023-11-02 09:00:30 +0100

    updated db and functions
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,88 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+
 import '@fontsource/plus-jakarta-sans';
 import '@fontsource/plus-jakarta-sans/500.css';
 import '@fontsource/plus-jakarta-sans/800.css';
 import Image from 'next/image';
-import markWebber from './assets/images/avatar-mark-webber.webp';
+
+interface Message {
+  person: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  boolean1: boolean;
+  timestamp1: string;
+  text4: string;
+}
+
+function MyComponent() {
+  const [messages, setMessages] = useState<Message[] | null>(null);
+  const dataJson = './data.json';
+
+  useEffect(() => {
+    fetch(dataJson)
+      .then((response) => response.json())
+      .then((jsonData: { messages: Message[] }) => {
+        setMessages(jsonData.messages);
+      })
+      .catch((error) => {
+        console.error('Error loading JSON: ', error);
+      });
+  }, []);
+
+  const prepDate = new Date('2023-11-01T10:00:00Z').getTime();
+  function timeDiff(parameter: string | number | Date) {
+    const days = Math.floor((prepDate - new Date(parameter).getTime()) / (1000 * 60 * 60 * 24));
+    const minutes = ((prepDate - new Date(parameter).getTime()) % (1000 * 60 * 60)) / (1000 * 60);
+    return [
+      (days || null)?.toString().concat(days === 1 ? ' day ago' : ' days ago'),
+      (minutes || null)?.toString().concat(' m ago'),
+    ];
+  }
+
+  if (messages) {
+    return (
+      <div className="flex flex-col gap-4">
+        {messages.map((message, index) => (
+          <div key={index} className="flex w-[42em] max-w-full gap-2.5 rounded-xl bg-veryLightGrayishBlue px-5 py-4">
+            <Image
+              className="h-[2.7em] w-auto"
+              src={`./images/${message.person}`}
+              alt={`Image of ${message.person}`}
+              width={45}
+              height={10}
+            />
+            <div className="ml-2 flex flex-col">
+              <div className="flex items-center">
+                <div className="flex">
+                  <span className="font-semibold">
+                    {message.text1}
+                    <span className="ml-1.5 font-medium text-darkGrayishBlue">
+                      {message.text2}
+                      <span className="ml-1.5 font-semibold text-darkGrayishBlue">{message.text3}</span>
+                    </span>
+                  </span>
+                </div>
+                <div className="ml-1.5 flex h-1 w-1 rounded-full bg-red p-1"></div>
+              </div>
+              <span className="font-medium text-grayishBlue">{timeDiff(message.timestamp1)}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <p>Loading database...</p>;
+  }
+}
 
 export default function Home() {
   return (
-    <main className="font-plusJakartaSans flex h-screen max-w-full flex-col items-center py-[4em]">
+    <main className="flex h-screen max-w-full flex-col items-center py-[4em] font-plusJakartaSans">
+      {/*wrapper*/}
       <div className="flex h-full max-w-full flex-col items-center rounded-xl bg-white px-8 ">
+        {/*top*/}
         <div className="flex w-[42em] max-w-full justify-between py-7">
           <span className="text-[1.5rem] font-bold text-veryDarkBlue">
             Notifications{' '}
@@ -17,23 +92,9 @@ export default function Home() {
           </span>
           <button className="font-medium text-darkGrayishBlue">Mark all as read</button>
         </div>
-        <div className="flex w-[42em] max-w-full gap-2.5 rounded-xl bg-veryLightGrayishBlue px-5 py-4">
-          <Image className="h-11 w-auto" src={markWebber} alt="Image of Mark Webber" />
-          <div className="ml-2 flex flex-col">
-            <div className="flex items-center">
-              <div className="flex">
-                <span className="font-semibold">
-                  Mark Webber
-                  <span className="ml-1.5 font-medium text-darkGrayishBlue">
-                    reacted to your recent post
-                    <span className="ml-1.5 font-semibold text-darkGrayishBlue">My first tournament today!</span>
-                  </span>
-                </span>
-              </div>
-              <div className="ml-1.5 flex h-1 w-1 rounded-full bg-red p-1"></div>
-            </div>
-            <span className="font-medium text-grayishBlue">1m ago</span>
-          </div>
+        {/*comments section*/}
+        <div>
+          <MyComponent></MyComponent>
         </div>
       </div>
     </main>
