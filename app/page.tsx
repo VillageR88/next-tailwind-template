3dca17fb75cc03f2facba9ab0e903f193a131cf1
VillageR88 <villager88pl@gmail.com>
2023-11-02 14:20:00 +0100

    text4 update
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -52,9 +52,9 @@ function MyComponent() {
 
   if (messages) {
     return (
-      <div className="flex flex-col gap-2 pb-60">
+      <div className="flex flex-col gap-2 pb-6 pt-8">
         {/*top*/}
-        <div className="flex w-[42em] max-w-full justify-between py-7">
+        <div className="flex w-[42em] justify-between pb-4">
           <span className="text-[1.5rem] font-bold text-veryDarkBlue">
             Notifications{' '}
             <span className="ml-1.5 rounded-md bg-blue px-[0.7em] py-0.5 align-[3px] text-[1rem] font-semibold text-white">
@@ -74,7 +74,7 @@ function MyComponent() {
             key={index}
             className={`${message.boolean1 ? 'cursor-pointer bg-veryLightGrayishBlue' : null}
               ${selectedMessageIndex === index ? (message.boolean1 = false) : null}
-            } flex w-[42em] max-w-full gap-2.5 rounded-xl px-5 py-4`}
+            } flex w-[42em] gap-2.5 rounded-xl px-5 py-4`}
           >
             <Image
               className="h-[2.7em] w-auto"
@@ -97,21 +97,30 @@ function MyComponent() {
                 <div className={`${message.boolean1 ? 'ml-1.5 flex h-1 w-1 rounded-full bg-red p-1' : null}`}></div>
               </div>
               <span className="font-medium text-grayishBlue">{timeDiff(message.timestamp1)}</span>
+              <span
+                className={`${
+                  message.text4 != ''
+                    ? 'mt-[0.6em] rounded-md border border-solid px-4 py-3.5 font-medium leading-5 text-darkGrayishBlue'
+                    : null
+                }`}
+              >
+                {message.text4}
+              </span>
             </div>
           </div>
         ))}
       </div>
     );
   } else {
-    return <p>Loading database...</p>;
+    return <p className="static p-2 text-[1.5rem] font-bold text-veryDarkBlue">Loading database...</p>;
   }
 }
 
 export default function Home() {
   return (
-    <main className="flex h-screen max-w-full flex-col items-center py-[4em] font-plusJakartaSans">
+    <main className="flex min-h-screen max-w-full flex-col items-center py-[4em] font-plusJakartaSans">
       {/*wrapper*/}
-      <div className="flex h-full max-w-full flex-col items-center rounded-xl bg-white px-8 ">
+      <div className="flex flex-col items-center rounded-xl bg-white px-8">
         <div>
           <MyComponent></MyComponent>
         </div>
