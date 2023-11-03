d34ed7e6f1869838c0eb42a86f154ff965bfced8
VillageR88 <villager88pl@gmail.com>
2023-11-03 16:00:38 +0100

    initial commit initial progress
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="flex h-full w-full flex-col pb-[1.7em] pt-[1.72em] md:flex-row">
+        <Image
+          className="flex h-2/6 w-full md:h-full md:w-[40em]"
+          src="./images/bg-main-desktop.png"
+          alt="colorful background"
+          width={10}
+          height={10}
+        />
+        <div className="fixed flex h-full w-[30em] flex-col justify-center gap-10 self-center pb-[1.7em] pt-[1.72em]">
+          <div className="ml-[10.3em] h-[15em] w-[28em] bg-red-400"></div>
+          <div className="ml-[10.3em] h-[15em] w-[28em] bg-red-400"></div>
+        </div>
+        <div className="flex h-4/6 w-full bg-[#FFFFFF] md:h-full"></div>
+      </div>
     </main>
   );
 }
