f89886130d0cec881f2cc16f1653a4426119efd1
VillageR88 <villager88pl@gmail.com>
2023-11-04 15:50:00 +0100

    more layout fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="main font-spaceGrotesk flex min-h-screen max-w-full md:pb-[1.7em] md:pt-[1.72em]">
+    <main className="main flex min-h-screen max-w-full font-spaceGrotesk md:pb-[1.7em] md:pt-[1.72em]">
       {/* main wrapper */}
-      <div className="flex max-h-full w-full flex-col gap-4 bg-white md:flex-row">
+      <div className="flex max-h-full w-full flex-col gap-4 bg-white md:flex-row md:pr-8">
         {/* first column */}
-        <div className="bg-bgMainDesktop flex justify-center bg-cover md:w-1/2 md:bg-contain md:bg-no-repeat md:pl-1">
+        <div className="flex w-full justify-center bg-bgMainDesktop bg-cover md:w-1/2 md:bg-contain md:bg-no-repeat md:pl-1">
           {/* first column wrapper */}
-          <div className="flex h-full max-w-full flex-col justify-center gap-[2.3em]">
+          <div className="flex h-full flex-col items-center justify-center gap-[2.3em] md:max-w-full">
             {/* card1 */}
             <div
-              className="bg-bgCardFront flex h-[15em] max-w-full flex-col justify-around rounded-[0.8em] bg-no-repeat md:w-[27.9em] 
-              xl:ml-[10em]"
+              className="flex h-[15.5em] w-full max-w-full  flex-col justify-around rounded-[0.8em] bg-bgCardFront bg-no-repeat md:w-[27.9em] 
+              xl:ml-[3.9em]"
             >
               <Image
                 className=" ml-8 flex h-auto w-[5em]"
@@ -25,22 +25,25 @@ export default function Home() {
                 height={10}
               />
               <div className="space-y-[1.5em]">
-                <span className="flex w-full justify-center text-[1.75rem] tracking-[0.12em] text-white">
-                  0000 0000 0000 0000
-                </span>
-                <div className="flex w-full justify-between px-[1em] text-[0.9rem] tracking-[0.12em] text-white md:ml-[-0.5em] md:px-[3em]">
+                <div className="flex w-full justify-center gap-[2.5%] px-[1em] text-[1.75rem] text-white md:tracking-[0.12em]">
+                  <span>0000</span>
+                  <span>0000</span>
+                  <span>0000</span>
+                  <span>0000</span>
+                </div>
+                <div className="flex w-full justify-between px-[1.5em] text-[0.9rem] tracking-[0.12em] text-white md:px-[2.5em]">
                   <span>JANE APPLESEED</span>
                   <span>00/00</span>
                 </div>
               </div>
             </div>
             {/* card2 */}
-            <div className="bg-bgCardBack flex h-[15em] max-w-full rounded-[0.8em] md:bg-contain bg-no-repeat md:w-[27.9em] xl:ml-[15em]"></div>
+            <div className="flex h-[15.5em] w-full max-w-full rounded-[0.8em] bg-bgCardBack bg-cover bg-no-repeat md:w-[27.9em] md:bg-contain xl:ml-[15em]"></div>
           </div>
         </div>
         {/* second column */}
-        <div className="max-h-auto mr-[4.7em flex w-full items-center justify-center bg-[#FFFFFF] md:w-1/2">
-          <div className="flex w-[23.8em]">
+        <div className="max-h-auto flex w-full items-center justify-center bg-[#FFFFFF] md:mr-[4em] md:w-1/2">
+          <div className="flex">
             <form className="max-h-auto flex max-w-full flex-col gap-[1.4em]">
               <div>
                 <label className="mb-2 block text-[0.8rem] font-bold tracking-[0.1em] text-gray-700" htmlFor="username">
@@ -62,7 +65,7 @@ export default function Home() {
                   placeholder="e.g. 1234 5678 9123 0000"
                 />
               </div>
-              <div className="flex justify-between">
+              <div className="flex justify-between gap-5">
                 <div>
                   <label
                     className="mb-2 block w-full text-[0.8rem] font-bold tracking-[0.1em] text-gray-700"
@@ -79,7 +82,7 @@ export default function Home() {
                     <input
                       type="text"
                       className="w-[4.5em] border border-solid px-4 py-2 text-[1.1rem]"
-                      placeholder="MM"
+                      placeholder="YY"
                     />
                   </div>
                 </div>
@@ -94,7 +97,7 @@ export default function Home() {
                     <input
                       type="text"
                       className="w-full border border-solid px-4 py-2 text-[1.1rem]"
-                      placeholder="MM"
+                      placeholder="e.g. 123"
                     />
                   </div>
                 </div>
