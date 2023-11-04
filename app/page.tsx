15f48a1ddd0d8d58d596992f9da9e94983973c5c
VillageR88 <villager88pl@gmail.com>
2023-11-04 02:01:18 +0100

    major layout fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,43 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="main font-spaceGrotesk flex h-screen w-full pb-[1.7em] pt-[1.72em]">
+    <main className="main font-spaceGrotesk flex min-h-screen max-w-full pb-[1.7em] pt-[1.72em]">
       {/* main wrapper */}
-      <div className="flex h-full w-full flex-col bg-white md:flex-row">
+      <div className="flex max-h-full w-full flex-col bg-white md:flex-row">
         {/* first column */}
-        <div className="bg-bgMainDesktop flex h-auto w-full flex-col justify-center bg-cover md:w-[55%]	">
+        <div className="bg-bgMainDesktop flex w-full justify-center bg-cover md:bg-contain md:bg-no-repeat ">
           {/* first column wrapper */}
-          {/* card1 */}
-          <div
-            className="bg-bgCardFront flex h-[15em] w-full flex-col rounded-[0.8em] md:ml-[0.1em]
-            md:w-[27.9em]"
-          >
-            <Image
-              className=" ml-8 mt-7 flex h-auto w-[5em]"
-              src={'./images/card-logo.svg' as string}
-              alt="card logo"
-              width={10}
-              height={10}
-            />
-            <span className="flex w-full justify-center text-[1.75rem] tracking-[0.12em] text-white">
-              0000 0000 0000 0000
-            </span>
-            <div className="flex w-full justify-between px-[1em] text-[0.9rem] tracking-[0.12em] text-white md:ml-[-0.5em] md:px-[3em]">
-              <span>JANE APPLESEED</span>
-              <span>00/00</span>
+          <div className="flex h-auto max-w-full flex-col justify-center gap-[2.3em]">
+            {/* card1 */}
+            <div
+              className="bg-bgCardFront flex h-[15em] max-w-full flex-col justify-around rounded-[0.8em] md:w-[27.9em] 
+              xl:ml-[10em]"
+            >
+              <Image
+                className=" ml-8 flex h-auto w-[5em]"
+                src={'./images/card-logo.svg' as string}
+                alt="card logo"
+                width={10}
+                height={10}
+              />
+              <div className="space-y-[1.5em]">
+                <span className="flex w-full justify-center text-[1.75rem] tracking-[0.12em] text-white">
+                  0000 0000 0000 0000
+                </span>
+                <div className="flex w-full justify-between px-[1em] text-[0.9rem] tracking-[0.12em] text-white md:ml-[-0.5em] md:px-[3em]">
+                  <span>JANE APPLESEED</span>
+                  <span>00/00</span>
+                </div>
+              </div>
             </div>
+            {/* card2 */}
+            <div className="bg-bgCardBack flex h-[15em] max-w-full rounded-[0.8em] md:w-[27.9em] xl:ml-[15em]"></div>
           </div>
-          {/* card2 */}
-          <div className="bg-bgCardBack ml-0 flex h-[15em] rounded-[0.8em] sm:ml-0 md:ml-[1.5em] md:w-[27.9em] md:w-[30em]"></div>
         </div>
         {/* second column */}
-        <div className="mr-[4.7em] flex h-auto w-full items-center justify-center bg-[#FFFFFF]">
+        <div className="mr-[4.7em] flex max-h-auto w-full items-center justify-center bg-[#FFFFFF]">
           <div className="flex w-[23.8em]">
-            <form className="flex h-full w-full flex-col gap-[1.4em]">
+            <form className="flex max-h-auto max-w-full flex-col gap-[1.4em]">
               <div>
                 <label className="mb-2 block text-[0.8rem] font-bold tracking-[0.1em] text-gray-700" htmlFor="username">
                   CARDHOLDER NAME
