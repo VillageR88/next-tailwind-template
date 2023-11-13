3d9186e02ccbcfc1f43b7fabfc33e3db5c019c54
VillageR88 <villager88pl@gmail.com>
2023-11-13 17:09:10 +0100

    initial ChartComponent
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
+import '@fontsource/dm-sans';
+import '@fontsource/dm-sans/400.css';
+import '@fontsource/dm-sans/500.css';
+import '@fontsource/dm-sans/700.css';
+import Image from 'next/image';
+
+const ChartComponent = () => {
+  return (
+    <div className="flex flex-col">
+      <span>test</span>
+      <span>test</span>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="font-dmSans flex h-screen flex-col items-center justify-center">
+      {/* main wrapper */}
+      <div className="md:f w-screen px-4 md:w-auto md:px-0">
+        {/* first wrapper */}
+        <div className="bg-softRed flex justify-between md:w-[30em]">
+          {/* left */}
+          <div className="flex flex-col text-white">
+            <span>My balance</span>
+            <span>$921.48</span>
+          </div>
+          <Image className="h-auto w-[5em]" src="/images/logo.svg" alt="Logo" height={10} width={10}></Image>
+        </div>
+        {/* second wrapper */}
+        <div>
+          <span>Spending - Last 7 days</span>
+          <div>
+            <ChartComponent />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,15 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        dmSans: 'DM Sans',
       },
       colors: {
+        softRed: 'hsl(10, 79%, 65%)',
+        cyan: 'hsl(186, 34%, 60%)',
+        darkBrown: 'hsl(25, 47%, 15%)',
+        mediumBrown: 'hsl(28, 10%, 53%)',
+        cream: 'hsl(27, 66%, 92%)',
+        veryPaleOrange: 'hsl(33, 100%, 98%)',
         // Slate
         // 'accent-brand-800': '#1e293b',
         // 'accent-brand-700': '#334155',
