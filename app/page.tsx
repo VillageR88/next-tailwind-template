20c66e887b4f1677e012dcb5596c8487b3c9677a
VillageR88 <villager88pl@gmail.com>
2023-11-13 18:56:13 +0100

    chart fetch
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
+'use client';
+import React, { useEffect, useState } from 'react';
 import '@fontsource/dm-sans';
 import '@fontsource/dm-sans/400.css';
 import '@fontsource/dm-sans/500.css';
 import '@fontsource/dm-sans/700.css';
 import Image from 'next/image';
 
+interface DataItem {
+  day: string;
+  amount: number;
+}
+
 const ChartComponent = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+
+  useEffect(() => {
+    const dataJson = './data.json';
+
+    fetch(dataJson)
+      .then((response) => response.json())
+      .then((jsonData: DataItem[]) => {
+        setData(jsonData);
+      })
+      .catch((error) => {
+        console.error('Error loading JSON:', error);
+      });
+  }, []);
   return (
-    <div className="flex flex-col">
-      <span>test</span>
-      <span>test</span>
+    <div className="flex justify-around">
+      {data.map((item) => (
+        <div key={item.day} className="flex flex-col">
+          <span>{item.amount}</span>
+          <span>{item.day}</span>
+        </div>
+      ))}
     </div>
   );
 };
@@ -25,14 +50,12 @@ export default function Home() {
             <span>My balance</span>
             <span>$921.48</span>
           </div>
-          <Image className="h-auto w-[5em]" src="/images/logo.svg" alt="Logo" height={10} width={10}></Image>
+          <Image className="h-auto w-[5em]" src="/images/logo.svg" alt="Logo" height={10} width={10} priority></Image>
         </div>
         {/* second wrapper */}
         <div>
           <span>Spending - Last 7 days</span>
-          <div>
-            <ChartComponent />
-          </div>
+          <ChartComponent />
         </div>
       </div>
     </main>
