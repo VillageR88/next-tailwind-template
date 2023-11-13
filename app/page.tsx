0bd92a46eeb44a46bed3bde5c8b0a18bc7da7a6c
VillageR88 <villager88pl@gmail.com>
2023-11-13 22:09:44 +0100

    bar chart added for testing
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,47 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart } from 'chart.js';
+import { BarElement, CategoryScale, LinearScale, Title } from 'chart.js';
+
 import '@fontsource/dm-sans';
 import '@fontsource/dm-sans/400.css';
 import '@fontsource/dm-sans/500.css';
 import '@fontsource/dm-sans/700.css';
 import Image from 'next/image';
+Chart.register(BarElement, CategoryScale, LinearScale, Title);
 
 interface DataItem {
   day: string;
   amount: number;
 }
+
+const BarChart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May'],
+    datasets: [
+      {
+        label: 'Monthly Sales',
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(75,192,192,0.4)',
+        hoverBorderColor: 'rgba(75,192,192,1)',
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+};
 
 const ChartComponent = () => {
   const [data, setData] = useState<DataItem[]>([]);
@@ -40,11 +72,11 @@ const ChartComponent = () => {
 
 export default function Home() {
   return (
-    <main className="font-dmSans flex h-screen flex-col items-center justify-center">
+    <main className="flex h-screen flex-col items-center justify-center font-dmSans">
       {/* main wrapper */}
       <div className="md:f w-screen px-4 md:w-auto md:px-0">
         {/* first wrapper */}
-        <div className="bg-softRed flex justify-between md:w-[30em]">
+        <div className="flex justify-between bg-softRed md:w-[30em]">
           {/* left */}
           <div className="flex flex-col text-white">
             <span>My balance</span>
@@ -58,6 +90,7 @@ export default function Home() {
           <ChartComponent />
         </div>
       </div>
+      <BarChart />
     </main>
   );
 }
