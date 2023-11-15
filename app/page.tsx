a0a625885251c73b5b35e0499a9be094478ea6a1
VillageR88 <villager88pl@gmail.com>
2023-11-15 19:31:38 +0100

    mobile layout introduced
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
     <Image className="h-[0.5em] w-[0.7em]" src="./images/icon-arrow-down.svg" alt="arrow down" height={1} width={10} />
   );
   return (
-    <div className="flex flex-col items-center gap-14 px-4 py-6 font-epilogue md:min-h-screen">
+    <div className="flex flex-col items-center gap-[2em] py-6 font-epilogue md:min-h-screen md:gap-14 md:px-4">
       <nav className="flex w-full flex-row justify-between xl:pr-10">
         {/*navbar left wrapper*/}
-        <div className="flex items-center gap-16">
+        <div className="flex items-center px-4 md:gap-16 md:px-0">
           <Image
             className="mt-2 h-[1.7em] w-[5.3em]"
             src="./images/logo.svg"
@@ -25,7 +25,7 @@ export default function Home() {
             width={10}
             priority
           />
-          <div className="flex gap-8">
+          <div className="hidden gap-8 md:flex">
             <div className={navDoubleLayout}>
               <span className={navButtonsLayout}>Feauters</span>
               {arrowDown}
@@ -42,8 +42,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/*navbar right wrapper*/}
-        <div className="flex gap-8">
+        {/*navbar right wrapper(desktop)*/}
+        <div className="hidden gap-8 md:flex">
           <div className={navSingleLayout}>
             <span className={navButtonsLayout}>Login</span>
           </div>
@@ -56,10 +56,10 @@ export default function Home() {
           </div>
         </div>
       </nav>
-      <main className="flex w-full xl:pl-[8em] flex-col justify-around md:flex-row md:gap-[2em]">
+      <main className="flex w-full flex-col-reverse justify-around gap-[2em] md:flex-row xl:pl-[8em]">
         {/*1st col wrapper*/}
-        <div className="flex  w-1/2 flex-col justify-between whitespace-break-spaces">
-          <span className="pt-[1em] font-[700] leading-[1em] text-almostBlack md:text-[3.5rem] xl:text-[4.8rem]">
+        <div className="flex w-full flex-col items-center justify-between text-center md:w-1/2 md:items-start md:whitespace-break-spaces md:text-left">
+          <span className="text-[2.2rem] font-[700] leading-[1em] text-almostBlack md:pt-[1em] md:text-[3.5rem] xl:text-[4.8rem]">
             {'Make\nremote work'}
           </span>
           <span className="md:text[0.8rem] lg:text[1rem] max-w-[26em] pb-[3em] pt-[2em] font-[600] text-mediumGray xl:text-[1.1rem]">
@@ -67,7 +67,7 @@ export default function Home() {
               'Get your team in sync, no matter your location.\nStreamline processes, create team rituals, and watch productivity soar.'
             }
           </span>
-          <button className="self-start rounded-[1em] bg-almostBlack px-8  py-4 text-[1.1rem] font-[600] text-almostWhite md:px-6">
+          <button className="rounded-[1em] bg-almostBlack px-8 py-4  text-[1.1rem] font-[600] text-almostWhite md:self-start md:px-6">
             Learn more
           </button>
           <div className="flex justify-start md:gap-1 md:pt-[3em] lg:pt-[7em] xl:gap-10">
@@ -102,8 +102,21 @@ export default function Home() {
           </div>
         </div>
         {/*2nd col wrapper*/}
-        <div className="flex h-full w-1/2 self-center">
-          <Image className="h-full w-[30em]" src="./images/image-hero-desktop.png" alt="logo" height={10} width={10} />
+        <div className="flex h-full self-center md:w-1/2">
+          <Image
+            className="hidden h-full w-[30em] md:flex"
+            src="./images/image-hero-desktop.png"
+            alt="logo"
+            height={10}
+            width={10}
+          />
+          <Image
+            className="flex h-full w-full md:hidden"
+            src="./images/image-hero-mobile.png"
+            alt="logo"
+            height={10}
+            width={10}
+          />
         </div>
       </main>
     </div>
