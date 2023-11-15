7e06aa4f742e5049d1c12af885ec59e8c06cc7f4
VillageR88 <villager88pl@gmail.com>
2023-11-15 17:34:31 +0100

    layout good
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <Image className="h-[0.5em] w-[0.7em]" src="./images/icon-arrow-down.svg" alt="arrow down" height={1} width={10} />
   );
   return (
-    <div className="flex flex-col items-center gap-10 py-6 font-epilogue md:min-h-screen">
+    <div className="flex flex-col items-center gap-14 py-6 font-epilogue md:min-h-screen">
       <nav className="flex w-full flex-row justify-between">
         {/*navbar left wrapper*/}
         <div className="flex items-center gap-16">
@@ -41,27 +41,60 @@ export default function Home() {
             <span className={navButtonsLayout}>Login</span>
           </div>
           <div className={navSingleLayout}>
-            <span
+            <button
               className={`${navButtonsLayout} rounded-[1em] border-2 border-mediumGray px-5 pb-1.5 pt-2 text-center`}
             >
               Register
-            </span>
+            </button>
           </div>
         </div>
       </nav>
-      <main className="flex w-full justify-center">
+      <main className="flex w-[85%] flex-col justify-center gap-[8em] md:flex-row">
         {/*1st col wrapper*/}
-        <div className="flex w-1/2 flex-col justify-center self-center whitespace-break-spaces ">
-          <span className="text-[5rem] font-[700] leading-[1em] text-almostBlack">{'Make\nremote work'}</span>
-          <span className="max-w-[26em] text-[1.1rem] font-[600] text-mediumGray">
+        <div className="flex flex-col whitespace-break-spaces">
+          <span className="pt-[1em] text-[4.8rem] font-[700] leading-[1em] text-almostBlack">{'Make remote work'}</span>
+          <span className="max-w-[26em] pb-[3em] pt-[2em] text-[1.1rem] font-[600] text-mediumGray">
             {
               'Get your team in sync, no matter your location.\nStreamline processes, create team rituals, and watch productivity soar.'
             }
           </span>
+          <button className="self-start rounded-[1em] bg-almostBlack px-8 py-4 text-[1.1rem] font-[600] text-almostWhite">
+            Learn more
+          </button>
+          <div className="flex justify-between gap-10 pt-[7em]">
+            <Image
+              className="h-[1.3em] w-[7em]"
+              src="./images/client-databiz.svg"
+              alt="databiz logo"
+              height={10}
+              width={10}
+            />
+            <Image
+              className="h-[2.2em] w-[4.4em]"
+              src="./images/client-audiophile.svg"
+              alt="audiophile logo"
+              height={10}
+              width={10}
+            />
+            <Image
+              className="h-[1.3em] w-[6em]"
+              src="./images/client-meet.svg"
+              alt="meet logo"
+              height={10}
+              width={10}
+            />
+            <Image
+              className="h-[1.4em] w-[5em]"
+              src="./images/client-maker.svg"
+              alt="maker logo"
+              height={10}
+              width={10}
+            />
+          </div>
         </div>
         {/*2nd col wrapper*/}
-        <div className="w-[40%]">
-          <Image className="h-auto w-full" src="./images/image-hero-desktop.png" alt="logo" height={10} width={10} />
+        <div className="h-full w-[55%]">
+          <Image className="h-full w-full" src="./images/image-hero-desktop.png" alt="logo" height={10} width={10} />
         </div>
       </main>
     </div>
