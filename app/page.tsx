e6befeab4c8eeb5ab38674b470290a14b9138aa4
VillageR88 <villager88pl@gmail.com>
2023-11-16 19:56:50 +0100

    layout vgood
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ import '@fontsource/manrope/800.css';
 export default function Home() {
   return (
     <main className="font-manrope flex min-h-screen flex-col items-center justify-center">
-      <div className="bg-darkGrayishBlue flex h-[25em] max-w-[33.7em] flex-col items-center text-center">
-        <span className="text-neonGreen text-[0.8rem] font-[600] tracking-[0.3em]">ADVICE #117</span>
-        <span className="text-lightCyan text-[1.7rem] font-[700]">
+      <div className="bg-darkGrayishBlue flex h-[20.7em] max-w-[33.7em] flex-col items-center text-center rounded-[1em] justify-center gap-6">
+        <span className="text-neonGreen text-[0.8rem] font-[600] tracking-[0.35em]">ADVICE #117</span>
+        <span className="text-lightCyan text-[1.7rem] font-[700] px-14 leading-[1.4em]">
           &ldquo;It is easy to sit up and take notice, what's difficult is getting up and taking action.&rdquo;
         </span>
-        <div className="flex">
+        <div className="flex pb-6 pt-4">
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
             <g fill="none" fill-rule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
@@ -43,7 +43,7 @@ export default function Home() {
           </svg>
         </div>
       </div>
-      <div className="bg-neonGreen mt-[-2em] flex items-center  justify-center rounded-full p-5">
+      <div className="bg-neonGreen mt-[-2em] flex items-center  justify-center rounded-full p-5 hover:cursor-pointer">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
