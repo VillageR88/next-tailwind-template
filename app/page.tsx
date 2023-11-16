8d875e8b8c5c280c49156dc4fd7baac4f5870ca0
VillageR88 <villager88pl@gmail.com>
2023-11-16 10:42:57 +0100

    code fixes hamburger
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -137,17 +137,38 @@ const ButtonArrow = ({ name }: { name: Names }) => {
 
 const ButtonMobile = () => {
   const [dropdown, setDropdown] = useState(DropdownState.closed);
-  return (
-    <div className="flex pr-4 md:hidden">
-      <button>
-        <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#151515" fillRule="evenodd">
-            <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
-          </g>
-        </svg>
-      </button>
-    </div>
-  );
+  {
+    return dropdown === DropdownState.closed ? (
+      <div className="flex pr-4 md:hidden">
+        <button
+          onClick={() => {
+            setDropdown(DropdownState.open);
+          }}
+        >
+          <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#151515" fillRule="evenodd">
+              <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+            </g>
+          </svg>
+        </button>
+      </div>
+    ) : (
+      <div className="flex pr-4 md:hidden">
+        <button
+          onClick={() => {
+            setDropdown(DropdownState.closed);
+          }}
+        >
+          <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#151515" fill-rule="evenodd">
+              <path d="m2.393.98 22.628 22.628-1.414 1.414L.979 2.395z" />
+              <path d="M.98 23.607 23.609.979l1.414 1.414L2.395 25.021z" />
+            </g>
+          </svg>
+        </button>
+      </div>
+    );
+  }
 };
 
 export default function Home() {
