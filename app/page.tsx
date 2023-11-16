454dec4105aa0b128db75e2397553d257e85a607
VillageR88 <villager88pl@gmail.com>
2023-11-16 21:58:18 +0100

    wip fetch
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
+'use client';
 import '@fontsource/manrope';
 import '@fontsource/manrope/400.css';
 import '@fontsource/manrope/500.css';
 import '@fontsource/manrope/600.css';
 import '@fontsource/manrope/700.css';
 import '@fontsource/manrope/800.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [text, setText] = useState('');
+  const dataJson = 'https://api.adviceslip.com/advice';
+  useState(() => {
+    fetch(dataJson)
+      .then((response) => response.json())
+      .then((data: string) => {
+        setText(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  });
+
   return (
-    <main className="font-manrope flex min-h-screen flex-col items-center justify-center">
-      <div className="bg-darkGrayishBlue flex h-[20.7em] max-w-[33.7em] flex-col items-center text-center rounded-[1em] justify-center gap-6">
-        <span className="text-neonGreen text-[0.8rem] font-[600] tracking-[0.35em]">ADVICE #117</span>
-        <span className="text-lightCyan text-[1.7rem] font-[700] px-14 leading-[1.4em]">
-          &ldquo;It is easy to sit up and take notice, what's difficult is getting up and taking action.&rdquo;
+    <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
+      <div className="flex h-[20.7em] max-w-[33.7em] flex-col items-center justify-center gap-6 rounded-[1em] bg-darkGrayishBlue text-center">
+        <span className="text-[0.8rem] font-[600] tracking-[0.35em] text-neonGreen">
+          ADVICE #{''}
+          {/*117*/}
+        </span>
+        <span className="px-14 text-[1.7rem] font-[700] leading-[1.4em] text-lightCyan">
+          {''}
+          {/*&ldquo;It is easy to sit up and take notice, what's difficult is getting up and taking action.&rdquo;*/}
         </span>
         <div className="flex pb-6 pt-4">
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd">
+            <g fill="none" fillRule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
               <g transform="translate(138)" fill="#CEE3E9">
                 <rect width="6" height="16" rx="3" />
@@ -24,7 +43,7 @@ export default function Home() {
             </g>
           </svg>
           <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd">
+            <g fill="none" fillRule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
               <g transform="translate(138)" fill="#CEE3E9">
                 <rect width="6" height="16" rx="3" />
@@ -33,7 +52,7 @@ export default function Home() {
             </g>
           </svg>
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd">
+            <g fill="none" fillRule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
               <g transform="translate(138)" fill="#CEE3E9">
                 <rect width="6" height="16" rx="3" />
@@ -43,7 +62,7 @@ export default function Home() {
           </svg>
         </div>
       </div>
-      <div className="bg-neonGreen mt-[-2em] flex items-center  justify-center rounded-full p-5 hover:cursor-pointer">
+      <div className="mt-[-2em] flex items-center justify-center  rounded-full bg-neonGreen p-5 hover:cursor-pointer">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
