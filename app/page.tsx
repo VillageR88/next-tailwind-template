6ee4ebcab76bd07e39cf1f62b46c8c8bb2385d5a
VillageR88 <villager88pl@gmail.com>
2023-11-16 19:20:22 +0100

    colors correction
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import '@fontsource/manrope/800.css';
 export default function Home() {
   return (
     <main className="font-manrope flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col">
+      <div className="bg-darkGrayishBlue flex flex-col">
         <span className="text-neonGreen">ADVICE #117</span>
         <span className="text-lightCyan">
           "It is easy to sit up and take notice, what's difficult is getting up and taking action."
