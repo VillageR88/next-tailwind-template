618e24bb60fee26d8ae2a9519aca8641b44af56b
VillageR88 <villager88pl@gmail.com>
2023-11-16 19:38:15 +0100

    layout update
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,41 @@ import '@fontsource/manrope/800.css';
 export default function Home() {
   return (
     <main className="font-manrope flex min-h-screen flex-col items-center justify-center">
-      <div className="bg-darkGrayishBlue flex flex-col">
+      <div className="bg-darkGrayishBlue flex h-[25em] max-w-[33.7em] flex-col items-center text-center">
         <span className="text-neonGreen">ADVICE #117</span>
-        <span className="text-lightCyan">
+        <span className="text-[2rem] text-lightCyan">
           "It is easy to sit up and take notice, what's difficult is getting up and taking action."
         </span>
-        <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fill-rule="evenodd">
-            <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
-            <g transform="translate(138)" fill="#CEE3E9">
-              <rect width="6" height="16" rx="3" />
-              <rect x="14" width="6" height="16" rx="3" />
+        <div className="flex">
+          <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fill-rule="evenodd">
+              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+              <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
             </g>
-          </g>
-        </svg>
-        <div className="bg-neonGreen flex h-[4em] w-[4em] items-center justify-center rounded-full">
+          </svg>
+          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fill-rule="evenodd">
+              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+              <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+          <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fill-rule="evenodd">
+              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+              <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div className="bg-neonGreen mt-[11.5em] flex items-center  justify-center rounded-full p-5">
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
