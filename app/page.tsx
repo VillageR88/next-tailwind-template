d8159944cd62b694492e9e6b3efafe0d97232d8c
VillageR88 <villager88pl@gmail.com>
2023-11-16 19:48:19 +0100

    layout update
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <main className="font-manrope flex min-h-screen flex-col items-center justify-center">
       <div className="bg-darkGrayishBlue flex h-[25em] max-w-[33.7em] flex-col items-center text-center">
-        <span className="text-neonGreen">ADVICE #117</span>
-        <span className="text-[2rem] text-lightCyan">
-          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+        <span className="text-neonGreen text-[0.8rem] font-[600] tracking-[0.3em]">ADVICE #117</span>
+        <span className="text-lightCyan text-[1.7rem] font-[700]">
+          &ldquo;It is easy to sit up and take notice, what's difficult is getting up and taking action.&rdquo;
         </span>
         <div className="flex">
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
@@ -42,14 +42,14 @@ export default function Home() {
             </g>
           </svg>
         </div>
-        <div className="bg-neonGreen mt-[11.5em] flex items-center  justify-center rounded-full p-5">
-          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
-              fill="#202733"
-            />
-          </svg>
-        </div>
+      </div>
+      <div className="bg-neonGreen mt-[-2em] flex items-center  justify-center rounded-full p-5">
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+            fill="#202733"
+          />
+        </svg>
       </div>
     </main>
   );
