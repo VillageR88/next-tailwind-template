ebe710431dee1fa76047bb04931cf9e5e48e56be
VillageR88 <villager88pl@gmail.com>
2023-11-17 13:23:59 +0100

    updated ShortBox
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,22 @@ import '@fontsource/rubik';
 import '@fontsource/rubik/300.css';
 import '@fontsource/rubik/400.css';
 import '@fontsource/rubik/500.css';
+import Image from 'next/image';
 
 const ShortBox = () => {
   return (
     <div className="flex flex-col">
-      <div className="rounded] mb-[-1em] h-[3.5em] w-[16em] rounded-t-[1em] bg-orange-300"></div>
-      <div className="bg-darkBlue rounded] h-[12em] w-[16em] rounded-[1em] p-7">
+      <div className="bg-lightRedStudy mb-[-1em] inline-grid h-[3.8em] w-[16em] justify-end  overflow-hidden rounded rounded-t-[0.8em]">
+        <Image
+          className="mr-[1em] mt-[-0.7em]"
+          src="./images/icon-work.svg"
+          width="79"
+          height="79"
+          alt="work icon"
+          priority
+        />
+      </div>
+      <div className="bg-darkBlue rounded] h-[12.5em] w-[16em] rounded-[0.8em] p-7">
         <span className="text-white">TEST</span>;
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
       },
       colors: {
         blue: 'hsl(246, 80%, 60%)',
+        lightRedStudy: 'hsl(15, 100%, 70%)',
         softBluePlay: 'hsl(195, 74%, 62%)',
-        lightRedStudy: 'hsl(348, 100%, 68%)',
         limeGreenExercise: 'hsl(145, 58%, 55%)',
         violetSocial: 'hsl(264, 64%, 52%)',
         softOrangeSelfCare: 'hsl(43, 84%, 65%)',
@@ -24,6 +24,9 @@ const config: Config = {
         darkBlue: 'hsl(235, 46%, 20%)',
         desaturatedBlue: 'hsl(235, 45%, 61%)',
         paleBlue: 'hsl(236, 100%, 87%)',
+      },
+      backgroundImage: {
+        //bgCardFront: "url('/Frontend_Mentor/interactive-card-details-form-main/src/images/bg-card-front.png')",
       },
     },
   },
