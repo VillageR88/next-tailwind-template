09ef14c9508e46a06fa6bceeb54f7d891ad3ddb4
VillageR88 <villager88pl@gmail.com>
2023-11-17 01:39:04 +0100

    fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     fetch(dataJson)
       .then((response) => response.json())
       .then((data: JSON | undefined) => {
-        setText(data.slip);
+        setText(data ? data.slip : undefined);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
@@ -24,17 +24,17 @@ export default function Home() {
   }, [clicked]);
 
   return text ? (
-    <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
-      <div className="flex h-[20.7em] max-w-[33.7em] flex-col items-center justify-center gap-6 rounded-[1em] bg-darkGrayishBlue text-center">
-        <span className="text-[0.8rem] font-[600] tracking-[0.35em] text-neonGreen">
+    <main className="md:0 flex h-screen w-full flex-col items-center justify-center px-4 font-manrope md:min-h-screen">
+      <div className="flex min-h-[20.7em] w-full flex-col items-center justify-center gap-6 rounded-[1em] bg-darkGrayishBlue text-center md:w-[33.7em] ">
+        <span className="pt-6 text-[0.8rem] font-[600] tracking-[0.35em] text-neonGreen">
           ADVICE #{text.id}
           {/*117*/}
         </span>
-        <span className="px-14 text-[1.7rem] font-[700] leading-[1.4em] text-lightCyan">
+        <span className="px-4 text-[1.7rem] font-[700] leading-[1.4em]  text-lightCyan md:px-14">
           {text.advice}
           {/*&ldquo;It is easy to sit up and take notice, what's difficult is getting up and taking action.&rdquo;*/}
         </span>
-        <div className="flex pb-6 pt-4">
+        <div className="hidden pb-8 pt-4 md:flex">
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
             <g fill="none" fillRule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
@@ -54,6 +54,17 @@ export default function Home() {
             </g>
           </svg>
           <svg className="w-[4.8em]" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="evenodd">
+              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+              <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div className="flex pb-8  md:hidden">
+          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
             <g fill="none" fillRule="evenodd">
               <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
               <g transform="translate(138)" fill="#CEE3E9">
