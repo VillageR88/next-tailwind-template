9b53761fe03ef064c022cc778b7ddc9106318502
VillageR88 <villager88pl@gmail.com>
2023-11-17 11:51:40 +0100

    shortBox test
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import '@fontsource/rubik'; // Defaults to weight 400
-import '@fontsource/rubik/400.css'; // Specify weight
+import '@fontsource/rubik';
+import '@fontsource/rubik/300.css';
+import '@fontsource/rubik/400.css';
+import '@fontsource/rubik/500.css';
+
+const ShortBox = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="rounded] mb-[-1em] h-[3.5em] w-[16em] rounded-t-[1em] bg-orange-300"></div>
+      <div className="bg-darkBlue rounded] h-[12em] w-[16em] rounded-[1em] p-7">
+        <span className="text-white">TEST</span>;
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <main className="font-rubik flex min-h-screen flex-col items-center justify-center">
-      <div className="text-white">Initial commit</div>
+      <ShortBox />
     </main>
   );
 }
