8e674da92e271fc9a90a8150f0010c8a02ddbd39
VillageR88 <villager88pl@gmail.com>
2023-11-17 10:57:21 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import '@fontsource/rubik'; // Defaults to weight 400
+import '@fontsource/rubik/400.css'; // Specify weight
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="font-rubik flex min-h-screen flex-col items-center justify-center">
+      <div className="text-white">Initial commit</div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,19 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        rubik: 'Rubik',
       },
       colors: {
-        // Slate
-        // 'accent-brand-800': '#1e293b',
-        // 'accent-brand-700': '#334155',
-        // 'accent-brand-600': '#475569',
+        blue: 'hsl(246, 80%, 60%)',
+        softBluePlay: 'hsl(195, 74%, 62%)',
+        lightRedStudy: 'hsl(348, 100%, 68%)',
+        limeGreenExercise: 'hsl(145, 58%, 55%)',
+        violetSocial: 'hsl(264, 64%, 52%)',
+        softOrangeSelfCare: 'hsl(43, 84%, 65%)',
+        veryDarkBlue: 'hsl(226, 43%, 10%)',
+        darkBlue: 'hsl(235, 46%, 20%)',
+        desaturatedBlue: 'hsl(235, 45%, 61%)',
+        paleBlue: 'hsl(236, 100%, 87%)',
       },
     },
   },
