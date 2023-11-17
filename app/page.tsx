5e35fcbd1e5b920feab795f301fbdd912c606ac7
VillageR88 <villager88pl@gmail.com>
2023-11-17 01:13:35 +0100

    working
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import '@fontsource/manrope/500.css';
 import '@fontsource/manrope/600.css';
 import '@fontsource/manrope/700.css';
 import '@fontsource/manrope/800.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [text, setText] = useState<JSON>();
+  const [clicked, setClicked] = useState<boolean>(false);
   const dataJson = 'https://api.adviceslip.com/advice';
-  useState(() => {
+
+  useEffect(() => {
     fetch(dataJson)
       .then((response) => response.json())
       .then((data: JSON | undefined) => {
@@ -19,7 +21,7 @@ export default function Home() {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  });
+  }, [clicked]);
 
   return text ? (
     <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
@@ -62,7 +64,12 @@ export default function Home() {
           </svg>
         </div>
       </div>
-      <div className="hover:shadow-t-aura2 mt-[-2em] flex items-center justify-center rounded-full bg-neonGreen p-5 hover:cursor-pointer hover:shadow-emerald-400">
+      <div
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+        className="hover:shadow-t-aura2 mt-[-2em] flex items-center justify-center rounded-full bg-neonGreen p-5 hover:cursor-pointer hover:shadow-emerald-400"
+      >
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
