a7054f1f31637fd84f059ef025cc5f2d6ce86c65
VillageR88 <villager88pl@gmail.com>
2023-11-18 13:25:22 +0100

    splitter grided and mapped
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import '@fontsource/space-mono/400.css';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="font-spaceMono text-lg">Space Mono 🎈</p>
-      <p className="font-mono">Font-Mono 🌍</p>
+    <main className="font-spaceMono flex min-h-screen flex-col items-center justify-center">
+      <div className="grid grid-cols-4">
+        {'SPLITTER'.split('').map((x) => (
+          <span className="p-[0.2em] font-[700]" key={x}>
+            {x}
+          </span>
+        ))}
+      </div>
     </main>
   );
 }
