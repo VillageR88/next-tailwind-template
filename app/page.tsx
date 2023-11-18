b13eaa7be27ab47c3f80656da96f60215d62476a
VillageR88 <villager88pl@gmail.com>
2023-11-18 03:01:53 +0100

    now mobile left
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -156,7 +156,7 @@ export default function Home() {
           </div>
           <div className="grid gap-[1.9em] md:col-span-1 md:grid-cols-3 lg:col-span-3">
             {dataJson.map((item, index) => (
-              <ShortBox key={index} title={item.title} timeframes={item.timeframes[buttonCol1]} />
+              <ShortBox key={index} title={item.title} timeframes={item.timeframes[buttonCol1 as keyof undefined]} />
             ))}
           </div>
         </div>
