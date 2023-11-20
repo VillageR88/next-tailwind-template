92634ab9bb93b000ab4df9e416cfd2cc6ee6bea7
VillageR88 <villager88pl@gmail.com>
2023-11-20 23:02:19 +0100

    improving layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
+'use client';
 import '@fontsource/barlow';
 import '@fontsource/barlow/600.css';
 import '@fontsource/fraunces';
 import '@fontsource/fraunces/700.css';
 import '@fontsource/fraunces/900.css';
+import logo from './images/logo.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <div className="grid h-full w-full grid-cols-2 justify-center">
-        <div className="bg-header col-span-2 h-full w-full bg-cover bg-[100%_100%] ">
-          <p className="font-barlow text-lg">Hello</p>
-          <p className="font-fraunces text-lg">Hello</p>
-          <p className="text-lg">Hello</p>
-          <p className="font-mono">World 🌍</p>
+        <div className="bg-header col-span-2 h-[50em] bg-cover  bg-top ">
+          {' '}
+          <Image src={logo as string} alt="logo" />
         </div>
         <div className="col-span-1 ">
           <span>TEST</span>
