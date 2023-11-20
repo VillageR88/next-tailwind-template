d2157a0964f2bfc348d769224076a21bc41d03dd
VillageR88 <villager88pl@gmail.com>
2023-11-20 23:28:35 +0100

    layout naming n positions corrected
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,24 @@ const buttonLayout1 = '';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <div className="grid h-full w-full grid-cols-2 justify-center">
-        <div className="bg-header col-span-2 flex h-[50em] items-start justify-between bg-cover bg-top pl-6 pr-10 pt-6">
-          <Image src={logo as string} alt="logo" className="" />
-          <div className="flex gap-4 ">
-            <button className={buttonLayout1}>About</button>
-            <button className={buttonLayout1}>Services</button>
-            <button className={buttonLayout1}>Projects</button>
-            <button className={buttonLayout1}>CONTACT</button>
-          </div>
+    <div className="items-top grid min-h-screen grid-cols-2 flex-col justify-center">
+      <nav className="bg-header col-span-2 flex h-[50em] items-start justify-between bg-cover bg-top pl-6 pr-10 pt-6">
+        <Image src={logo as string} alt="logo" className="" />
+        <div className="flex gap-4 ">
+          <button className={buttonLayout1}>About</button>
+          <button className={buttonLayout1}>Services</button>
+          <button className={buttonLayout1}>Projects</button>
+          <button className={buttonLayout1}>CONTACT</button>
         </div>
+      </nav>
+      <main>
         <div className="col-span-1 ">
           <span>TEST</span>
         </div>
         <div className="col-span-1 ">
           <span>TEST</span>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
