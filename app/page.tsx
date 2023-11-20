2c31a20518ad13a98558cd5fcc083e49a62b7207
VillageR88 <villager88pl@gmail.com>
2023-11-20 22:20:52 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import '@fontsource/barlow';
+import '@fontsource/barlow/600.css';
+import '@fontsource/fraunces';
+import '@fontsource/fraunces/700.css';
+import '@fontsource/fraunces/900.css';
+
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+      <div className="grid h-full w-full grid-cols-2 justify-center">
+        <div className="bg-header col-span-2 h-full w-full bg-cover bg-[100%_100%] ">
+          <p className="font-barlow text-lg">Hello</p>
+          <p className="font-fraunces text-lg">Hello</p>
+          <p className="text-lg">Hello</p>
+          <p className="font-mono">World 🌍</p>
+        </div>
+        <div className="col-span-1 ">
+          <span>TEST</span>
+        </div>
+        <div className="col-span-1 ">
+          <span>TEST</span>
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,23 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        barlow: 'Barlow',
+        fraunces: 'Fraunces',
+      },
+      backgroundImage: {
+        header: "url('./images/desktop/image-header.jpg')",
       },
       colors: {
-        // Slate
-        // 'accent-brand-800': '#1e293b',
-        // 'accent-brand-700': '#334155',
-        // 'accent-brand-600': '#475569',
+        softRed: 'hsl(7, 99%, 70%)',
+        yellow: 'hsl(51, 100%, 49%)',
+        darkDesaturatedCyanGgraphicDesignText: 'hsl(167, 40%, 24%)',
+        darkBluePhotographyText: 'hsl(198, 62%, 26%)',
+        darkModerateCyanFooter: 'hsl(168, 34%, 41%)',
+        veryDarkDesaturatedBlue: 'hsl(212, 27%, 19%)',
+        veryDarkGrayishBlue: 'hsl(213, 9%, 39%)',
+        darkGrayishBlue: 'hsl(232, 10%, 55%)',
+        grayishBlue: 'hsl(210, 4%, 67%)',
+        white: 'hsl(0, 0%, 100%)',
       },
     },
   },
