1a48211bd6dcbdeca47d083a778b453e4b175ee9
VillageR88 <villager88pl@gmail.com>
2023-11-21 09:57:05 +0100

    layout top improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import '@fontsource/fraunces';
 import '@fontsource/fraunces/700.css';
 import '@fontsource/fraunces/900.css';
 import logo from './images/logo.svg';
+import arrowDown from './images/icon-arrow-down.svg';
 import Image from 'next/image';
 
 const buttonLayout1 = 'text-white text-[1.1rem] font-barlow font-[600]';
@@ -12,8 +13,8 @@ const buttonLayout1 = 'text-white text-[1.1rem] font-barlow font-[600]';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <nav className="col-span-2 flex h-[50em] items-start  bg-header bg-cover bg-top">
-        <div className="flex w-full items-center justify-between px-2 pt-9 md:mx-0 md:pl-10 md:pr-12">
+      <nav className="col-span-2 flex h-[50em] flex-col items-start items-center gap-[6em] bg-header bg-cover bg-top">
+        <div className="flex w-full items-center  justify-between px-2 pt-9 md:mx-0 md:pl-10 md:pr-12">
           <Image src={logo as string} alt="logo" className="h-auto w-[10.5em]" />
           <div className="flex gap-2 md:gap-12 ">
             <button className={buttonLayout1}>About</button>
@@ -21,6 +22,10 @@ export default function Home() {
             <button className={buttonLayout1}>Projects</button>
             <button className="rounded-[2em] bg-white px-7 py-4 font-fraunces font-[700]">CONTACT</button>
           </div>
+        </div>
+        <div className="flex flex-col items-center gap-[1.3em] font-fraunces text-[2rem] font-[900] tracking-[0.18em] text-white md:text-[3.5rem]">
+          <span>WE ARE CREATIVES</span>
+          <Image src={arrowDown as string} className="" alt="arrow down" />
         </div>
       </nav>
       <main className="md:grid md:grid-cols-2">
