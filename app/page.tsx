b413bd63d6bc1b7f64986afc883a6bca1b0b3d31
VillageR88 <villager88pl@gmail.com>
2023-11-21 10:30:48 +0100

    next window
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const buttonLayout1 = 'text-white text-[1.1rem] font-barlow font-[600]';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <nav className="col-span-2 flex h-[50em] flex-col items-start items-center gap-[6em] bg-header bg-cover bg-top">
+      <nav className="col-span-2 flex h-[50em] flex-col items-center gap-[6em] bg-header bg-cover bg-top">
         <div className="flex w-full items-center  justify-between px-2 pt-9 md:mx-0 md:pl-10 md:pr-12">
           <Image src={logo as string} alt="logo" className="h-auto w-[10.5em]" />
           <div className="flex gap-2 md:gap-12 ">
@@ -23,17 +23,33 @@ export default function Home() {
             <button className="rounded-[2em] bg-white px-7 py-4 font-fraunces font-[700]">CONTACT</button>
           </div>
         </div>
-        <div className="flex flex-col items-center gap-[1.3em] font-fraunces text-[2rem] font-[900] tracking-[0.18em] text-white md:text-[3.5rem]">
+        <div className="flex flex-col items-center gap-[1.54em] font-fraunces text-[2rem] font-[900] tracking-[0.18em] text-white md:text-[2.8rem] lg:text-[3.5rem]">
           <span>WE ARE CREATIVES</span>
           <Image src={arrowDown as string} className="" alt="arrow down" />
         </div>
       </nav>
-      <main className="md:grid md:grid-cols-2">
-        <div className="bg-white">
-          <span>TEST</span>
-        </div>
-        <div className="bg-yellow">
-          <span>TEST</span>
+      <main>
+        {/*six pack*/}
+        <div>
+          {/*row1*/}
+          <div className="grid grid-cols-2">
+            <div className=" bg-[#FFFBF8]">
+              {/*row1-col1*/}
+              <div className="flex flex-col h-[35em] justify-center items-start gap-[2em] pl-[10.5em] pr-[7em]">
+                <span className="font-fraunces text-[2.5rem] font-[900] leading-[1.2em] text-veryDarkDesaturatedBlue">
+                  Transform your brand
+                </span>
+                <span className="font-barlow text-[1.1rem] font-[600] text-[#8D8C90]">
+                  We are a full-service creative agency specializing in helping brands grow fast. Engage your clients
+                  through compelling visuals that do most of the marketing for you.
+                </span>
+                <button className="font-fraunces font-[900] text-veryDarkDesaturatedBlue">LEARN MORE</button>
+              </div>
+            </div>
+            <div className="bg-yellow">
+              <span>TEST</span>
+            </div>
+          </div>
         </div>
       </main>
     </div>
