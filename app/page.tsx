9dbf97591289aefafc39ad139879d12198a05091
VillageR88 <villager88pl@gmail.com>
2023-11-21 09:34:36 +0100

    layout improved plus nav wrapper added
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,20 @@ import '@fontsource/fraunces/900.css';
 import logo from './images/logo.svg';
 import Image from 'next/image';
 
-const buttonLayout1 = 'text-white';
+const buttonLayout1 = 'text-white text-[1.1rem] font-barlow font-[600]';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <nav className="bg-header col-span-2 flex h-[50em] items-start justify-between bg-cover bg-top pl-6 pr-10 pt-6">
-        <Image src={logo as string} alt="logo" className="" />
-        <div className="flex gap-4 ">
-          <button className={buttonLayout1}>About</button>
-          <button className={buttonLayout1}>Services</button>
-          <button className={buttonLayout1}>Projects</button>
-          <button className="rounded-[2em] bg-white px-4 py-2 font-fraunces font-[700]">CONTACT</button>
+      <nav className="col-span-2 flex h-[50em] items-start  bg-header bg-cover bg-top">
+        <div className="flex w-full items-center justify-between px-2 pt-9 md:mx-0 md:pl-10 md:pr-12">
+          <Image src={logo as string} alt="logo" className="h-auto w-[10.5em]" />
+          <div className="flex gap-2 md:gap-12 ">
+            <button className={buttonLayout1}>About</button>
+            <button className={buttonLayout1}>Services</button>
+            <button className={buttonLayout1}>Projects</button>
+            <button className="rounded-[2em] bg-white px-7 py-4 font-fraunces font-[700]">CONTACT</button>
+          </div>
         </div>
       </nav>
       <main className="md:grid md:grid-cols-2">
