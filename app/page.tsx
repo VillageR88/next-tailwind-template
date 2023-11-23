144d54ca834855d1ee0a029db1d8c3de056a180e
VillageR88 <villager88pl@gmail.com>
2023-11-23 13:13:10 +0100

    damn that was challenging nav background fixed
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,10 @@ import '@fontsource/ubuntu/700.css';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center">
-      <nav className="bg h-[37.5em] w-full overflow-hidden rounded-bl-[6.5em] bg-gradient-to-r  from-veryLightRed to-lightRed">
-        <div className="static ml-[-27.5em] mt-[-84em] h-[140em] w-auto bg-[url('./images/bg-pattern-intro-desktop.svg')]"></div>
+      <nav className="bg flex h-[37.5em] w-full justify-center overflow-hidden rounded-bl-[6.5em] bg-gradient-to-r from-veryLightRed to-lightRed">
+        <div className="flex h-full w-full justify-center bg-[url('./images/bg-pattern-intro-desktop.svg')] bg-[25.4%_52.1%]">
+          <span>sdfsdf</span>
+        </div>
       </nav>
       <main></main>
     </div>
