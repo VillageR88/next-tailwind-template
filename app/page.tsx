8ad700b3fd895a47aecf4c88556a49d918d457a5
VillageR88 <villager88pl@gmail.com>
2023-11-23 12:30:33 +0100

    background nav whoa that was tricky
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,13 @@ import '@fontsource/ubuntu';
 import '@fontsource/ubuntu/400.css';
 import '@fontsource/ubuntu/500.css';
 import '@fontsource/ubuntu/700.css';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center">
-      <nav className="h-[37.5em] w-full bg-veryLightRed"></nav>
+      <nav className="bg h-[37.5em] w-full overflow-hidden rounded-bl-[6.5em] bg-gradient-to-r  from-veryLightRed to-lightRed">
+        <div className="static ml-[-27.5em] mt-[-84em] h-[140em] w-auto bg-[url('./images/bg-pattern-intro-desktop.svg')]"></div>
+      </nav>
       <main></main>
     </div>
   );
