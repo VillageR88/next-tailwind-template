58a0e8226e2e8207014e4ba9e3207fb7729aa418
VillageR88 <villager88pl@gmail.com>
2023-11-26 18:44:26 +0100

    layout improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-start font-commissioner">
       <nav className="h-full w-full">
         <div className="h-[25em] w-full  bg-[url('./images/image-hero-desktop.jpg')]">
-          <div className="from-customDark h-[8em] flex items-end pb-[3.8em] pr-[10.3em]  justify-between from-5%   to-transparent to-100% bg-gradient-to-b pl-[10.3em]">
+          <div className="from-customDark flex h-[8.05em] items-end justify-between bg-gradient-to-b from-5% to-transparent to-100% pb-[3.8em] pl-[10.4em] pr-[10.3em]">
             <Image className="h-fit" src={logo as string} alt="logo image" />
-            <div className="text-white gap-[2em] flex text-[0.85rem]">
+            <div className="flex gap-[2.44em] text-[0.83rem] text-gray-50">
               <button>About</button>
               <button>Discover</button>
               <button>Get Started</button>
@@ -20,6 +20,13 @@ export default function Home() {
           </div>
         </div>
       </nav>
+      <main>
+        <div className="flex flex-col gap-[1.6em]">
+          {/*first block*/}
+          <div className="border-1 mt-[-5.67em] h-[16.7em] w-[45.5em] rounded-[0.5em] bg-white outline outline-1 outline-gray-100"></div>
+          <div className="border-1 h-[60em] w-[45.5em] rounded-[0.5em] bg-white outline outline-1 outline-gray-100"></div>
+        </div>
+      </main>
     </div>
   );
 }
