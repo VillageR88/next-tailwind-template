144bba6ef0325ad633a6f65eaf8f65380dd886a0
VillageR88 <villager88pl@gmail.com>
2023-11-30 12:18:42 +0100

    central wrapper bettered layout initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import '@fontsource/manrope/800.css';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
-      <div className="flex flex-col h-[50em] w-full items-center bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat ">
+      <div className="flex h-[50em] w-full flex-col items-center bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat ">
         {/*top wrapper*/}
         <div className="mt-[3.75em] flex h-[10em] w-fit flex-col items-center justify-center gap-[0.62em] bg-[url('./images/pattern-circles.svg')] bg-center  bg-no-repeat">
           <span className="text-[1.75rem] font-[800] text-darkDesaturatedBlue_TextCTABackground">
@@ -17,7 +17,7 @@ export default function Home() {
           </span>
         </div>
         {/*main wrapper*/}
-        <div className='h-[3em] bg-white w-[3em]'></div>
+        <div className="h-[24.8em] shadow-xl shadow-slate-200 mt-[2.9em] w-[33.8em] rounded-[0.5em] bg-white"></div>
       </div>
     </main>
   );
