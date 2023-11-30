16e901f435ad233972d367f0df11ee9a027a33b3
VillageR88 <villager88pl@gmail.com>
2023-11-30 14:39:44 +0100

    layout update WIP
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+'use client';
 import '@fontsource/manrope';
 import '@fontsource/manrope/400.css';
 import '@fontsource/manrope/600.css';
 import '@fontsource/manrope/800.css';
+import React from 'react';
+import Slider from '@mui/material/Slider';
+import thumbImage from './images/icon-slider.svg';
+import { styled } from '@mui/system'; // Import styled from @mui/system
+
+const CustomThumb = styled('span')({
+  width: 32,
+  height: 32,
+  borderRadius: '50%',
+  '&::before': {
+    content: "''",
+    backgroundImage: `url(${thumbImage})`,
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100%',
+    display: 'block',
+    zIndex: 1, // Ensure the thumb is above other elements
+  },
+  '&:hover': {
+    boxShadow: 'none',
+  },
+});
 
 export default function Home() {
   return (
@@ -17,7 +40,29 @@ export default function Home() {
           </span>
         </div>
         {/*main wrapper*/}
-        <div className="h-[24.8em] shadow-xl shadow-slate-200 mt-[2.9em] w-[33.8em] rounded-[0.5em] bg-white"></div>
+        <div className="mt-[2.9em] flex h-[24.8em] w-[33.8em] flex-col rounded-[0.5em] bg-white shadow-xl shadow-slate-200">
+          <div className="mt-[3.6em] flex justify-between px-[2.8em]">
+            <span className="text-[0.9rem] font-[800]">100K PAGEVIEWS</span>
+            <div>
+              <span>$16.00</span>
+              <span>/ month</span>
+            </div>
+          </div>
+          <Slider
+            className="h-[0.5em] w-[82%] self-center"
+            max={4}
+            min={0}
+            components={{ Thumb: CustomThumb }} // Apply the custom thumb component
+            sx={{
+              '& .MuiSlider-track': {
+                backgroundColor: 'hsl(174, 86%, 45%)', // Light gray background for track
+              },
+              '& .MuiSlider-rail': {
+                backgroundColor: '#E0E0E0', // Light gray background for rail
+              },
+            }}
+          />{' '}
+        </div>
       </div>
     </main>
   );
