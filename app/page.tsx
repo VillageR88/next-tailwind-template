cae18f0c6384dc8354b6e9128c9f26c5ab5860d4
VillageR88 <villager88pl@gmail.com>
2023-11-30 12:10:52 +0100

    top amazingly close to design
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,13 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
       <div className="flex h-[50em] w-full justify-center bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat ">
         {/*top*/}
-        <div className="mt-[4em] flex h-[10em] w-fit flex-col justify-center items-center bg-[url('./images/pattern-circles.svg')] bg-center  bg-no-repeat">
-          <span>Simple, traffic-based pricing</span>
-          <span>Sign-up for our 30-day trial. No credit card required.</span>
+        <div className="mt-[3.75em] flex h-[10em] w-fit flex-col items-center justify-center gap-[0.62em] bg-[url('./images/pattern-circles.svg')] bg-center  bg-no-repeat">
+          <span className="text-[1.75rem] font-[800] text-darkDesaturatedBlue_TextCTABackground">
+            Simple, traffic-based pricing
+          </span>
+          <span className="text-[0.94rem] font-[600] text-grayishBlue_Text">
+            Sign-up for our 30-day trial. No credit card required.
+          </span>
         </div>
       </div>
     </main>
