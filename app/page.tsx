14cd69d06e1d0b9cfc1bb90bb61f1e2b6b50aa55
VillageR88 <villager88pl@gmail.com>
2023-11-30 12:06:24 +0100

    top initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,13 @@ import '@fontsource/manrope/800.css';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
-      <div className="h-[50em] w-full bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat "></div>
+      <div className="flex h-[50em] w-full justify-center bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat ">
+        {/*top*/}
+        <div className="mt-[4em] flex h-[10em] w-fit flex-col justify-center items-center bg-[url('./images/pattern-circles.svg')] bg-center  bg-no-repeat">
+          <span>Simple, traffic-based pricing</span>
+          <span>Sign-up for our 30-day trial. No credit card required.</span>
+        </div>
+      </div>
     </main>
   );
 }
