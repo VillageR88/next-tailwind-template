babe0b864c076311fe328f71cf5864d7342d8582
VillageR88 <villager88pl@gmail.com>
2023-11-30 11:55:07 +0100

    initial background
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import '@fontsource/manrope';
+import '@fontsource/manrope/400.css';
+import '@fontsource/manrope/600.css';
+import '@fontsource/manrope/800.css';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="flex min-h-screen flex-col items-center justify-center font-manrope">
+      <div className="h-[50em] w-full bg-[#F9FAFF] bg-[url('./images/bg-pattern.svg')] bg-top bg-no-repeat "></div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,22 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        manrope: 'Manrope',
       },
       colors: {
-        // Slate
-        // 'accent-brand-800': '#1e293b',
-        // 'accent-brand-700': '#334155',
-        // 'accent-brand-600': '#475569',
+        //### Primary
+        softCyan_FullSliderBar: 'hsl(174, 77%, 80%)',
+        strongCyan_SliderBackground: 'hsl(174, 86%, 45%)',
+        lightGrayishRed_DiscountBackground: 'hsl(14, 92%, 95%)',
+        lightRed_Discount_Text: 'hsl(15, 100%, 70%)',
+        paleBlue_CTA_Text: 'hsl(226, 100%, 87%)',
+        //### Neutral
+        white_PricingComponentBackground: 'hsl (0, 0%, 100%)',
+        veryPaleBlue_MainBackground: 'hsl(230, 100%, 99%)',
+        lightGrayishBlue_EmptySliderBar: 'hsl(224, 65%, 95%)',
+        lightGrayishBlue_ToggleBackground: 'hsl(223, 50%, 87%)',
+        grayishBlue_Text: 'hsl(225, 20%, 60%)',
+        darkDesaturatedBlue_TextCTABackground: 'hsl(227, 35%, 25%)',
       },
     },
   },
