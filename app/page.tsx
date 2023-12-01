878ed15124e4afe201985a4ec5ce4457fdc5b869
VillageR88 <villager88pl@gmail.com>
2023-12-01 12:19:57 +0100

    nav bg mask 0.40
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <nav className="h-full w-full">
-        <div className="h-[40.6em] w-full bg-[url('./images/desktop/image-hero.jpg')] bg-center"></div>
+        <div className="h-[40.6em] w-full bg-[url('./images/desktop/image-hero.jpg')] bg-center">
+          <div className="h-full w-full bg-black bg-opacity-40"></div>
+        </div>
       </nav>
       <div className="h-[120em] w-full bg-white"></div>
     </div>
