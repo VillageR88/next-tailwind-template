7358bf3e7ed948478255eff7aa76cded854a204d
VillageR88 <villager88pl@gmail.com>
2023-12-02 22:24:48 +0100

    grid building blocks
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import '@fontsource/barlow-semi-condensed/600.css';
 export default function Home() {
   return (
     <main className="font-barlowSemiCondensed flex min-h-screen flex-col items-center justify-center">
-      <div className="h-[35.7em] w-[69.4em] bg-green-500">
-        <div className="bg-gre h-[5em] w-[5em]"></div>
+      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 bg-green-500">
+        <div className="col-span-2 bg-moderateViolet"></div>
+        <div className="col-span-1 bg-veryDarkGrayishBlue"></div>
+        <div className="row-span-2 bg-white"></div>
+        <div className="col-span-1 bg-white"></div>
+        <div className="col-span-2 bg-veryDarkBlackishBlue"></div>
       </div>
     </main>
   );
