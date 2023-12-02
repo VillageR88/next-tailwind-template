72eab3d7ea8ac42dc4d0d4481d8ffab5603fb47e
VillageR88 <villager88pl@gmail.com>
2023-12-02 22:20:08 +0100

    temp greenscreen
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import '@fontsource/barlow-semi-condensed/600.css';
 export default function Home() {
   return (
     <main className="font-barlowSemiCondensed flex min-h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+      <div className="h-[35.7em] w-[69.4em] bg-green-500">
+        <div className="bg-gre h-[5em] w-[5em]"></div>
+      </div>
     </main>
   );
 }
