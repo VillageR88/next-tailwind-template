631c6d0677aff07b729e74b0748423077bfcca86
VillageR88 <villager88pl@gmail.com>
2023-12-02 22:04:38 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import '@fontsource/barlow-semi-condensed/500.css';
+import '@fontsource/barlow-semi-condensed/600.css';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="font-barlowSemiCondensed flex min-h-screen flex-col items-center justify-center">
       <p className="text-lg">Hello</p>
       <p className="font-mono">World 🌍</p>
     </main>
