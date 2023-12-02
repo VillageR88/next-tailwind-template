d55c00aae8b156433d1b43622c91391d27506b59
VillageR88 <villager88pl@gmail.com>
2023-12-02 22:38:42 +0100

    building blocks initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import '@fontsource/barlow-semi-condensed/600.css';
 export default function Home() {
   return (
     <main className="font-barlowSemiCondensed flex min-h-screen flex-col items-center justify-center">
-      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 bg-green-500">
-        <div className="col-span-2 bg-moderateViolet"></div>
-        <div className="col-span-1 bg-veryDarkGrayishBlue"></div>
+      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 gap-x-[1.9em] gap-y-[2.5em]">
+        <div className="col-span-2 h-[17.6em] bg-moderateViolet"></div>
+        <div className="col-span-1 h-[17.6em] bg-veryDarkGrayishBlue"></div>
         <div className="row-span-2 bg-white"></div>
-        <div className="col-span-1 bg-white"></div>
+        <div className="col-span-1 bg-white "></div>
         <div className="col-span-2 bg-veryDarkBlackishBlue"></div>
       </div>
     </main>
