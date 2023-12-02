b90207ddfc4d2c6dafb847960d8cdae8e40f0837
VillageR88 <villager88pl@gmail.com>
2023-12-02 23:12:16 +0100

    quotation mark bg
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center font-barlowSemiCondensed">
       <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 gap-x-[1.9em] gap-y-[2.5em] ">
-        <div className="col-span-2 h-[17.6em] w-full rounded-lg bg-moderateViolet ">
-          <div className="h-full w-full bg-['url(./images/bg-pattern-quotation.svg)'] bg-cover"></div>
+        <div className="col-span-2 h-[17.6em] w-full rounded-lg bg-moderateViolet">
+          <div className="h-full w-full bg-[url('./images/bg-pattern-quotation.svg')] bg-no-repeat bg-[81.7%_top]"></div>
         </div>
         <div className="col-span-1 h-[17.6em] rounded-lg bg-veryDarkGrayishBlue shadow-2xl"></div>
         <div className="row-span-2 rounded-lg bg-white shadow-2xl"></div>
