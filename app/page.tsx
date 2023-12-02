c1414c9bc8411f0737f96c209b452c410a9e6415
VillageR88 <villager88pl@gmail.com>
2023-12-02 23:06:50 +0100

    update layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import '@fontsource/barlow-semi-condensed/600.css';
 
 export default function Home() {
   return (
-    <main className="font-barlowSemiCondensed flex min-h-screen flex-col items-center justify-center">
-      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 gap-x-[1.9em] gap-y-[2.5em]">
-        <div className="col-span-2 h-[17.6em] bg-moderateViolet"></div>
-        <div className="col-span-1 h-[17.6em] bg-veryDarkGrayishBlue"></div>
-        <div className="row-span-2 bg-white"></div>
-        <div className="col-span-1 bg-white "></div>
-        <div className="col-span-2 bg-veryDarkBlackishBlue"></div>
+    <main className="flex min-h-screen flex-col items-center justify-center font-barlowSemiCondensed">
+      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 gap-x-[1.9em] gap-y-[2.5em] ">
+        <div className="col-span-2 h-[17.6em] w-full rounded-lg bg-moderateViolet ">
+          <div className="h-full w-full bg-['url(./images/bg-pattern-quotation.svg)'] bg-cover"></div>
+        </div>
+        <div className="col-span-1 h-[17.6em] rounded-lg bg-veryDarkGrayishBlue shadow-2xl"></div>
+        <div className="row-span-2 rounded-lg bg-white shadow-2xl"></div>
+        <div className="col-span-1 rounded-lg bg-white shadow-2xl"></div>
+        <div className="col-span-2 rounded-lg bg-veryDarkBlackishBlue shadow-2xl"></div>
       </div>
     </main>
   );
