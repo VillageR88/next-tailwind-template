58053d3933cff5dbc50ef1bc77de00f3c99cd9c3
VillageR88 <villager88pl@gmail.com>
2023-12-03 22:13:46 +0100

    left wrapper buttons
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -14,21 +14,23 @@ export default function Home() {
         <div className="h-[50em] w-full bg-[url('./images/bg-desktop.png')] bg-bottom bg-no-repeat">
           <div className="flex h-full w-full items-center justify-center">
             <div className="mb-[1.22em] flex items-end gap-[1.88em]">
-              <div className="flex h-[12.58em] w-[21.88em] flex-col justify-center gap-[2.08em] rounded-l-[0.6em] rounded-br-[0.6em] rounded-tr-[6.2em] bg-darkBlue pl-[2.6em]">
+              {/*left wrapper*/}
+              <div className="flex h-[12.58em] w-[21.88em] flex-col justify-center gap-[2.08em] rounded-l-[0.6em] rounded-br-[0.6em] rounded-tr-[6.2em] bg-darkBlue pl-[2.6em] shadow-2xl">
                 <Image className="h-fit" src={logo as string} alt="logo" />
                 <div className="flex flex-row items-center gap-[1em]">
-                  <div className="rounded-[0.6em] bg-veryDarkBlue py-[0.75em] pl-[0.85em] pr-[0.87em]">
+                  <button className="rounded-[0.6em] bg-veryDarkBlue py-[0.75em] pl-[0.85em] pr-[0.87em]">
                     <Image className="h-fit" src={documentIcon as string} alt="document icon" />
-                  </div>
-                  <div className="rounded-[0.6em] bg-veryDarkBlue pb-[0.87em] pl-[0.76em] pr-[0.74em] pt-[0.88em]">
+                  </button>
+                  <button className="rounded-[0.6em] bg-veryDarkBlue pb-[0.87em] pl-[0.76em] pr-[0.74em] pt-[0.88em]">
                     <Image className="h-fit" src={folderIcon as string} alt="folder icon" />
-                  </div>
-                  <div className="rounded-[0.6em] bg-veryDarkBlue px-[0.75em] py-[1em]">
+                  </button>
+                  <button className="rounded-[0.6em] bg-veryDarkBlue px-[0.75em] py-[1em]">
                     <Image className="h-fit" src={uploadIcon as string} alt="upload icon" />
-                  </div>
+                  </button>
                 </div>
               </div>
-              <div className="flex h-[9.65em] w-[33.8em] rounded-[0.6em] bg-darkBlue"></div>
+              {/*right wrapper*/}
+              <div className="flex h-[9.65em] w-[33.8em] rounded-[0.6em] bg-darkBlue shadow-2xl"></div>
             </div>
           </div>
         </div>
