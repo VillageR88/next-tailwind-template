b2f1659ce3fb5406c8be47722f32c43575eb55e8
VillageR88 <villager88pl@gmail.com>
2023-12-03 15:35:34 +0100

    improved grid setup universality
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ enum Style {
 const StylePalette: Record<Style, string[]> = {
   [Style.light]: ['bg-white', 'text-veryDarkBlackishBlue', 'text-veryDarkGrayishBlue'],
   [Style.punk]: [],
-  [Style.medium]: [],
+  [Style.medium]: ['bg-veryDarkGrayishBlue', 'text-veryDarkBlackishBlue', 'text-veryDarkGrayishBlue'],
   [Style.dark]: [],
 };
 
@@ -63,11 +63,11 @@ const Block_1_1 = ({ style, person, header, main }: { style: Style; person: Peop
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center font-barlowSemiCondensed">
-      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-2 gap-x-[1.9em] gap-y-[2.5em] ">
-        <div className="col-span-2 h-[17.6em] w-full rounded-lg bg-moderateViolet">
+      <div className="grid h-[35.7em] w-[69.4em] grid-cols-4 grid-rows-[17.6em] gap-x-[1.9em] gap-y-[1.5em]">
+        <div className="col-span-2 w-full rounded-lg bg-moderateViolet">
           <div className="h-full w-full bg-[url('./images/bg-pattern-quotation.svg')] bg-[81.7%_top] bg-no-repeat"></div>
         </div>
-        <div className="col-span-1 h-[17.6em] rounded-lg bg-veryDarkGrayishBlue shadow-2xl"></div>
+        <div className="col-span-1 rounded-lg bg-veryDarkGrayishBlue shadow-2xl"></div>
         <div className="row-span-2 rounded-lg bg-white shadow-2xl"></div>
         <Block_1_1
           style={Style.light}
