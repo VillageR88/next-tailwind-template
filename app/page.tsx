cd6059f939040f2978adaf0f43fb65be1a7e72ed
VillageR88 <villager88pl@gmail.com>
2023-12-03 21:46:58 +0100

    center wrapper good
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,15 @@ import '@fontsource/raleway/700.css';
 export default function Home() {
   return (
     <main className="font-raleway flex min-h-screen flex-col items-center justify-center">
-      <div className='bg-veryDarkBlue w-full h-full'>
-        <div className="h-[50em] w-full bg-[url('./images/bg-desktop.png')] bg-bottom bg-no-repeat"></div>
+      <div className="h-full w-full bg-veryDarkBlue">
+        <div className="h-[50em] w-full bg-[url('./images/bg-desktop.png')] bg-bottom bg-no-repeat">
+          <div className="flex h-full w-full items-center justify-center">
+            <div className="mb-[1.22em] flex items-end gap-[1.88em]">
+              <div className="h-[12.58em] w-[21.88em] rounded-l-[0.6em] rounded-br-[0.6em] rounded-tr-[6.2em] bg-darkBlue"></div>
+              <div className="h-[9.7em] w-[33.8em] rounded-[0.6em] bg-darkBlue"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
