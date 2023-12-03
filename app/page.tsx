912cf9e5db078dcfb0b38dcda36a8ef74f7a7b2d
VillageR88 <villager88pl@gmail.com>
2023-12-03 14:16:31 +0100

    bettered
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
 import '@fontsource/barlow-semi-condensed/500.css';
 import '@fontsource/barlow-semi-condensed/600.css';
 import Image from 'next/image';
+import DanielImage from './images/image-daniel.jpg';
+import JeanetteImage from './images/image-jeanette.jpg';
+import JonathanImage from './images/image-jonathan.jpg';
+import KiraImage from './images/image-kira.jpg';
+import PatrickImage from './images/image-patrick.jpg';
+
+enum People {
+  daniel = 'Daniel Clifford',
+  jeanette = 'Jeanette Harmon',
+  jonathan = 'Jonathan Walters',
+  kira = 'Kira Whittle',
+  patrick = 'Patrick Abrams',
+}
+const PeopleImages = {
+  [People.daniel]: DanielImage,
+  [People.jeanette]: JeanetteImage,
+  [People.jonathan]: JonathanImage,
+  [People.kira]: KiraImage,
+  [People.patrick]: PatrickImage,
+};
+
+const Block_1_1 = ({ image, name, header, main }: { image: unknown; name: string; header: string; main: string }) => {
+  return (
+    <div className="col-span-1 rounded-lg bg-white shadow-2xl">
+      <div className="flex h-full w-full flex-col justify-between pb-[1.5em] pl-[2em] pr-[2.5em] pt-[1.5em]">
+        <div className="flex flex-row items-center gap-[1em]">
+          <Image className="h-fit rounded-full" width={28} height={28} src={image as string} alt="image of a person" />
+          <div className="flex flex-col">
+            <span className="text-[0.8rem] leading-[1.4em] text-veryDarkBlackishBlue">{name}</span>
+            <span className="text-[0.7rem] text-veryDarkGrayishBlue">Verified Graduate</span>
+          </div>
+        </div>
+        <span className="text-[1.2rem] leading-[1.25em] text-veryDarkBlackishBlue">{header}</span>
+        <span className="text-[0.8rem] text-veryDarkGrayishBlue">{main}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -11,30 +49,13 @@ export default function Home() {
         </div>
         <div className="col-span-1 h-[17.6em] rounded-lg bg-veryDarkGrayishBlue shadow-2xl"></div>
         <div className="row-span-2 rounded-lg bg-white shadow-2xl"></div>
-        <div className="col-span-1 rounded-lg bg-white shadow-2xl">
-          <div className="flex flex-col">
-            <div className="flex flex-row">
-              <Image
-                className="h-fit rounded-full"
-                width={28}
-                height={28}
-                src={'./images/image-jeanette.jpg'}
-                alt="image of a person"
-              />
-              <div className="flex flex-col">
-                <span className="text-[0.8rem] text-veryDarkBlackishBlue">Jeanette Harmon</span>
-                <span className="text-[0.7rem] text-veryDarkGrayishBlue">Verified Graduate</span>
-              </div>
-            </div>
-            <span className="text-[1.2rem] text-veryDarkBlackishBlue">
-              An overall wonderful and rewarding experience
-            </span>
-            <span className="text-[0.8rem] text-veryDarkGrayishBlue">
-              “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while
-              doing something I love. ”
-            </span>
-          </div>
-        </div>
+        <Block_1_1
+          image={PeopleImages[People.jeanette]}
+          name={People.jeanette}
+          header="An overall wonderful and rewarding experience"
+          main="“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing
+          something I love. ”"
+        />
         <div className="col-span-2 rounded-lg bg-veryDarkBlackishBlue shadow-2xl"></div>
       </div>
     </main>
