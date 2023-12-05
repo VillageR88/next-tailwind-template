e5a2eae85e401cf7232bc55d42aec796ee3d8694
VillageR88 <villager88pl@gmail.com>
2023-12-05 14:24:37 +0100

    bg desktop good
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import '@fontsource/inter/700.css';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center font-inter">
-      <div className='w-full h-[50em] bg-white'></div>
+      <div className="h-[50em] w-full bg-white">
+        <div className="h-full w-full bg-[url('./images/pattern-curve.svg')] bg-left-bottom bg-no-repeat">
+          <div className="h-full w-full bg-[url('./images/pattern-bg.svg')] bg-[90.2%_35%] bg-no-repeat"></div>
+        </div>
+      </div>
     </main>
   );
 }
