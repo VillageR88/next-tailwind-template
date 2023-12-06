66a19ab92f57b81d05b1e5f243e58797d1ca6b42
VillageR88 <villager88pl@gmail.com>
2023-12-06 20:52:53 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import '@fontsource/inter';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="font-inter flex h-screen flex-col items-center justify-center">
       <p className="text-lg">Hello</p>
       <p className="font-mono">World 🌍</p>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,34 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        inter: 'Inter',
       },
       colors: {
-        // Slate
-        // 'accent-brand-800': '#1e293b',
-        // 'accent-brand-700': '#334155',
-        // 'accent-brand-600': '#475569',
+        //## Colors
+        //### Primary
+        limeGreen: 'hsl(163, 72%, 41%)',
+        brightRed: 'hsl(356, 69%, 56%)',
+        facebook: 'hsl(208, 92%, 53%)',
+        tTwitter: 'hsl(203, 89%, 53%)',
+        instagram: 'linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)',
+        youTube: 'hsl(348, 97%, 39%)',
+        //#### Dark Theme
+        toggleDark: 'linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)',
+        //#### Light Theme
+        toggleLight: 'hsl(230, 22%, 74%)',
+        //### Neutral
+        //#### Dark Theme
+        veryDarkBlue_BG: 'hsl(230, 17%, 14%)',
+        veryDarkBlue_Top_BG_Pattern: 'hsl(232, 19%, 15%)',
+        darkDesaturatedBlue_Card_BG: 'hsl(228, 28%, 20%)',
+        desaturatedBlue_Text: 'hsl(228, 34%, 66%)',
+        whiteText: 'hsl(0, 0%, 100%)',
+        //#### Light Theme
+        white_BG: 'hsl(0, 0%, 100%)',
+        veryPaleBlue_Top_BG_Pattern: 'hsl(225, 100%, 98%)',
+        lightGrayishBlue_Card_BG: 'hsl(227, 47%, 96%)',
+        darkGrayishBlue_Text: 'hsl(228, 12%, 44%)',
+        veryDarkBlue_Text: 'hsl(230, 17%, 14%)',
       },
     },
   },
