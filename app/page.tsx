28de367c52790674b5600cdf2a6ea229857234ff
VillageR88 <villager88pl@gmail.com>
2023-12-06 21:45:16 +0100

    left nav wrapper
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import '@fontsource/inter/700.css';
 
 export default function Home() {
   return (
-    <main className="font-inter flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
-    </main>
+    <div className="flex min-h-screen w-full items-center font-inter">
+      <div className="h-[50em] w-full bg-white">
+        <nav className="h-[15.3em] w-full rounded-b-[1.3em] bg-veryPaleBlue_Top_BG_Pattern">
+          <div className="flex flex-row justify-between px-[10.1em] pt-[2.3em]">
+            <div className="flex flex-col">
+              <span className="text-[1.75rem] leading-[1.25em] font-[700]">Social Media Dashboard</span>
+              <span className='text-[0.9rem] font-[700]'>Total Followers: 23,004</span>
+            </div>
+            <div className="flex flex-row">
+              <span>Dark Mode</span>
+              <div>DIV</div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
