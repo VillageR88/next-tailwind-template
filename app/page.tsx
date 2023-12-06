936eba74dceb42b17fd8a423d29ca800593e8686
VillageR88 <villager88pl@gmail.com>
2023-12-06 21:58:52 +0100

    top nav good
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ export default function Home() {
     <div className="flex min-h-screen w-full items-center font-inter">
       <div className="h-[50em] w-full bg-white">
         <nav className="h-[15.3em] w-full rounded-b-[1.3em] bg-veryPaleBlue_Top_BG_Pattern">
-          <div className="flex flex-row justify-between px-[10.1em] pt-[2.3em]">
+          <div className="flex flex-row items-center justify-between px-[10.1em] pt-[2.3em]">
             <div className="flex flex-col">
-              <span className="text-[1.75rem] leading-[1.25em] font-[700]">Social Media Dashboard</span>
-              <span className='text-[0.9rem] font-[700]'>Total Followers: 23,004</span>
+              <span className="text-[1.75rem] font-[700] leading-[1.25em]">Social Media Dashboard</span>
+              <span className="text-[0.9rem] font-[700] text-darkGrayishBlue_Text">Total Followers: 23,004</span>
             </div>
-            <div className="flex flex-row">
-              <span>Dark Mode</span>
-              <div>DIV</div>
+            <div className="flex flex-row gap-[0.88em] pb-[0.5em] pr-[0.2em]">
+              <span className="pt-[0.2em] text-[0.87rem] font-[700] text-[#8F93AD]">Dark Mode</span>
+              <button className="flex w-[3em] items-center justify-end rounded-[2em] bg-toggleLight py-[0.75em] px-[0.2em]">
+                <div className="absolute h-[1.12em] w-[1.12em] rounded-full bg-[#F2F3F8]"></div>
+              </button>
             </div>
           </div>
         </nav>
