1538cfe788107323b47cfdf6e38d71c29bfbb5a3
VillageR88 <villager88pl@gmail.com>
2023-12-07 01:04:42 +0100

    BigBox initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,21 @@ import '@fontsource/inter';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
 
+const BigBox = () => {
+  return (
+    <div className="flex h-[13.5em] w-[15.95em] flex-col">
+      <div
+        style={{
+          borderBottom: '4px solid hsl(208 92% 53%)',
+          borderLeft: '4px solid transparent',
+          borderRight: '4px solid transparent',
+        }}
+      ></div>
+      <div className="h-full w-full rounded-b-[0.3em] bg-[#F0F3FA]"></div>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full items-center font-inter">
@@ -14,13 +29,26 @@ export default function Home() {
             </div>
             <div className="flex flex-row gap-[0.88em] pb-[0.5em] pr-[0.2em]">
               <span className="pt-[0.2em] text-[0.87rem] font-[700] text-[#8F93AD]">Dark Mode</span>
-              <button className="flex w-[3em] items-center justify-end rounded-[2em] bg-toggleLight py-[0.75em] px-[0.2em]">
+              <button className="flex w-[3em] items-center justify-end rounded-[2em] bg-toggleLight px-[0.2em] py-[0.75em]">
                 <div className="absolute h-[1.12em] w-[1.12em] rounded-full bg-[#F2F3F8]"></div>
               </button>
             </div>
           </div>
         </nav>
+        <main className="h-fit">
+          <div className="mt-[-6.8em] h-full w-full gap-[2em] px-[10.3em]">
+            <BigBox />
+          </div>
+        </main>
       </div>
     </div>
   );
 }
+/*
+      <div
+                style={{
+                  borderBottom: '4px solid',
+                  borderLeft: '4px solid transparent',
+                  borderRight: '4px solid transparent',
+                }}
+              ></div>*/
