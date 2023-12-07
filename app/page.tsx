3ab116c98c900cc54fbe96d153c8a65a74eb9ce5
VillageR88 <villager88pl@gmail.com>
2023-12-07 17:20:28 +0100

    small box initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -135,13 +135,29 @@ const BigBox = ({
     </div>
   );
 };
-fetch;
+
+const SmallBox = () => {
+  const [data, setData] = useState<dataJSON>();
+  useEffect(() => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((response: dataJSON) => {
+        setData(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return <div className="h-[7.8em] max-w-[15.95em] rounded-[0.3em] bg-[#F0F3FA]"></div>;
+};
+
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full items-center font-inter">
       <div className="h-[50em] w-full bg-white">
         <nav className="h-[15.3em] w-full rounded-b-[1.3em] bg-veryPaleBlue_Top_BG_Pattern">
-          <div className="flex flex-row items-center justify-between lg:px-[5em] xl:px-[10.1em] pt-[2.3em]">
+          <div className="flex flex-row items-center justify-between pt-[2.3em] md:px-[3em] lg:px-[5em] xl:px-[10.1em]">
             <div className="flex flex-col">
               <span className="text-[1.75rem] font-[700] leading-[1.25em]">Social Media Dashboard</span>
               <span className="text-[0.9rem] font-[700] text-darkGrayishBlue_Text">Total Followers: 23,004</span>
@@ -155,8 +171,8 @@ export default function Home() {
           </div>
         </nav>
         <main className="h-fit w-full">
-          <div className="mt-[-6.8em] flex h-full w-full flex-row lg:px-[5em] xl:px-[10.3em]">
-            <div className="grid w-full gap-[1.9em] md:grid-cols-2 lg:grid-cols-4">
+          <div className="mt-[-6.8em] flex h-full w-full flex-col md:px-[3em] lg:px-[5em] xl:px-[10.3em]">
+            <div className="grid w-full gap-[1.9em] md:grid-cols-3 lg:grid-cols-4">
               <BigBox social={Social.facebook} top="hsl(208 92% 53%)" />
               <BigBox social={Social.twitter} top="hsl(203, 89%, 53%)" />
               <BigBox
@@ -164,6 +180,19 @@ export default function Home() {
                 top="linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))"
               />
               <BigBox social={Social.youtube} top="hsl(348, 97%, 39%)" textSubscribers={true} />
+            </div>
+            <span className="mt-[1.8em] pb-[0.85em] text-[1.5rem] font-[700] text-darkGrayishBlue_Text">
+              Overview - Today
+            </span>
+            <div className="grid grid-cols-4 gap-x-[1.9em] gap-y-[1.5em]">
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
+              <SmallBox />
             </div>
           </div>
         </main>
