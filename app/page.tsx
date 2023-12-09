079b230f15bca83d4d79b8a641c6caf299a8c7c3
VillageR88 <villager88pl@gmail.com>
2023-12-09 22:38:03 +0100

    background waterfalls
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import '@fontsource/montserrat/700.css';
 
 export default function Home() {
   return (
-    <main className="font-montserrat flex h-screen flex-col items-center justify-center">
-      <div className="h-[53.125em] w-full bg-veryLightGrayishBlue"></div>
+    <main className="flex h-screen flex-col items-center justify-center font-montserrat">
+      <div className="h-[53.125em] w-full bg-veryLightGrayishBlue">
+        <div className="h-full w-full bg-[url('./images/bg-top.svg')] bg-right-top bg-no-repeat ">
+          {' '}
+          <div className="h-full w-full bg-[url('./images/bg-bottom.svg')] bg-left-bottom bg-no-repeat "></div>
+        </div>
+      </div>
     </main>
   );
 }
