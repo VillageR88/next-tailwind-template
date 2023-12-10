9e19d06d5dd5a0fe1ae98b96f2e1180eda7ff5a0
VillageR88 <villager88pl@gmail.com>
2023-12-10 20:33:39 +0100

    layout improvements desktop
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,9 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <nav className="flex-flex-row h-[26.7em] w-full">
         <div className="h-0">
-          <div className="flex items-center justify-between px-[10.3em] pt-[4em] font-barlowCondensed">
+          <div className="flex items-center justify-between pt-[4em] font-barlowCondensed md:px-[3em] lg:px-[5em] xl:px-[10.3em]">
             <Image className="z-10 h-full" src={logo as string} alt="logo" />
-            <div className="flex items-center gap-[2.45em]">
+            <div className="hidden items-center gap-[2.45em] md:flex">
               <CustomButton1 text="PRODUCT" />
               <CustomButton1 text="FEATURES" />
               <CustomButton1 text="PRICING" />
@@ -41,34 +41,33 @@ export default function Home() {
           </div>
         </div>
         <div className="flex h-full w-full justify-end bg-white">
-          <div className=" h-[full] w-[44.05em] rounded-bl-[3.8em] bg-lightGrayishBlue"></div>
+          <div className=" h-[full] rounded-bl-[3.8em] bg-lightGrayishBlue md:w-[60%] xl:w-[44.05em]"></div>
         </div>
       </nav>
       <main className="h-[23.301em] w-full bg-white">
         <div className="mt-[-13.5em] flex justify-between">
-          <div>
-            <div className="ml-[10.3em] mt-[3em] flex flex-col font-barlowCondensed">
-              <div className="flex gap-[1em]">
-                <span className="flex h-fit w-[3.3em] justify-center rounded-[1em] bg-veryDarkBlue py-0.5 text-[0.9rem] font-[700] tracking-[0.1em] text-white">
-                  NEW
-                </span>
-                <span className="tracking-[0.28em] text-grayishBlue">MONOGRAPH DASHBOARD</span>
-              </div>
-              <span className="mt-[0.3em] font-barlowCondensed text-[4rem] font-[700] leading-[1em] text-veryDarkBlue">
-                POWERFUL INSIGHTS INTO YOUR TEAM
+          {/***/}
+          <div className="mt-[3em] flex flex-col font-barlowCondensed md:ml-[1em] lg:ml-[5em] xl:ml-[10.3em]">
+            <div className="flex gap-[1em]">
+              <span className="flex h-fit w-[3.3em] justify-center rounded-[1em] bg-veryDarkBlue py-0.5 text-[0.9rem] font-[700] tracking-[0.1em] text-white">
+                NEW
               </span>
-              <span className="mt-[1.6em] max-w-[15em] font-barlow text-[1.15rem] leading-[1.4em] tracking-[-0.01em] text-darkGrayishBlue">
-                Project planning and time tracking for agile teams
-              </span>
-              <div className="mt-[4em] flex items-center gap-[1.4em]">
-                <button className="rounded-[0.3em] bg-[#FF5D5E] px-8 py-[0.7em] font-barlowCondensed text-[1.03rem] font-[700] tracking-[0.06em] text-lightGrayishBlue hover:bg-[#FF8584]">
-                  SCHEDULE A DEMO
-                </button>
-                <span className="tracking-[0.3em] text-grayishBlue">TO SEE A PREVIEW</span>
-              </div>
+              <span className="tracking-[0.28em] text-grayishBlue">MONOGRAPH DASHBOARD</span>
+            </div>
+            <span className="mt-[0.3em] font-barlowCondensed text-[4rem] font-[700] leading-[1em] text-veryDarkBlue">
+              POWERFUL INSIGHTS INTO YOUR TEAM
+            </span>
+            <span className="mt-[1.6em] max-w-[15em] font-barlow text-[1.15rem] leading-[1.4em] tracking-[-0.01em] text-darkGrayishBlue">
+              Project planning and time tracking for agile teams
+            </span>
+            <div className="mt-[4em] flex xl:flex-row flex-col items-start xl:items-center gap-[0.5em] xl:gap-[1.4em]">
+              <button className="rounded-[0.3em] bg-[#FF5D5E] px-8 py-[0.7em] font-barlowCondensed text-[1.03rem] font-[700] tracking-[0.06em] text-lightGrayishBlue hover:bg-[#FF8584]">
+                SCHEDULE A DEMO
+              </button>
+              <span className="tracking-[0.3em] text-grayishBlue">TO SEE A PREVIEW</span>
             </div>
           </div>
-          <Image className="mr-[-16.75em]" src={illustration as string} alt="image of a computer" />
+          <Image className="mr-[-16.75em] h-fit w-fit" src={illustration as string} alt="image of a computer" />
         </div>
       </main>
     </div>
