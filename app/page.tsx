24074d36074973033d82a553b0d852f2c2c1852f
VillageR88 <villager88pl@gmail.com>
2023-12-10 18:35:32 +0100

    bg set
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,13 @@ import '@fontsource/barlow/400.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className='w-full h-[50em] bg-white'></div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <nav className="flex-flex-row h-[26.7em] w-full">
+        <div className="flex h-full w-full justify-end bg-white">
+          <div className="bg-lightGrayishBlue h-[full] rounded-bl-[3.8em] w-[44.05em]"></div>
+        </div>
+      </nav>
+      <main className="h-[23.3em] w-full bg-white"></main>
+    </div>
   );
 }
