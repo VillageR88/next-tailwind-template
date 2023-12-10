e4151fd83af5db97efa2c588cac430a5992f6a13
VillageR88 <villager88pl@gmail.com>
2023-12-10 18:53:16 +0100

    initial nav row left
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,26 @@ import '@fontsource/barlow-condensed/400.css';
 import '@fontsource/barlow-condensed/700.css';
 import '@fontsource/barlow';
 import '@fontsource/barlow/400.css';
+import Image from 'next/image';
+import logo from './images/logo.svg';
+import navMobileOpen from './images/icon-hamburger.svg';
+import navMobileClose from './images/icon-close.svg';
+import illustration from './images/illustration-devices.svg';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <nav className="flex-flex-row h-[26.7em] w-full">
+        <div className="h-0">
+          <div className="h-0 px-[10.3em] pt-[4em]">
+            <Image className="h-fit" src={logo as string} alt="logo" />
+          </div>
+        </div>
         <div className="flex h-full w-full justify-end bg-white">
-          <div className="bg-lightGrayishBlue h-[full] rounded-bl-[3.8em] w-[44.05em]"></div>
+          <div className="h-[full] w-[44.05em] rounded-bl-[3.8em] bg-lightGrayishBlue"></div>
         </div>
       </nav>
-      <main className="h-[23.3em] w-full bg-white"></main>
+      <main className="h-[23.301em] w-full bg-white"></main>
     </div>
   );
 }
