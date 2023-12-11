23c4f71c28a53f2f749b47e9ea1770a590e0749d
VillageR88 <villager88pl@gmail.com>
2023-12-11 12:32:36 +0100

    next line and layout improvements
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,17 @@ export default function Home() {
   return (
     <div className="font-baiJamjuree flex min-h-screen flex-col items-center justify-center">
       <main className="h-full w-full bg-white">
-        <div className="flex h-[140em] w-full flex-col items-center bg-[url('./images/bg-header-desktop.png')] bg-[0_0.2em] bg-no-repeat">
-          <Image className="mt-[8.2em] h-fit" src={logo as string} alt="logo" />
-          <span className="mt-[1.15em] text-[2.8rem] tracking-[-0.005em] font-[600] text-darkGrayishBlue">A history of everything you copy</span>
+        <div className="flex h-[140em] w-full justify-center bg-[url('./images/bg-header-desktop.png')] bg-[0_0.2em] bg-no-repeat">
+          <div className="flex h-full w-[43em] flex-col items-center text-center">
+            <Image className="mt-[8.2em] h-fit" src={logo as string} alt="logo" />
+            <span className="mt-[1.15em] text-[2.8rem] font-[600] tracking-[-0.005em] text-darkGrayishBlue">
+              A history of everything you copy
+            </span>
+            <span className="mt-[0.55em] text-[1.25rem] tracking-[0.008em] text-grayishBlue">
+              Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all
+              your devices.
+            </span>
+          </div>
         </div>
       </main>
     </div>
