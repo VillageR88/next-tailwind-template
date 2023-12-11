13170c55a712cf38689900aa35671d51ae308c0b
VillageR88 <villager88pl@gmail.com>
2023-12-11 13:10:41 +0100

    from top to first buttons
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,29 @@ import '@fontsource/bai-jamjuree/600.css';
 import Image from 'next/image';
 import logo from './images/logo.svg';
 
+enum CustomButtonType {
+  iOS,
+  mac,
+}
+
+const CustomButton = ({ type }: { type: CustomButtonType }) => {
+  return (
+    <button
+      className={`${
+        type === CustomButtonType.iOS ? 'bg-[#2F9681] shadow-[#D5F7E9]' : 'bg-[#5964BE]'
+      } flex h-[3.5em] w-[14.15em] items-start rounded-[3em] shadow-xl`}
+    >
+      <div
+        className={`${
+          type === CustomButtonType.iOS ? 'bg-[#26BBA5]' : 'bg-[#6174FF]'
+        } flex h-[95%] w-full items-center justify-center rounded-[3em] pt-[0.2em] text-[1.1rem] font-[600] tracking-[0.05em] text-white`}
+      >
+        {type === CustomButtonType.iOS ? 'Download for iOS' : 'Download for Mac'}
+      </div>
+    </button>
+  );
+};
+
 export default function Home() {
   return (
     <div className="font-baiJamjuree flex min-h-screen flex-col items-center justify-center">
@@ -18,6 +41,10 @@ export default function Home() {
               Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all
               your devices.
             </span>
+            <div className="mt-[3em] flex gap-[1em]">
+              <CustomButton type={CustomButtonType.iOS} />
+              <CustomButton type={CustomButtonType.mac} />
+            </div>
           </div>
         </div>
       </main>
