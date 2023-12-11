c96cb88f4096a72b11fae2e303d406ec9a47d2f9
VillageR88 <villager88pl@gmail.com>
2023-12-11 12:26:46 +0100

    A history of everything you copy text
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ import logo from './images/logo.svg';
 export default function Home() {
   return (
     <div className="font-baiJamjuree flex min-h-screen flex-col items-center justify-center">
-      <div className="h-full w-full bg-white">
-        <div className="flex h-[140em] w-full justify-center bg-[url('./images/bg-header-desktop.png')] bg-[0_0.2em] bg-no-repeat">
+      <main className="h-full w-full bg-white">
+        <div className="flex h-[140em] w-full flex-col items-center bg-[url('./images/bg-header-desktop.png')] bg-[0_0.2em] bg-no-repeat">
           <Image className="mt-[8.2em] h-fit" src={logo as string} alt="logo" />
+          <span className="mt-[1.15em] text-[2.8rem] tracking-[-0.005em] font-[600] text-darkGrayishBlue">A history of everything you copy</span>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
