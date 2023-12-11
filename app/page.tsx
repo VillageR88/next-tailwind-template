c8f8da9baa3018f30b1c4d96a3e8e66227dae4a3
VillageR88 <villager88pl@gmail.com>
2023-12-11 20:43:37 +0100

    first icons row
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -50,12 +50,26 @@ const Article2 = ({ header, main }: { header: string; main: string }) => {
   );
 };
 
-const Article3 = ({ image, header, main }: { image: string; header: string; main: string }) => {
+const Article3 = ({
+  image,
+  header,
+  main,
+  optionalMt,
+  optionalMt2,
+}: {
+  image: string;
+  header: string;
+  main: string;
+  optionalMt?: string;
+  optionalMt2?: string;
+}) => {
   return (
-    <div className='flex flex-col items-center'>
-      <Image src={image} alt="icon" />
-      <span className="text-[1.45rem] font-[600] leading-[1.4em] text-darkGrayishBlue">{header}</span>
-      <span className="mt-[0.5em] text-[1rem] leading-[1.85em] text-grayishBlue">{main}</span>
+    <div className="flex w-[22em] flex-col items-center text-center">
+      <Image className={`${optionalMt ? optionalMt : 'mt-[1.85em]'} h-fit`} src={image} alt="icon" />
+      <span className={`${optionalMt2 ? optionalMt2 : 'mt-[1.85em]'} text-[1.45rem] font-[600] text-darkGrayishBlue`}>
+        {header}
+      </span>
+      <span className="mt-[0.8em] text-[1rem] leading-[1.85em] tracking-[0.01em] text-grayishBlue">{main}</span>
     </div>
   );
 };
@@ -109,13 +123,26 @@ export default function Home() {
             main="We’ve got the tools to boost your productivity."
             mt={'mt-[2em]'}
           />
-          <div className="mt-[4em] flex">
+          <div className="mt-[2.8em] flex gap-[1.8em]">
+            <Article3
+              image={iconBlacklist as string}
+              header="Create blacklists"
+              main="Ensure sensitive information never makes its way to your clipboard by excluding certain sources."
+              optionalMt="mt-[1.55em]"
+              optionalMt2="mt-[1.68em]"
+            />
             <Article3
               image={iconText as string}
               header="Plain text snippets"
               main="Remove unwanted formatting from copied text for a consistent look."
             />
+            <Article3
+              image={iconPreview as string}
+              header="Sneak preview"
+              main="Quick preview of all snippets on your Clipboard for easy access."
+            />
           </div>
+          <div className="mt-[15em]">s</div>
         </div>
       </main>
     </div>
