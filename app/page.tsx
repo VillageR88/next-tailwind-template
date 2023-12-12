eff74c4118fcb83d879517264b7f5eb132b51f1f
VillageR88 <villager88pl@gmail.com>
2023-12-12 10:45:56 +0100

    first line ok
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="h-[45.06em] w-full bg-veryPaleCyan">
-        <div className="h-full md:px-[5em] w-full bg-[url('./images/bg-hero-desktop.svg')]">
-          <div className="mt-[3.55em]">
+        <div className="h-full w-full bg-[url('./images/bg-hero-desktop.svg')] md:px-[5em]">
+          <div className="mt-[3em] flex items-center justify-between">
             <Image className="h-[1.95em] w-[12.5em]" src={logo as string} alt="logo" />
+            <button className="h-[3.3em] w-[13.8em] rounded-[2em] bg-white font-openSans text-[0.9rem] font-[700] shadow-[0_4px_30px_-13px_rgba(0,0,0,0.3)]">
+              Try It Free
+            </button>
           </div>
         </div>
       </div>
