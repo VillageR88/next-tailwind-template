3da5d1108ab3d362c6d3afb889033434b6b03d66
VillageR88 <villager88pl@gmail.com>
2023-12-12 21:46:53 +0100

    initial central mobile
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,7 @@ const LongBox1 = ({
   return (
     <div
       className={`${
-        reversed ? 'flex-row-reverse' : 'flex-row'
+        reversed ? 'flex-col-reverse md:flex-row-reverse' : 'flex-col-reverse md:flex-row'
       } flex h-[27.5em] w-full items-center justify-between gap-6 rounded-[1em] shadow-[0_1px_15px_-5px_rgba(0,0,0,0.2)]`}
     >
       <div
@@ -192,7 +192,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <main className="h-full w-full">
-        <div className="flex h-[45.06em] w-full bg-veryPaleCyan">
+        <div className="flex h-[45.06em] w-full flex-row bg-veryPaleCyan">
           <div className="h-full w-full bg-[url('./images/bg-hero-desktop.svg')] pb-[3em] pt-[3em] md:px-[2em] lg:px-[3em] xl:px-[5em]">
             <div className=" flex items-center justify-between px-4 md:px-0">
               <Image className="h-[1.3em] w-[8em] md:h-[1.95em] md:w-[12.5em]" src={logo as string} alt="logo" />
@@ -200,12 +200,12 @@ export default function Home() {
                 Try It Free
               </button>
             </div>
-            <div className="flex h-full w-full items-center justify-between">
-              <div className="mt-[0.9em] flex w-[32em] flex-col items-start gap-[1.7em]">
-                <span className="font-poppins font-[600] md:text-[1.8rem] lg:text-[2rem] xl:text-[2.48rem]">
+            <div className="flex h-full w-full flex-col items-center justify-between md:flex-row">
+              <div className="mt-[0.9em] flex flex-col items-center gap-[1.7em] text-center md:w-[32em] md:items-start md:text-left">
+                <span className="font-poppins text-[1.8rem] font-[600] md:text-[1.8rem] lg:text-[2rem] xl:text-[2.48rem]">
                   Build The Community Your Fans Will Love
                 </span>
-                <div className="flex flex-col items-start gap-[2em]">
+                <div className="flex flex-col items-center gap-[2em] md:items-start">
                   <span className="font-openSans md:text-[0.95rem] lg:text-[1rem] xl:text-[1.125rem]">
                     Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create
                     connections with your users as you engage in genuine discussion.
@@ -215,7 +215,11 @@ export default function Home() {
                   </button>
                 </div>
               </div>
-              <Image className="mr-[0.2em] h-fit w-[54.2%]" src={illustrationMockups as string} alt="computer image" />
+              <Image
+                className="h-fit md:mr-[0.2em] md:w-[54.2%]"
+                src={illustrationMockups as string}
+                alt="computer image"
+              />
             </div>
           </div>
         </div>
