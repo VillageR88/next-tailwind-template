6abb282139af4f60a27d005b5fa2b815c0b3f267
VillageR88 <villager88pl@gmail.com>
2023-12-13 12:28:14 +0100

    next two spans
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,17 @@ export default function Home() {
               ))}
             </div>
           </nav>
-          <div className="mt-[4.7em] flex h-full w-full justify-center">
+          <div className="mt-[4.7em] flex h-full w-full flex-col items-center">
             <Image className="h-fit" src={intro} alt="image of the people" />
+            <div className="mt-[2.2em] flex h-full w-full flex-col items-center text-center">
+              <span className="font-raleway w-[20em] text-[2.52rem] font-[700] text-white">
+                All your files in one secure location, accessible anywhere.
+              </span>
+              <span className="font-openSans mt-[1.65em] w-[30em] text-[1.25rem] text-[#FBFFFF]">
+                Fylo stores all your most important files in one secure location. Access them wherever you need, share
+                and collaborate with friends family, and co-workers.
+              </span>
+            </div>
           </div>
         </div>
         <div className="h-[81em] w-full bg-[#181E2A]"></div>
