801df3926ca18eecc80171cf58dd46ac87b17b2c
VillageR88 <villager88pl@gmail.com>
2023-12-13 11:52:25 +0100

    nav desktop
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,15 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="h-full w-full bg-[#1C2230]">
         <div className="h-[81em] w-full bg-[url('./images/bg-curvy-desktop.svg')] bg-bottom bg-no-repeat">
-          <nav className="mt-[4.55em] flex w-full flex-row pl-[5em]">
-            <Image className='h-fit' src={logo as string} alt="logo" />
+          <nav className="mt-[4.55em] flex w-full flex-row items-center justify-between px-[5em]">
+            <Image className="h-fit" src={logo as string} alt="logo" />
+            <div className="flex gap-[3.7em]">
+              {['Features', 'Team', 'Sign In'].map((x, i) => (
+                <button className="font-raleway text-[0.97rem] text-[#BFC6CE]" key={i}>
+                  {x}
+                </button>
+              ))}
+            </div>
           </nav>
         </div>
         <div className="h-[81em] w-full bg-[#181E2A]"></div>
