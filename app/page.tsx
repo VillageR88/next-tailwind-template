3c303a45402967ee9e4e6b7b26ffedfbc857530c
VillageR88 <villager88pl@gmail.com>
2023-12-13 14:51:56 +0100

    initial four times component central
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,36 @@ import '@fontsource/open-sans/700.css';
 import Image from 'next/image';
 import logo from './images/logo.svg';
 import intro from './images/illustration-intro.png';
+import iconAccessAnywhere from './images/icon-access-anywhere.svg';
+import iconSecurity from './images/icon-security.svg';
+import iconCollaboration from './images/icon-collaboration.svg';
+import iconAnyFile from './images/icon-any-file.svg';
+
+const Article1 = ({
+  width,
+  image,
+  alt,
+  header,
+  main,
+}: {
+  width: string;
+  image: string;
+  alt: string;
+  header: string;
+  main: string;
+}) => {
+  return (
+    <div className="flex justify-center">
+      <div className={`flex ${width} flex-col items-center text-center`}>
+        <div className="flex h-[5.5em] justify-center">
+          <Image className="h-fit" src={image} alt={alt} />
+        </div>
+        <span className="font-raleway mt-[1em] text-[1.2rem] font-[700] text-white">{header}</span>
+        <span className="font-openSans mt-[0.3em] text-[0.9rem] text-[#BFC6CE]">{main}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -33,13 +63,48 @@ export default function Home() {
                 Fylo stores all your most important files in one secure location. Access them wherever you need, share
                 and collaborate with friends family, and co-workers.
               </span>
-              <button className="mt-[2em] from-cyanInside_call_to_action_gradient to-blueInside_call_to_action_gradient rounded-[2em] bg-gradient-to-r px-[6em] py-[1em] text-white">
+              <button className="from-cyanInside_call_to_action_gradient font-raleway to-blueInside_call_to_action_gradient mt-[2em] rounded-[2em] bg-gradient-to-r px-[5.95em] py-[1em] font-[700] text-white">
                 Get Started
               </button>
             </div>
           </div>
         </div>
-        <div className="h-[81em] w-full bg-[#181E2A]"></div>
+        <div className="h-s[81em] mt-[-2em] w-full bg-[#181E2A]">
+          <div className="flex w-full justify-center">
+            <div className="grid grid-cols-2 gap-x-[7em] gap-y-20">
+              <Article1
+                width="w-[22em]"
+                header="Access your files, anywhere"
+                main="The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
+                image={iconAccessAnywhere as string}
+                alt="image of computer"
+              />
+              <Article1
+                width="w-[25em]"
+                header="Security you can trust"
+                main="2-factor authentication and user-controlled encryption are just a couple of the security 
+  features we allow to help secure your files."
+                image={iconSecurity as string}
+                alt="image of shield"
+              />
+              <Article1
+                width="w-[25em]"
+                header="Real-time collaboration"
+                main="Securely share files and folders with friends, family and colleagues for live collaboration. 
+  No email attachments required."
+                image={iconCollaboration as string}
+                alt="image of people together"
+              />
+              <Article1
+                width="w-[22em]"
+                header="Store any type of file"
+                main="Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+                image={iconAnyFile as string}
+                alt="image of folder"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
