2edc3a761bad2d3db1dd7f7bd257aa808c8f991f
VillageR88 <villager88pl@gmail.com>
2023-12-15 00:11:11 +0100

    before bed commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,29 @@ import '@fontsource/open-sans/700.css';
 import Image from 'next/image';
 import logo from './images/logo.svg';
 import imageMockup from './images/screen-mockups.svg';
+import imageCommunities from './images/icon-communities.svg';
+import imageMessages from './images/icon-messages.svg';
+import illustrationGrowTogether from './images/illustration-grow-together.svg';
+
+const SmallBlock1 = ({
+  image,
+  scale,
+  text1,
+  text2,
+}: {
+  image: string;
+  scale?: string;
+  text1: string;
+  text2: string;
+}) => {
+  return (
+    <div className="flex flex-col text-center">
+      <Image className={`${scale} mb-[-0.5em] h-fit w-fit pl-[0.3em]`} src={image} alt="image of communities" />
+      <span className="font-openSans text-[6rem] font-[700] text-veryDarkCyan">{text1}</span>
+      <span className="font-openSans text-[1.5rem] text-[#979EA8]">{text2}</span>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -17,7 +40,7 @@ export default function Home() {
           Try It Free
         </button>
       </nav>
-      <main className="h-[200em] w-full bg-white">
+      <main className="h-full w-full bg-white">
         <div className="mt-[9.55em] flex w-full flex-col items-center">
           <span className="text-center font-poppins text-[2.99rem] font-[700] text-veryDarkCyan drop-shadow-md">
             Build The Community Your Fans Will Love
@@ -30,7 +53,25 @@ export default function Home() {
             Get Started For Free
           </button>
           <Image className="mt-[6.86em] h-fit" src={imageMockup as string} alt="image of computer" />
+          <div className="mt-[7.45em] flex w-full flex-row justify-between lg:px-[10em] xl:px-[17.5em]">
+            <SmallBlock1 image={imageCommunities as string} text1="1.4k+" text2="Communities Formed" />
+            <SmallBlock1 scale="scale-[84%]" image={imageMessages as string} text1="2.7m+" text2="Messages Sent" />
+          </div>
         </div>
+        <div className="mt-[8.4em] h-[10em] w-full bg-[url('./images/bg-section-top-desktop-1.svg')]"></div>
+        <div className="h-[33em] w-full bg-[#F6FBFF]">
+          <div className="flex h-full w-full items-center justify-center px-[10em]">
+            <div className="w-1/2">
+              <span>assadsa</span>
+            </div>
+            <Image
+              className="h-fit w-1/2 items-center"
+              src={illustrationGrowTogether as string}
+              alt="image of two people"
+            />
+          </div>
+        </div>
+        <div className="h-[10em] w-full bg-[url('./images/bg-section-bottom-desktop-1.svg')]"></div>
       </main>
     </div>
   );
