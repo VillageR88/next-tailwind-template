142b8b6697e3ca03072e0882535d54c85a7107ec
VillageR88 <villager88pl@gmail.com>
2023-12-14 21:49:08 +0100

    improvements
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,17 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <nav className="flex h-[12.2em] w-full items-center justify-between bg-white pl-[5em] pr-[4em]">
         <Image src={logo as string} alt="logo" />
-        <button className="rounded-[2em] px-7 py-[0.45em] text-lightPink shadow-[0_0px_5px_3px_rgba(0,0,0,0.1)] shadow-[#FBE5FC] outline outline-2 hover:opacity-50">
+        <button className="rounded-[2em] px-[1.7em] py-[0.42em] font-openSans font-[600] text-lightPink shadow-[0_0px_5px_3px_rgba(0,0,0,0.1)] shadow-[#FBE5FC] outline outline-2 hover:opacity-50">
           Try It Free
         </button>
       </nav>
-      <div className="h-[100em] w-full bg-slate-50"></div>
+      <main className="h-[100em] w-full bg-white">
+        <div className="mt-[9.55em] flex w-full flex-col items-center">
+          <span className="font-poppins text-[2.99rem] font-[700] text-veryDarkCyan">
+            Build The Community Your Fans Will Love
+          </span>
+        </div>
+      </main>
     </div>
   );
 }
