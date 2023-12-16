9ac435bc30a71b996fec4e3524e2e7891ff32095
VillageR88 <villager88pl@gmail.com>
2023-12-16 13:28:01 +0100

    improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
+  const [clicked, setClicked] = useState<number>(0);
   const buttons = [];
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   for (let i = 1; i <= 100; i++) {
-    buttons.push(<button className="h-10 w-10 outline outline-1 hover:bg-slate-300" key={i}></button>);
+    buttons.push(
+      <button
+        onClick={() => {
+          setClicked(i);
+        }}
+        className="h-10 w-10 outline outline-1 hover:bg-slate-300"
+        key={i}
+      ></button>,
+    );
   }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
+      <div>
+        <span>debug selected number: {clicked}</span>
+      </div>
       <div className="flex flex-row">
         <div className="flex flex-col">
           <div className="h-10 w-10"></div>
           <div className="flex flex-col">
             {letters.map((_, i) => (
-              <button className="h-10 w-10" key={i + 1}>
+              <button disabled className="h-10 w-10" key={i + 1}>
                 {i + 1}
               </button>
             ))}
@@ -21,7 +36,7 @@ export default function Home() {
         <div>
           <div className="grid grid-cols-10">
             {letters.map((x, i) => (
-              <button className="h-10 w-10" key={i}>
+              <button disabled className="h-10 w-10" key={i}>
                 {x}
               </button>
             ))}
