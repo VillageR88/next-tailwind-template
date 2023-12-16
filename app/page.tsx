b2f9c7244af959f3e806846ad03bbff1e23c6aa7
VillageR88 <villager88pl@gmail.com>
2023-12-16 12:13:49 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
+  const buttons = [];
+
+  for (let i = 1; i <= 100; i++) {
+    buttons.push(<button className="h-10 w-10 outline outline-1 hover:bg-black" key={i}></button>);
+  }
+
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="grid grid-cols-10 gap-0 outline outline-2">{buttons}</div>
     </main>
   );
 }
