c522e92611f99c345e8cc0b175bd8974e713c347
VillageR88 <villager88pl@gmail.com>
2023-12-16 09:55:14 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import '@fontsource/rubik';
+import '@fontsource/rubik/400.css';
+import '@fontsource/rubik/500.css';
+import '@fontsource/rubik/700.css';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+    <main className="font-rubik flex min-h-screen flex-col items-center justify-center bg-[#F5F6FA]">
+      <div className="h-[100em] items-center flex flex-col w-full mt-[4em]">
+        <div className="h-[10.45em] w-[45.625em] rounded-[0.5em] bg-white"></div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,20 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+        rubik: 'Rubik',
       },
       colors: {
-        // Slate
-        // 'accent-brand-800': '#1e293b',
-        // 'accent-brand-700': '#334155',
-        // 'accent-brand-600': '#475569',
+        //### Primary
+        moderateBlue: 'hsl(238, 40%, 52%)',
+        softRed: 'hsl(358, 79%, 66%)',
+        lightGrayishBlue: 'hsl(239, 57%, 85%)',
+        paleRed: 'hsl(357, 100%, 86%)',
+        //### Neutral
+        darkBlue: 'hsl(212, 24%, 26%)',
+        grayishBlue: 'hsl(211, 10%, 45%)',
+        lightGray: 'hsl(223, 19%, 93%)',
+        veryLightGray: 'hsl(228, 33%, 97%)',
+        white: 'hsl(0, 0%, 100%)',
       },
     },
   },
