90e3f324fa638e4fa00864a03e3b12c799bf0c3c
VillageR88 <villager88pl@gmail.com>
2023-12-16 12:42:02 +0100

    letters improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 export default function Home() {
   const buttons = [];
-
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   for (let i = 1; i <= 100; i++) {
     buttons.push(<button className="h-10 w-10 outline outline-1 hover:bg-black" key={i}></button>);
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="grid grid-cols-10 gap-0 outline outline-2">{buttons}</div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="grid grid-cols-10">
+        {letters.map((x, i) => (
+          <button className="h-10 w-10" key={i}>
+            {x}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-10 outline outline-2">{buttons}</div>
+    </div>
   );
 }
