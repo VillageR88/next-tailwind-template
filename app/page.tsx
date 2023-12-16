5af338bb2cf1168d6736aaa200d4db4caedcbdcc
VillageR88 <villager88pl@gmail.com>
2023-12-16 13:39:11 +0100

    improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -26,18 +26,18 @@ export default function Home() {
         <div className="flex flex-col">
           <div className="h-10 w-10"></div>
           <div className="flex flex-col">
-            {letters.map((_, i) => (
+            {letters.map((x, i) => (
               <button disabled className="h-10 w-10" key={i + 1}>
-                {i + 1}
+                {x}
               </button>
             ))}
           </div>
         </div>
         <div>
           <div className="grid grid-cols-10">
-            {letters.map((x, i) => (
+            {letters.map((_, i) => (
               <button disabled className="h-10 w-10" key={i}>
-                {x}
+                {i + 1}
               </button>
             ))}
           </div>
