ed68ec6ce92da7e7bf7a74633f85458f784b5581
VillageR88 <villager88pl@gmail.com>
2023-12-16 21:16:45 +0100

    improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [clicked, setClicked] = useState<number>(0);
+  enum GamePhase {
+    setup,
+  }
   const buttons = [];
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   for (let i = 1; i <= 100; i++) {
@@ -17,10 +19,30 @@ export default function Home() {
     );
   }
 
+  const [gamePhase, setGamePhase] = useState<GamePhase>(GamePhase.setup);
+  const [clicked, setClicked] = useState<number>(0);
+  const [border1, setBorder1] = useState<number[]>([]);
+  const [border2, setBorder2] = useState<number[]>([]);
+  useEffect(() => {
+    setBorder1([clicked, clicked - 10]);
+  }, [clicked]);
+  useEffect(() => {
+    function calculateBorder2() {
+      const array = [];
+      array.push(border1[0] + 10);
+      for (const i of border1) array.push(i);
+      array.push(border1[border1.length - 1] - 10);
+      return array;
+    }
+    setBorder2(calculateBorder2);
+  }, [border1]);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <div>
+      <div className="flex flex-col">
         <span>debug selected number: {clicked}</span>
+        <span>debug shipSizeOf2Vertical sector: {border1.toLocaleString()}</span>
+        <span>buffer shipSizeOf2Vertical sector: {border2.toLocaleString()}</span>
       </div>
       <div className="flex flex-row">
         <div className="flex flex-col">
