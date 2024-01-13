db7f3afabed2bd90de5eff1f2a4063b141c44862
VillageR88 <villager88pl@gmail.com>
2024-01-13 22:53:07 +0100

    layout improvement
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,8 @@ import iconMinus from './images/icon-minus.svg';
 import iconPlus from './images/icon-plus.svg';
 import whiteCart from './images/icon-cart_white.svg';
 import iconDelete from './images/icon-delete.svg';
-import { useState } from 'react';
+import mobileNav from './images/icon-menu.svg';
+import { useEffect, useState } from 'react';
 
 const Close = () => {
   return (
@@ -41,11 +42,27 @@ export default function Home() {
   const [storage, setStorage] = useState<number>(0);
   const [shoes, setShoes] = useState<number>(0);
   const [cartShown, setCartShown] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) setIsMobile(false);
+      else {
+        setIsMobile(true);
+        setLightboxShoeSelected(null);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const Cart = () => {
     const [color, setColor] = useState('#69707D');
     return (
       <button
+        className="scale-125 p-1 md:scale-100"
         onClick={() => {
           setCartShown(!cartShown);
         }}
@@ -85,7 +102,7 @@ export default function Home() {
         className="absolute z-10 ml-[-1.75em] flex h-[3.5em] w-[3.5em] items-center justify-center self-start rounded-full bg-white"
       >
         <svg className="ml-[-0.2em]" width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11 1 3 9l8 8" stroke={color} strokeWidth="3" fill="none" fill-rule="evenodd" />
+          <path d="M11 1 3 9l8 8" stroke={color} strokeWidth="3" fill="none" fillRule="evenodd" />
         </svg>
       </button>
     );
@@ -109,7 +126,7 @@ export default function Home() {
         className="absolute mr-[-1.75em] flex h-[3.5em] w-[3.5em] items-center justify-center self-end rounded-full bg-white"
       >
         <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
-          <path d="m2 1 8 8-8 8" stroke={color} stroke-width="3" fill="none" fill-rule="evenodd" />
+          <path d="m2 1 8 8-8 8" stroke={color} strokeWidth="3" fill="none" fillRule="evenodd" />
         </svg>
       </button>
     );
@@ -117,7 +134,7 @@ export default function Home() {
 
   return (
     <div className={`flex min-h-screen flex-col items-center justify-start bg-white`}>
-      {lightboxShoeSelected !== null && (
+      {lightboxShoeSelected !== null && !isMobile && (
         <div
           className={`${'overflow-auto'} fixed z-10 flex h-full w-full items-start justify-center  bg-black  bg-opacity-70 pt-[5em]`}
         >
@@ -182,11 +199,21 @@ export default function Home() {
         </div>
       )}
       <div className=" flex min-h-full w-full flex-col font-kumbhSans  xl:px-[3em] xxl:px-[10.3em]">
-        <nav className="flex flex-col py-4 md:px-[2em] md:py-0 md:pt-[1.5em] xxl:px-0">
+        <nav className="flex flex-col px-[1.5em] py-4 md:px-[2em] md:py-0 md:pt-[1.5em] xxl:px-0">
           <div className="flex items-center justify-between">
             <div className="flex gap-[3.5em] text-darkGrayishBlue">
-              <Image className="h-full" src={logo as string} alt="logo" />
-              <div className="hidden gap-[1.98em] text-[0.95rem] md:flex">
+              <div className="flex gap-4">
+                <button className="mt-[0.25em] flex md:hidden">
+                  <Image src={mobileNav as string} alt="menu" />
+                </button>
+                <Image className="h-full md:h-fit" src={logo as string} alt="logo" />
+              </div>
+              <div
+                onMouseEnter={() => {
+                  setCartShown(false);
+                }}
+                className="hidden gap-[1.98em] text-[0.95rem] md:flex"
+              >
                 {['Collections', 'Men', 'Women', 'About', 'Contact'].map((x, i) => {
                   return (
                     <div key={i} className="flex flex-col items-center ">
@@ -218,69 +245,88 @@ export default function Home() {
                 })}
               </div>
             </div>
-            <div className="flex items-center gap-[2.9em]">
+            <div className="mt-[0.4em] flex items-center gap-[1.2em] md:mt-0 md:gap-[2.9em]">
               <div>
                 <Cart />
                 {storage !== 0 && (
-                  <span className="absolute ml-[-1.5em] mt-[-0.7em] rounded-[2em] bg-orange px-[0.8em] text-[0.55rem] text-white">
+                  <span className="pointer-events-none absolute ml-[-1.9em]	 mt-[-0.25em] rounded-[2em] bg-orange px-[0.8em] text-[0.55rem] text-white">
                     {storage}
                   </span>
                 )}
                 {cartShown && (
-                  <div
-                    onMouseLeave={() => {
-                      setCartShown(false);
-                    }}
-                    className="absolute z-10 ml-[-15em] mt-[1.5em] flex h-[17em] w-[22em] flex-col rounded-[0.5em] bg-white shadow-2xl xl:ml-[-10em]"
-                  >
-                    <span className="pl-[1.5em] pt-[1.4em] font-[700]">Cart</span>
-                    <div className="mt-[1.4em] h-[1px] w-full bg-[#E9E9ED]"></div>
-                    <div className="flex h-full w-full items-center justify-center  text-darkGrayishBlue">
-                      {storage == 0 ? (
-                        <span className="font-[700]">Your cart is empty.</span>
-                      ) : (
-                        <div className="flex h-full w-full flex-col justify-center gap-[1.8em] px-6">
-                          <div className="flex items-center justify-between">
-                            <Image src={shoe1thumb} alt="shoe image" className="rounded-[0.3em]" height={50} />
-                            <div className="flex flex-col">
-                              <span>Fall Limited Edition Sneakers</span>{' '}
-                              <div className="flex gap-1.5">
-                                <span>$125.00 x 3</span>
-                                <span className="font-[700] text-black">$375.00</span>
+                  <div className="absolute left-0 z-10 mt-[2.5em] w-full rounded-[0.5em] px-2 shadow-2xl md:left-auto md:ml-[-15em] md:mt-[1.5em] md:w-[22em] md:px-0 xl:ml-[-10em]">
+                    <div
+                      onMouseLeave={() => {
+                        setCartShown(false);
+                      }}
+                      className="flex h-[17em] flex-col rounded-[0.5em] bg-white"
+                    >
+                      <span className="pl-[1.5em] pt-[1.4em] font-[700]">Cart</span>
+                      <div className="mt-[1.4em] h-[1px] w-full bg-[#E9E9ED]"></div>
+                      <div className="flex h-full w-full items-center justify-center  text-darkGrayishBlue">
+                        {storage == 0 ? (
+                          <span className="font-[700]">Your cart is empty.</span>
+                        ) : (
+                          <div className="flex h-full w-full flex-col justify-center gap-[1.8em] px-6">
+                            <div className="flex items-center justify-between">
+                              <Image src={shoe1thumb} alt="shoe image" className="rounded-[0.3em]" height={50} />
+                              <div className="flex flex-col">
+                                <span>Fall Limited Edition Sneakers</span>{' '}
+                                <div className="flex gap-1.5">
+                                  <span>$125.00 x 3</span>
+                                  <span className="font-[700] text-black">$375.00</span>
+                                </div>
                               </div>
+                              <button
+                                onClick={() => {
+                                  setStorage(0);
+                                  setCartShown(false);
+                                }}
+                              >
+                                <Image className="h-fit" src={iconDelete as string} alt="delete" />
+                              </button>
                             </div>
                             <button
                               onClick={() => {
                                 setStorage(0);
+                                setCartShown(false);
                               }}
+                              className="rounded-[0.5em] bg-orange py-4 text-[0.95rem] font-[700] text-white"
                             >
-                              <Image className="h-fit" src={iconDelete as string} alt="delete" />
+                              Checkout
                             </button>
                           </div>
-                          <button
-                            onClick={() => {
-                              setStorage(0);
-                            }}
-                            className="rounded-[0.5em] bg-orange py-4 text-[0.95rem] font-[700] text-white"
-                          >
-                            Checkout
-                          </button>
-                        </div>
-                      )}
+                        )}
+                      </div>
                     </div>
                   </div>
                 )}
               </div>
-              <button className="out rounded-full outline-2 -outline-offset-[2px] outline-[#EC9858] hover:outline">
-                <Image className="mt-[-0.05em]" height={50} src={avatar} alt="avatar" />
+              <button
+                onMouseEnter={() => {
+                  setCartShown(false);
+                }}
+                className="out rounded-full outline-2 -outline-offset-[2px] outline-[#EC9858] hover:outline"
+              >
+                <Image
+                  className="mt-[-0.45em] scale-[65%] md:mt-[-0.05em] md:scale-100"
+                  height={50}
+                  src={avatar}
+                  alt="avatar"
+                />
               </button>
             </div>
           </div>
           <div className="mt-[2em] hidden h-[1px] w-full bg-[#EAE9EC] md:block"></div>
         </nav>
-        <main className="flex h-full w-full flex-col items-center justify-center pb-[2.5em] pr-0 md:mt-[5.5em] md:flex-row md:gap-0 md:px-0 md:pr-6 lg:gap-[4em] lg:px-[3em] xl:gap-[6em] xxl:gap-[8em]">
+        <main
+          onMouseEnter={() => {
+            setCartShown(false);
+          }}
+          className="flex h-full w-full flex-col items-center justify-center pb-[2.5em] pr-0 md:mt-[5.5em] md:flex-row md:gap-0 md:px-0 md:pr-6 lg:gap-[4em] lg:px-[3em] xl:gap-[6em] xxl:gap-[8em]"
+        >
           <div className="flex w-full flex-col  md:w-fit md:min-w-fit">
-            <div className=" flex scale-100 flex-col gap-[2em] md:scale-75 lg:scale-100">
+            <div className="flex scale-100 flex-col gap-[2em] md:scale-75 lg:scale-100">
               <button
                 onClick={() => {
                   setLightboxShoeSelected(shoeSelected ?? 0);
@@ -323,24 +369,26 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="flex w-full flex-col items-start md:w-[33em]">
+          <div className="flex w-full flex-col items-start px-6 py-6 md:w-[33em] md:px-0 md:py-0">
             <span className="mb-[1em] text-[0.85rem] font-[700] tracking-[0.1em] text-[orange]">SNEAKER COMPANY</span>
-            <span className="mb-[0.8em] text-[2.8rem]  font-[700]  leading-[1em] text-[#1E1F25]">
+            <span className="mb-[0.8em] text-[2rem] font-[700] leading-[1em]  text-[#1E1F25] md:text-[2.8rem]">
               Fall Limited Edition Sneakers
             </span>
             <span className="mb-[1em] text-darkGrayishBlue">
               These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole,
               they’ll withstand everything the weather can offer.
             </span>
-            <div className="flex items-center gap-[1em]">
-              <span className="text-[1.8rem] font-[700]">$125.00</span>
-              <span className="rounded-md bg-paleOrange px-[0.6em] font-[700] text-orange">50%</span>
+            <div className="flex w-full items-center justify-between md:flex-col md:items-start">
+              <div className="flex items-center gap-[1em]">
+                <span className="text-[1.8rem] font-[700]">$125.00</span>
+                <span className="rounded-md bg-paleOrange px-[0.6em] font-[700] text-orange">50%</span>
+              </div>
+              <div className="flex flex-col md:mb-[2em]">
+                <span className="line-clamp-3 font-[700] text-grayishBlue">$250.00</span>
+                <div className="absolute mt-[0.865em] h-[1px] w-[3.7em] bg-grayishBlue"></div>
+              </div>
             </div>
-            <div className="mb-[2em] flex flex-col">
-              <span className="line-clamp-3 font-[700] text-grayishBlue">$250.00</span>
-              <div className="absolute mt-[0.865em] h-[1px] w-[3.7em] bg-grayishBlue"></div>
-            </div>
-            <div className="flex h-full w-full  flex-col gap-[1em] md:h-[3.5em] md:flex-row">
+            <div className="mt-4 flex h-full w-full flex-col gap-[1em] md:mt-0 md:h-[3.5em] md:flex-row">
               <div className="flex w-full items-center justify-between rounded-lg bg-lightGrayishBlue py-4 font-[700] md:w-[36%] md:py-0">
                 <button
                   onClick={() => {
