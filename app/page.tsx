aa2a581322520eb72f301863a6d8c8d40f699b28
VillageR88 <villager88pl@gmail.com>
2024-01-19 13:44:33 +0100

    draggable improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,17 @@ interface TodoJSON {
 
 export default function Home() {
   const [dataJSON, setDataJSON] = useState<TodoJSON[]>([]);
+  const dataJSONDivRef = useRef<HTMLDivElement | null>(null);
   const dataJSONRefIndex = useRef<number | null>(null);
-  console.log(dataJSONRefIndex.current);
   const [mouseHoverReference, setMouseHoverReference] = useState<number | null>(null);
   const [firstLoad, setFirstLoad] = useState<boolean>(false);
   const [inputText, setInputText] = useState<string>('');
-  const mouseYRef = useRef<number>(0);
   const dragImageRef2 = useRef<HTMLDivElement | null>(null);
   const [dragging, setDragging] = useState<boolean>(false);
-  const [hold, setHold] = useState<boolean>(false);
-  useEffect(() => {
-    if (dataJSONRefIndex.current === mouseHoverReference) setHold(true);
-    else setHold(false);
-  }, [mouseHoverReference]);
 
   useEffect(() => {
     if (dataJSONRefIndex.current !== null && mouseHoverReference !== null) {
-      if (mouseHoverReference !== dataJSONRefIndex.current && !hold) {
+      if (mouseHoverReference !== dataJSONRefIndex.current) {
         if (mouseHoverReference < dataJSONRefIndex.current) {
           setDataJSON([
             ...dataJSON.slice(0, mouseHoverReference),
@@ -47,15 +41,17 @@ export default function Home() {
       }
       dataJSONRefIndex.current = mouseHoverReference;
     }
-  }, [dataJSON, hold, mouseHoverReference]);
+  }, [dataJSON, mouseHoverReference]);
 
   useEffect(() => {
     window.addEventListener('mouseup', () => {
       setDragging(false);
+      document.body.style.cursor = 'default';
     });
     return () => {
       window.removeEventListener('mouseup', () => {
         setDragging(false);
+        document.body.style.cursor = 'default';
       });
     };
   }, [dragging]);
@@ -64,13 +60,14 @@ export default function Home() {
     if (dragging) {
       const handleMouseMove = (e: MouseEvent) => {
         if (dragImageRef2.current) {
-          dragImageRef2.current.style.top = `${e.clientY}px`;
-          dragImageRef2.current.style.left = `${e.clientX}px`;
+          const offsetY = e.clientY;
+          if (dataJSONDivRef.current && dataJSONDivRef.current.getBoundingClientRect().top + 20 < offsetY)
+            dragImageRef2.current.style.top = `${offsetY - 30}px`;
+          else dragImageRef2.current.style.top = `${dataJSONDivRef.current?.getBoundingClientRect()?.top}px`;
+          //q: help me with dataJSONDivRef.current?.getBoundingClientRect()?.bottom
         }
       };
-
       window.addEventListener('mousemove', handleMouseMove);
-
       return () => {
         window.removeEventListener('mousemove', handleMouseMove);
         if (dragImageRef2.current) document.body.removeChild(dragImageRef2.current);
@@ -114,67 +111,72 @@ export default function Home() {
     classExtension: string;
     index: number;
   }) => {
-    const [xIsVisible, setXIsVisible] = useState<boolean>(false);
     const dragImageRef = useRef<HTMLDivElement | null>(null);
 
     return (
-      <div
-        style={{ visibility: dragging && dataJSONRefIndex.current === index ? 'hidden' : 'visible' }}
-        ref={dragImageRef}
-        draggable
-        onDragStart={(e) => {
-          dataJSONRefIndex.current = index;
-
-          mouseYRef.current = e.clientY;
-          if (dragImageRef.current) {
-            dragImageRef2.current = dragImageRef.current.cloneNode(true) as HTMLDivElement;
-            dragImageRef2.current.style.position = 'fixed';
-            dragImageRef2.current.style.width = '30.8em';
-            dragImageRef2.current.style.height = 'full';
-            dragImageRef2.current.style.boxShadow = '0 0 0.5em 0.1em #000000';
-            dragImageRef2.current.style.color = '#CACCE3';
-            dragImageRef2.current.style.fontFamily = 'Josefin Sans';
-            dragImageRef2.current.style.fontSize = '1.1rem';
-            dragImageRef2.current.style.fontWeight = 'normal';
-            document.body.appendChild(dragImageRef2.current);
-            setDragging(true);
+      <div>
+        <div
+          style={{ visibility: dragging && dataJSONRefIndex.current === index ? 'hidden' : 'visible' }}
+          ref={dragImageRef}
+          draggable
+          onMouseEnter={() => {
+            setMouseHoverReference(index);
+          }}
+          onDragStart={(e) => {
             e.preventDefault();
-          }
-        }}
-        onMouseEnter={() => {
-          setMouseHoverReference(index);
-          setXIsVisible(true);
-        }}
-        onMouseLeave={() => {
-          setXIsVisible(false);
-        }}
-        className={`${classExtension} h-[4em] w-full items-center gap-[1em] bg-[#25273C] px-[1.5em]`}
-      >
-        <button className=" select-none" onClick={buttonCheckClick}>
-          <div
-            className={`${
-              completed && 'bg-gradient-to-br hover:outline-offset-2 hover:outline-[#CACCE3]'
-            } flex h-[1.45em]  w-[1.45em] items-center justify-center rounded-full from-[#6ABFFB] to-[#A373E8] outline outline-1 outline-[#37394E] hover:bg-gradient-to-br`}
-          >
-            {completed ? (
-              <Image src={iconCheck as string} alt="check" priority />
-            ) : (
-              <div className="h-[1.29em] w-[1.29em] rounded-full bg-[#25273C]"></div>
-            )}
-          </div>
-        </button>
-        <span
-          className={`${
-            !completed ? 'text-[#CACCE3]' : 'text-[#4E5065] line-through'
-          } w-[25em] select-none bg-transparent px-2 text-[1.1rem] placeholder-[#73758A]`}
+            dataJSONRefIndex.current = index;
+            if (dragImageRef.current) {
+              dragImageRef2.current = dragImageRef.current.cloneNode(true) as HTMLDivElement;
+              dragImageRef2.current.style.position = 'fixed';
+              dragImageRef2.current.style.left = screen.width > 768 ? 'calc(50% - 15.5em)' : 'calc(50% - 15.5em)';
+              dragImageRef2.current.style.width = '30.8em';
+              dragImageRef2.current.style.height = 'full';
+              dragImageRef2.current.style.boxShadow = '0 0 0.5em 0.1em #000000';
+              dragImageRef2.current.style.color = '#CACCE3';
+              dragImageRef2.current.style.fontFamily = 'Josefin Sans';
+              dragImageRef2.current.style.fontSize = '1.1rem';
+              dragImageRef2.current.style.fontWeight = 'normal';
+              dragImageRef2.current.style.userSelect = 'none';
+              dragImageRef2.current.style.pointerEvents = 'none';
+              document.body.appendChild(dragImageRef2.current);
+              document.body.style.cursor = 'grabbing';
+              setDragging(true);
+            }
+          }}
+          className={`${classExtension} self h-[4em] w-full items-center gap-[1em] bg-[#25273C] px-[1.5em]`}
         >
-          {task}
-        </span>
-        {xIsVisible && (
-          <button className="select-none" onClick={buttonCrossCLick}>
-            <Image src={iconCross as string} alt="delete Task" />
+          <button className=" select-none" onClick={buttonCheckClick}>
+            <div
+              className={`${
+                completed && 'bg-gradient-to-br hover:outline-offset-2 hover:outline-[#CACCE3]'
+              } flex h-[1.45em]  w-[1.45em] items-center justify-center rounded-full from-[#6ABFFB] to-[#A373E8] outline outline-1 outline-[#37394E] hover:bg-gradient-to-br`}
+            >
+              {completed ? (
+                <Image src={iconCheck as string} alt="check" priority unoptimized />
+              ) : (
+                <div className="h-[1.29em] w-[1.29em] rounded-full bg-[#25273C]"></div>
+              )}
+            </div>
           </button>
-        )}
+          <span
+            className={`${
+              !completed ? 'text-[#CACCE3]' : 'text-[#4E5065] line-through'
+            } w-[25em] select-none bg-transparent px-2 text-[1.1rem] placeholder-[#73758A]`}
+          >
+            {task}
+          </span>
+          {mouseHoverReference === index && (
+            <button className="select-none" onClick={buttonCrossCLick}>
+              <Image src={iconCross as string} alt="delete Task" />
+            </button>
+          )}
+        </div>
+        <div
+          onMouseEnter={() => {
+            setMouseHoverReference(index);
+          }}
+          className={`${!dragging && 'pointer-events-none'} absolute left-0 mt-[-4em] h-[4em] w-screen`}
+        ></div>
       </div>
     );
   };
@@ -186,12 +188,13 @@ export default function Home() {
           <div className="mt-[-14.4em] w-[33.8em] flex-col justify-center">
             <div className="flex items-center justify-between">
               <span className="text-[2.45rem] font-[700] tracking-[0.4em]">TODO</span>
-              <button>
+              <button className={`${dragging && 'pointer-events-none'}`}>
                 <Image className="h-fit pb-[0.5em]" src={iconSun as string} alt="icon-sun" />
               </button>
             </div>
             <div className="mt-[1.9em] flex h-[4em] w-full items-center gap-[1em] bg-[#25273C] pl-[1.5em]">
               <button
+                className={`${dragging && 'pointer-events-none'}`}
                 onClick={() => {
                   if (inputText === '') return;
                   dataJSON.push({ completed: false, task: inputText });
@@ -203,8 +206,11 @@ export default function Home() {
                 </div>
               </button>
               <input
+                disabled={dragging}
                 value={inputText}
-                className="mt-[0.3em] w-[25em] bg-transparent px-2 text-[1.1rem] text-[#CACCE3] placeholder-[#73758A]"
+                className={`${
+                  dragging && 'pointer-events-none'
+                } mt-[0.3em] w-[25em] bg-transparent px-2 text-[1.1rem] text-[#CACCE3] placeholder-[#73758A]`}
                 placeholder="Create a new todo..."
                 type="text"
                 onChange={(e) => {
@@ -213,7 +219,7 @@ export default function Home() {
               />
             </div>
             <div></div>
-            <div className="mt-[1.5em] flex w-full flex-col">
+            <div ref={dataJSONDivRef} className="mt-[1.5em] flex w-full flex-col">
               {dataJSON.map((x: TodoJSON, i: number) => (
                 <div key={i} className="flex flex-col">
                   <TodoBlock
@@ -251,7 +257,7 @@ export default function Home() {
               ))}
               <div className="flex h-[3em] w-full items-center justify-between bg-[#25273C]  px-6 text-[#62647D]">
                 <span className="text-[0.9rem]">{dataJSON.filter((x) => !x.completed).length} items left</span>
-                <div className="flex gap-[3.3em]">
+                <div className={`${dragging && 'pointer-events-none'} flex gap-[3.3em]`}>
                   <div className="flex gap-[1em]">
                     {[Mode.All, Mode.Active, Mode.Completed].map((x) => (
                       <button
