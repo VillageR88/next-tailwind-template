68474aefe11fbc43a189ae8dbdcf2a41ccafa876
VillageR88 <villager88pl@gmail.com>
2024-01-20 23:25:04 +0100

    initial commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
-    </main>
+    <div className="font-leagueSpartan flex h-screen flex-col items-center justify-center ">
+      <main className="h-[50em] w-full bg-white">
+        <nav className="h-[33.4em] w-[52.5em] bg-[url('./images/desktop-image-hero-1.jpg')]"></nav>
+      </main>
+    </div>
   );
 }
