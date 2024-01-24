78662524c5c81b51f85360e1d44c05a070837b4b
VillageR88 <villager88pl@gmail.com>
2024-01-24 19:18:47 +0100

    code improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,32 +7,32 @@ import { Icon } from 'leaflet';
 
 export default function Home() {
   interface locationJSON {
-    ip: '8.8.8.8';
+    ip: string;
     location: {
-      country: 'US';
-      region: 'California';
-      city: 'Mountain View';
-      lat: 37.40599;
-      lng: -122.078514;
-      postalCode: '94043';
-      timezone: '-07:00';
-      geonameId: 5375481;
+      country: string;
+      region: string;
+      city: string;
+      lat: number;
+      lng: number;
+      postalCode: string;
+      timezone: string;
+      geonameId: number;
     };
-    domains: ['0d2.net', '003725.com', '0f6.b0094c.cn', '007515.com'];
+    domains: string[];
     as: {
-      asn: 15169;
-      name: 'Google LLC';
-      route: '8.8.8.0/24';
-      domain: 'https://about.google/intl/en/';
-      type: 'Content';
+      asn: number;
+      name: string;
+      route: string;
+      domain: string;
+      type: string;
     };
-    isp: 'Google LLC';
+    isp: string;
   }
 
   const [text, setText] = useState('');
   const [ip, setIp] = useState('');
   const [location, setLocation] = useState<locationJSON | null>(null);
-  const [sendRequest, setSendRequest] = useState(false);
+  const [sendRequest, setSendRequest] = useState(true);
   const [mapCenter, setMapCenter] = useState<[number, number]>([51.505, -0.09]);
   console.log('Map Center:', mapCenter);
   useEffect(() => {
@@ -61,99 +61,100 @@ export default function Home() {
   }, [ip, sendRequest]);
 
   const customIcon = new Icon({
-    //black icon
     iconUrl: './icon-location.svg',
     iconSize: [45, 58],
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center font-rubik">
-      <div className="flex h-[50em] w-full flex-col bg-white">
-        <div className="flex h-full w-full flex-col bg-[url('./images/pattern-bg-desktop.png')] bg-no-repeat">
-          <div className="mt-[1.6em] flex h-[24.5em] w-full flex-col items-center">
-            <h1>IP Address Tracker</h1>
-            <form
-              id="form"
-              onSubmit={(e) => {
-                e.preventDefault();
-              }}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  setIp(text);
-                  setSendRequest(true);
-                }
-              }}
-              className="mt-[1.35em] flex h-[3.65em] w-[34.688em] rounded-[1em] "
-            >
-              <input
-                onChange={(e) => {
-                  setText(e.target.value);
+    location && (
+      <main className="flex min-h-screen flex-col items-center justify-center font-rubik">
+        <div className="flex h-[50em] w-full flex-col bg-white">
+          <div className="flex h-full w-full flex-col bg-[url('./images/pattern-bg-desktop.png')] bg-no-repeat">
+            <div className="mt-[1.6em] flex h-[24.5em] w-full flex-col items-center">
+              <h1>IP Address Tracker</h1>
+              <form
+                id="form"
+                onSubmit={(e) => {
+                  e.preventDefault();
                 }}
-                value={text}
-                className="h-full w-full rounded-l-[1em] pl-[1.3em] pr-[2.5em] text-[1.15em] tracking-[-0.008em] outline-none"
-                type="text"
-                placeholder="Search for any IP address or domain"
-              />
-              <button
-                aria-label="search"
-                onClick={() => {
-                  setIp(text);
-                  setSendRequest(true);
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    setIp(text);
+                    setSendRequest(true);
+                  }
                 }}
-                className="flex h-full w-[4.1em] items-center justify-center rounded-r-[1em] bg-[#000000] pl-[0.1em]"
+                className="mt-[1.35em] flex h-[3.65em] w-[34.688em] rounded-[1em] "
               >
-                <Image src={iconArrow as string} alt="Search" />
-              </button>
-            </form>
-            <div className="absolute z-10 mt-[10.95em] flex min-h-[10.05em] w-[69.4em] rounded-[1em] bg-white pb-[1em] pl-[2em] ">
-              <div className="flex w-[15.3em] flex-col gap-[0.45em] pt-[2.1em]">
-                <h2>IP ADDRESS</h2>
-                <span className="span1">{location?.ip}</span>
-              </div>
-              <div className="flex items-start gap-[2em]">
-                <div className="mt-[1em] h-[4.7em] w-[1px] self-center bg-[#d3cfcf]"></div>
-                <div className="flex w-[15.35em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
-                  <h2>LOCATION</h2>
-                  <span className="span1 leading-[1.2em]">
-                    {location?.location.country + ', ' + location?.location.region}
-                  </span>
+                <input
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                  value={text}
+                  className="h-full w-full rounded-l-[1em] pl-[1.3em] pr-[2.5em] text-[1.15em] tracking-[-0.008em] outline-none"
+                  type="text"
+                  placeholder="Search for any IP address or domain"
+                />
+                <button
+                  aria-label="search"
+                  onClick={() => {
+                    setIp(text);
+                    setSendRequest(true);
+                  }}
+                  className="flex h-full w-[4.1em] items-center justify-center rounded-r-[1em] bg-[#000000] pl-[0.1em]"
+                >
+                  <Image src={iconArrow as string} alt="Search" />
+                </button>
+              </form>
+              <div className="absolute z-10 mt-[10.95em] flex min-h-[10.05em] w-[69.4em] rounded-[1em] bg-white pb-[1em] pl-[2em] ">
+                <div className="flex w-[15.3em] flex-col gap-[0.45em] pt-[2.1em]">
+                  <h2>IP ADDRESS</h2>
+                  <span className="span1">{location.ip}</span>
                 </div>
-              </div>
-              <div className="flex items-start gap-[2em]">
-                <div className="mt-[1em] h-[4.7em]  w-[1px] self-center bg-[#d3cfcf]"></div>
-                <div className="flex w-[15.25em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
-                  <h2>TIMEZONE</h2>
-                  <span className="span1 leading-[1.2em]">{'UTC ' + location?.location.timezone}</span>
+                <div className="flex items-start gap-[2em]">
+                  <div className="mt-[1em] h-[4.7em] w-[1px] self-center bg-[#d3cfcf]"></div>
+                  <div className="flex w-[15.35em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
+                    <h2>LOCATION</h2>
+                    <span className="span1 leading-[1.2em]">
+                      {location.location.country + ', ' + location.location.region}
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-start gap-[2em]">
-                <div className="mt-[1em] h-[4.7em]  w-[1px] self-center bg-[#d3cfcf]"></div>
-                <div className="flex w-[12em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
-                  <h2>ISP</h2>
-                  <span className="span1 leading-[1.2em]">{location?.isp}</span>
+                <div className="flex items-start gap-[2em]">
+                  <div className="mt-[1em] h-[4.7em]  w-[1px] self-center bg-[#d3cfcf]"></div>
+                  <div className="flex w-[15.25em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
+                    <h2>TIMEZONE</h2>
+                    <span className="span1 leading-[1.2em]">{'UTC ' + location.location.timezone}</span>
+                  </div>
+                </div>
+                <div className="flex items-start gap-[2em]">
+                  <div className="mt-[1em] h-[4.7em]  w-[1px] self-center bg-[#d3cfcf]"></div>
+                  <div className="flex w-[12em] flex-col gap-[0.7em] pr-[1em] pt-[2.1em]">
+                    <h2>ISP</h2>
+                    <span className="span1 leading-[1.2em]">{location.isp}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div id="map" className=" h-full w-full">
-            <MapContainer
-              center={mapCenter}
-              zoom={13}
-              scrollWheelZoom={false}
-              dragging={false}
-              doubleClickZoom={false}
-              touchZoom={false}
-              zoomControl={false}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker icon={customIcon} position={mapCenter}></Marker>
-            </MapContainer>
+            <div id="map" className=" h-full w-full">
+              <MapContainer
+                center={mapCenter}
+                zoom={13}
+                scrollWheelZoom={false}
+                dragging={false}
+                doubleClickZoom={false}
+                touchZoom={false}
+                zoomControl={false}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker icon={customIcon} position={mapCenter}></Marker>
+              </MapContainer>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    )
   );
 }
