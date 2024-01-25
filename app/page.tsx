5eb297bbbabbbd5f29a57430301f2d607f67a92d
VillageR88 <villager88pl@gmail.com>
2024-01-26 00:32:44 +0100

    layout improved and button component initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,47 @@ import { mdiChevronRight } from '@mdi/js';
 import Image from 'next/image';
 import logo from './images/logo.png';
 
+const ButtonWithSlider = () => {
+  const [buttonHoverValue, setButtonHoverValue] = useState(0);
+
+  return (
+    <button
+      onMouseEnter={() => {
+        setButtonHoverValue(100);
+      }}
+      onMouseLeave={() => {
+        setButtonHoverValue(0);
+      }}
+      className="flex h-[44px] w-[271.88px] items-center justify-between rounded-[0.5em] bg-[#ff8b00]"
+    >
+      <div className="flex h-full w-full items-center justify-end">
+        <span className="absolute z-10 pr-2.5 font-baloo2 text-[1.2rem] font-[500] text-white">
+          {'Przedszkole Mały Skarb'}
+        </span>
+        <div
+          style={{ width: `${buttonHoverValue}%` }}
+          className="h-full rounded-l-[0.5em] bg-[#e07b21] transition-all duration-[400ms] ease-in-out"
+        ></div>
+      </div>
+      <div className="flex h-full items-center rounded-r-[0.5em] bg-[#e07b21] px-1">
+        <Icon color="white" path={mdiChevronRight} size={1.4} />
+      </div>
+    </button>
+  );
+};
+
 export default function Home() {
   const [facebookHover, setFacebookHover] = useState(false);
-  const [button1HoverValue, setButton1HoverValue] = useState(0);
   return (
     <div className="flex min-h-screen flex-col items-center justify-start font-roboto">
       <nav className="flex h-[2.9em] w-full justify-center bg-[#5DBA3B]">
         <div className="flex w-[75.7em] items-center justify-between pb-[0.1em] text-[0.94rem] text-[#fff]">
           <button>
-            <span className="hover:underline">Mały Skarb - Centrum twórczego rozwoju</span>
+            <span className="hover:underline">{'Mały Skarb - Centrum twórczego rozwoju'}</span>
           </button>
           <div className="flex items-center gap-4">
             <button className="hover:text-[#FF5157] hover:underline">
-              <span className=" text-[1.05rem] ">RODO</span>
+              <span className=" text-[1.05rem] ">{'RODO'}</span>
             </button>
             <button
               onMouseEnter={() => {
@@ -45,35 +73,24 @@ export default function Home() {
         </div>
       </nav>
       <main className="flex h-[50.5em] w-full flex-col">
-        <div className="flex h-full w-full items-center justify-end bg-[url('./images/home-slide_img.jpg')] bg-[50%_0%]">
+        <div className="flex h-full w-full items-center justify-center bg-[url('./images/home-slide_img.jpg')] bg-[50%_0%]">
           <div className="h-[34.5em] w-[69.5em] rounded-[3em] bg-white p-[0.65em]">
             <div className="flex h-full w-full flex-col items-center rounded-[3em] outline-dashed outline-1">
               <Image className="w-[50em] pt-[3.1em]" src={logo} alt="image of child" />
               <div className="mt-[5em] flex w-full justify-around px-[7em]">
-                <div>
-                  <button
-                    onMouseEnter={() => {
-                      setButton1HoverValue(100);
-                    }}
-                    onMouseLeave={() => {
-                      setButton1HoverValue(0);
-                    }}
-                    className="flex h-[44px] w-[271.88px] items-center justify-between rounded-[0.5em] bg-[#ff8b00]"
-                  >
-                    <div className="flex h-full w-full items-center justify-end">
-                      <span className="absolute z-10 pr-2.5 font-baloo2 text-[1.2rem] font-[500] text-white">
-                        Przedszkole Mały Skarb
-                      </span>
-                      <div
-                        style={{ width: `${button1HoverValue}%` }}
-                        className="h-full rounded-l-[0.5em] bg-[#e07b21] transition-all duration-300"
-                      ></div>
-                    </div>
-                    <div className="flex h-full items-center rounded-r-[0.5em] bg-[#e07b21] px-1">
-                      <Icon color="white" path={mdiChevronRight} size={1.4} />
-                    </div>
-                  </button>
+                <div className="w-[271.88px]">
+                  <ButtonWithSlider />
+
+                  <div>
+                    <span className="mt-[2em] flex whitespace-pre-line text-center text-[15px] leading-6 text-[#777]">
+                      {`Nasze przedszkole funkcjonuje
+                    od 2003 roku. Przedszkolaki korzystają 
+                    z barwnych, w pełni przygotowanych
+                    do potrzeb dzieci sal oraz ogrodu`}
+                    </span>
+                  </div>
                 </div>
+
                 <div>
                   <button className="h-[44px] w-[211.5px] bg-blue-400"></button>
                 </div>
