9dbcf7c7529196272cb6b058fffd64f224f6bde6
VillageR88 <villager88pl@gmail.com>
2024-01-27 16:42:27 +0100

    header2 layout improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -5,46 +5,83 @@ import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
 import Image from 'next/image';
 import logo from '@/app/images/logo.png';
-import { mdiEmailOpenOutline, mdiInstagram, mdiMapMarker, mdiPhone, mdiYoutube } from '@mdi/js';
+import { mdiEmailOpenOutline, mdiMapMarker, mdiPhone } from '@mdi/js';
 import Icon from '@mdi/react';
 
 export default function Start() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start font-roboto">
       <Header />
-      <div className="mt-5 flex w-full justify-between px-10 xl:w-[75.7em]">
-        <div className="flex flex-col items-center">
-          <Image height={60} src={logo} alt="logo" />
-          <span className="font-varelaRound mt-[0.3em] text-[1.7rem] font-[800] tracking-[0.1em] text-[#00b596]">
-            przedszkole
-          </span>
-        </div>
-        <div className="mt-[2em] grid grid-cols-2 flex-row items-center gap-[3em] text-[15px] text-[#777] md:flex xl:pr-8">
-          <div className="flex w-full items-center justify-center gap-[0.5em] md:w-fit">
-            <Icon className="text-[#5dba3b]" path={mdiPhone} size={1.6} />
-            <div className="flex flex-col gap-[0.2em]">
+      <div className="flex flex-col">
+        <div className="mt-5 flex w-full justify-between px-10 xl:w-[75.7em]">
+          <button>
+            <div className="flex flex-col items-center decoration-[#23527c] hover:underline">
+              <Image height={60} src={logo} alt="logo" />
+              <span className="font-varelaRound mt-[0.3em] text-[1.7rem] font-[800] tracking-[0.1em] text-[#00b596]">
+                przedszkole
+              </span>
+            </div>
+          </button>
+          <div className="mt-[2em] grid grid-cols-2 flex-row items-center gap-[3em] text-[15px] text-[#777] md:flex xl:pr-8">
+            <div className="flex w-full items-center justify-center gap-[0.5em] md:w-fit">
+              <Icon className="text-[#5dba3b]" path={mdiPhone} size={1.6} />
+              <div className="flex flex-col gap-[0.2em]">
+                <button className="duration-300 hover:text-[#5255c5]">
+                  <span>{'608-674-006'}</span>
+                </button>
+                <button className="duration-300 hover:text-[#5255c5]">
+                  <span>{'698-675-406'}</span>
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-center gap-[0.5em]  md:w-fit">
+              <Icon className="text-[#ff8b00]" path={mdiMapMarker} size={1.6} />
               <button className="duration-300 hover:text-[#5255c5]">
-                <span>{'608-674-006'}</span>
+                <span className="flex whitespace-pre-wrap text-left leading-7">
+                  {'11 Listopada 68,\n80-180 Gdańsk'}
+                </span>
               </button>
+            </div>
+            <div className="col-span-2 flex w-full justify-center gap-[0.5em] md:w-fit">
+              <Icon className="text-[#ff5157]" path={mdiEmailOpenOutline} size={1.6} />
               <button className="duration-300 hover:text-[#5255c5]">
-                <span>{'698-675-406'}</span>
+                <span>{'poczta@malyskarb.pl'}</span>
               </button>
             </div>
           </div>
-          <div className="flex w-full items-center justify-center gap-[0.5em]  md:w-fit">
-            <Icon className="text-[#ff8b00]" path={mdiMapMarker} size={1.6} />
-            <button className="duration-300 hover:text-[#5255c5]">
-              <span className="flex whitespace-pre-wrap text-left leading-7">{'11 Listopada 68,\n80-180 Gdańsk'}</span>
-            </button>
-          </div>
-          <div className="col-span-2 flex w-full justify-center gap-[0.5em] md:w-fit">
-            <Icon className="text-[#ff5157]" path={mdiEmailOpenOutline} size={1.6} />
-            <button className="duration-300 hover:text-[#5255c5]">
-              <span>{'poczta@malyskarb.pl'}</span>
-            </button>
-          </div>
+        </div>
+        <div className="mt-[2.9em] h-0">
+          <div className="absolute left-0 h-[1px] w-screen bg-gray-200"></div>
+        </div>
+        <div className="mt-[1px] flex w-full justify-between px-9">
+          {['Start', 'O nas', 'Co jemy?', 'Kadra', 'Pliki', 'Rekrutacja', 'Wirtualny spacer', 'Zajęcia dodatkowe'].map(
+            (item, index) => {
+              const color = () => {
+                if ((index + 1) % 5 === 1) return { background: '#5255c5', hover: 'hover:text-[#5255c5]' };
+                else if ((index + 1) % 5 === 2) return { background: '#5dba3b', hover: 'hover:text-[#5dba3b]' };
+                else if ((index + 1) % 5 === 3) return { background: '#ff8b00', hover: 'hover:text-[#ff8b00]' };
+                else if ((index + 1) % 5 === 4) return { background: '#ff5157', hover: 'hover:text-[#ff5157]' };
+                else if ((index + 1) % 5 === 0) return { background: '#ffc000', hover: 'hover:text-[#ffc000]' };
+              };
+              return (
+                <button className={`${color()?.hover} transition duration-[250ms]`} key={index}>
+                  <div className="flex flex-col items-center gap-[0.75em]">
+                    <div
+                      style={{
+                        width: `${0.8 * item.length * (item.length < 10 ? 1 : 0.85)}em`,
+                        backgroundColor: `${color()?.background}`,
+                      }}
+                      className="h-[0.3em] rounded-[1em]"
+                    ></div>
+                    <h2 className="font-baloo text-[20px]">{item}</h2>
+                  </div>
+                </button>
+              );
+            },
+          )}
         </div>
       </div>
+
       <div className="flex h-[50em] w-full items-center justify-center">
         <span>test</span>
       </div>
