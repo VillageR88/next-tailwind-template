d91292974640cbf9369d2ef44301acc7713a3829
VillageR88 <villager88pl@gmail.com>
2024-01-27 12:57:14 +0100

    refresh layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- pages/Start.tsx
+++ pages/Start.tsx
@@ -1,9 +1,13 @@
-const Start = () => {
+import React from 'react';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
+
+export default function Start() {
   return (
-    <div>
-      <h1>Start</h1>
+    <div className="flex min-h-screen flex-col items-center justify-start font-roboto">
+      <Header />
+      <span>test</span>
+      <Footer />
     </div>
   );
-};
-
-export default Start;
+}
