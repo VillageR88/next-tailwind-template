c03a7cc596517bee33389e79bee1d813dfa04e98
VillageR88 <villager88pl@gmail.com>
2024-02-04 14:26:36 +0100

    main 2nd row mobile
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow2.tsx
+++ app/components/MainRow2.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 const MainRow2 = () => {
   return (
-    <div className="flex min-h-[42.25em] w-full flex-col bg-[#F4F5F7] py-[6.2em] md:px-[2em] lg:px-[5em] min-[1458px]:px-[10.3em]">
-      <div className="flex w-[40em] flex-col">
+    <div className="flex min-h-[42.25em] w-full flex-col bg-[#F4F5F7] px-6 py-[4em] md:px-[2em] md:py-[6.2em] lg:px-[5em] min-[1458px]:px-[10.3em]">
+      <div className="flex flex-col text-center md:w-[40em] md:text-left">
         <h2 className="scale-y-[105%] pl-[0.03em] text-[2.41rem] font-[300] text-[hsl(233,26%,24%)]">
           Why choose Easybank?
         </h2>
@@ -11,7 +11,7 @@ const MainRow2 = () => {
           before.
         </span>
       </div>
-      <ul className="mt-[4.6em] flex h-full flex-wrap justify-around gap-x-[2em] gap-y-[5em] text-wrap min-[916px]:justify-between min-[1458px]:gap-0">
+      <ul className="mt-[4.6em] flex h-full flex-wrap justify-around gap-x-[2em] gap-y-[3em] text-wrap md:gap-y-[5em] min-[916px]:justify-between min-[1458px]:gap-0">
         {[
           {
             image: 'images/icon-online.svg',
@@ -34,10 +34,13 @@ const MainRow2 = () => {
             span: 'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
           },
         ].map((x) => (
-          <li className="flex max-w-[16.06em] flex-col" key={x.h3}>
+          <li
+            className="flex flex-col items-center text-center md:max-w-[16.06em] md:items-start md:text-left"
+            key={x.h3}
+          >
             <Image className="h-fit w-fit" src={x.image} width={10} height={10} alt="icon" />
-            <h3 className="mt-[1.6em] text-[1.45rem] text-[hsl(233,26%,24%)]">{x.h3}</h3>
-            <span className="mt-[1.7em] scale-y-[105%] text-[0.97rem] leading-[1.6em] text-[hsl(233,8%,62%)]">
+            <h3 className="mt-[1em] text-[1.45rem] text-[hsl(233,26%,24%)] md:mt-[1.6em]">{x.h3}</h3>
+            <span className="mt-[1.3em] scale-y-[105%] text-[0.97rem] leading-[1.6em] text-[hsl(233,8%,62%)] md:mt-[1.7em]">
               {x.span}
             </span>
           </li>
