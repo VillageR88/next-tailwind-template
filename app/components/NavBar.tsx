dcef1b8a155476811e7c5090dc8d0e39d201007a
VillageR88 <villager88pl@gmail.com>
2024-02-05 12:04:09 +0100

    Add navigation bar items and update font style
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,18 @@
 import Logo from './Logo';
-
+const navBarItems = ['Pricing', 'Product', 'About Us', 'Careers', 'Community'];
 const NavBar = () => {
   return (
     <nav className="z-20 flex h-[10.6em] w-full items-center justify-between bg-[white] px-6 md:px-[2em] lg:px-[5em] min-[1458px]:px-[10.3em]">
       <Logo />
+      <ul className="flex">
+        {navBarItems.map((item, iterator) => (
+          <li key={iterator}>
+            <button aria-label={item} className="text-[0.9rem]">
+              {item}
+            </button>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar';
 
 export default function Home() {
   return (
-    <main className="font-beVietnamPro flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center font-beVietnamPro">
       <NavBar />
       <div className="flex h-[100em] w-full items-center justify-center bg-[#ffffff]"></div>
     </main>
