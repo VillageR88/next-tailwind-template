014801858dd7c8d193b81808150ba4c3e05f4dab
VillageR88 <villager88pl@gmail.com>
2024-02-05 14:58:26 +0100

    Add MainRow2 component to Main
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,9 +1,11 @@
 import MainRow1 from './MainRow1';
+import MainRow2 from './MainRow2';
 
 const Main = () => {
   return (
     <main className="flex h-full w-full flex-col">
       <MainRow1 />
+      <MainRow2 />
     </main>
   );
 };
