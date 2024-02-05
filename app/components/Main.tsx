92ac3167343e9c51c135f6d799fe25d0c31a50e6
Karol Piecuch <villager88pl@gmail.com>
2024-02-05 23:05:48 +0100

    Add MainRow3 component to Main

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,11 +1,15 @@
 import MainRow1 from './MainRow1';
 import MainRow2 from './MainRow2';
+import MainRow3 from './MainRow3';
 
 const Main = () => {
+  const carouselItems= [
+    
   return (
     <main className="flex h-full w-full flex-col">
       <MainRow1 />
       <MainRow2 />
+      <MainRow3 />
     </main>
   );
 };
