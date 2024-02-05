86091d35c3d366ce8de4d63983b5de9ef0b27134
Karol Piecuch <villager88pl@gmail.com>
2024-02-05 22:47:38 +0100

    Remove ButtonInstagram component and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import ButtonInstagram from '../node_modules/my-component-library/components/fmc/ButtonInstagram';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex h-[50em] w-full items-center justify-center bg-blue-400">
-        <ButtonInstagram />
-      </div>
+      <div className="flex h-[50em] w-full items-center justify-center bg-blue-400"></div>
     </main>
   );
 }
