e82f59c1c3b3678f81322ebb37aad24006724062
VillageR88 <villager88pl@gmail.com>
2024-02-05 01:35:23 +0100

    Add ButtonInstagram component and update dependencies
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import ButtonInstagram from '../node_modules/my-component-library/components/fmc/ButtonInstagram';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <p className="text-lg">Hello</p>
-      <p className="font-mono">World 🌍</p>
+      <div className="flex h-[50em] w-full items-center justify-center bg-blue-400">
+        <ButtonInstagram />
+      </div>
     </main>
   );
 }
