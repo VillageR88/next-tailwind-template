776eb9d107b85fd26b5222fd0203df05c53d7a5d
VillageR88 <villager88pl@gmail.com>
2024-02-07 00:58:30 +0100

    Refactor MainRow1 and MainRow2 components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow2.tsx
+++ app/components/MainRow2.tsx
@@ -18,25 +18,32 @@ const MainRow2 = () => {
   ];
 
   return (
-    <div className="flex w-full flex-col-reverse gap-[2em] bg-[#FFFFFF] pb-[5em] pt-[3em] md:h-[39.3em] md:flex-row md:gap-0 md:pb-0">
+    <div className="flex w-full flex-col gap-[2em] bg-[#FFFFFF] pb-[5em] pt-[3em] md:h-[39.3em] md:flex-row md:gap-0 md:pb-0 md:pr-6">
       <div className="mb-[0.4em] flex flex-col items-center justify-start px-6 text-center md:w-[50%] md:items-start md:pl-[2em] md:text-left lg:pl-[5em] xl:pr-[1em] min-[1458px]:pl-[10.3em]">
-        <h2 className="ml-[0.25em] mt-[0.3em] scale-x-[103%] scale-y-[105%] text-[2.65rem] font-[700] leading-[1.16em] text-[hsl(233,26%,24%)] md:text-[1.85rem] lg:text-[2.1rem] xl:text-[2.25rem]">
+        <h2 className="mt-[0.3em] scale-x-[103%] scale-y-[105%] text-[1.9rem] font-[700] leading-[1.16em] text-[hsl(233,26%,24%)] md:ml-[0.25em] md:text-[1.85rem] lg:text-[2.1rem] xl:text-[2.25rem]">
           What’s different about Manage?
         </h2>
-        <span className="mb-[2.4em] mt-[1.8em] max-w-[20em] tracking-[-0.03em] text-[hsl(233,8%,62%)] xl:text-[1.05rem]">
+        <p className="mb-[2.4em] mt-[1.8em] tracking-[-0.03em] text-[hsl(233,8%,62%)] md:max-w-[20em] xl:text-[1.05rem]">
           Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for
           modern digital product teams.
-        </span>
+        </p>
       </div>
-      <ul className="z-10 ml-[1.7em]  flex flex-col items-start justify-start gap-[2.3em] md:w-[50%]">
+      <ul className="z-10 ml-[1em] flex flex-col items-center justify-start gap-[2.3em] md:ml-[1.7em] md:w-[50%] md:items-start">
         {articles.map((article, index) => (
-          <li className="flex items-start gap-[1.6em]" key={index}>
-            <div className="rounded-[1.8em] bg-[hsl(12,88%,59%)] px-[1.55em] py-[0.5em] font-[500] text-[hsl(13,100%,96%)]">
-              {(index + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}
+          <li className="flex flex-col gap-[1.6em] md:flex-row md:items-start" key={index}>
+            <div className="flex items-center gap-[0.8em] rounded-l-[1.8em] bg-[#FFF0EB] md:bg-transparent">
+              <div className="rounded-[1.8em] bg-[hsl(12,88%,59%)] px-[1.55em] py-[0.5em] font-[500] text-[hsl(13,100%,96%)]">
+                {(index + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}
+              </div>
+              <h3 className="flex pr-2 text-[0.97rem] font-[700] tracking-[-0.03em] text-[hsl(233,26%,24%)] md:hidden md:pr-0">
+                {article.title}
+              </h3>
             </div>
             <div className="mt-[0.5em] flex flex-col gap-[1.2em]">
-              <h3 className="text-[0.97rem] font-[700] tracking-[-0.03em] text-[hsl(233,26%,24%)]">{article.title}</h3>
-              <span className="max-w-[28em] text-[0.97rem] leading-[1.7em] text-[hsl(233,8%,62%)]">
+              <h3 className="hidden text-[0.97rem] font-[700] tracking-[-0.03em] text-[hsl(233,26%,24%)] md:flex">
+                {article.title}
+              </h3>
+              <span className="px-2 text-[0.88rem] leading-[1.7em] text-[hsl(233,8%,62%)] md:max-w-[28em] md:px-0 md:text-[0.97rem]">
                 {article.description}
               </span>
             </div>
