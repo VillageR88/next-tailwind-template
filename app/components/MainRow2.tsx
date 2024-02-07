4a1e7da5ceac00be2499a9bfcdad0f60f57359ea
VillageR88 <villager88pl@gmail.com>
2024-02-07 19:34:51 +0100

    (initial) Add MainRow2 component to Home page
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/components/MainRow2.tsx
@@ -0,0 +1,12 @@
+const MainRow2 = () => {
+  return (
+    <div className="flex min-h-[20em] w-full flex-col justify-start bg-[#F0F1F6]">
+      <div className="h-[7.3em] bg-[#FFFFFF]"></div>
+      <div className="flex justify-center">
+        <div className="mt-[-5.25em] h-[10.5em] w-[77%] rounded-[0.6em] bg-[hsl(257,27%,26%)]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainRow2;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
 import MainRow1 from './components/MainRow1';
+import MainRow2 from './components/MainRow2';
 
 export default function Home() {
   return (
     <main className="font-poppins flex min-h-screen flex-col items-center justify-center">
       <NavBar />
       <MainRow1 />
+      <MainRow2 />
       <div className="flex h-[60em] w-full items-center justify-center bg-transparent"></div>
     </main>
   );
