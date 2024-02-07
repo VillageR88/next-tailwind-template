7d485900ce17a9b623115c1cd4cca92cf0216688
VillageR88 <villager88pl@gmail.com>
2024-02-07 01:03:30 +0100

    Update MainRow3 and MainRow4 components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow3.tsx
+++ app/components/MainRow3.tsx
@@ -4,7 +4,7 @@ import ButtonGetStarted from './ButtonGetStarted';
 const MainRow3 = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-[#FFFFFF] pb-[3em] md:pb-[11.5em] md:pt-[7em]">
-      <h2 className="text-[2.4rem] font-[700] text-[hsl(233,26%,24%)]">What they’ve said</h2>
+      <h2 className="text-[1.9rem] font-[700] text-[hsl(233,26%,24%)] md:text-[2.4rem]">What they’ve said</h2>
       <MultipleItems />
       <div className="mt-[2.5em]">
         <ButtonGetStarted />
--- app/components/MainRow4.tsx
+++ app/components/MainRow4.tsx
@@ -2,8 +2,8 @@ import ButtonGetStarted from './ButtonGetStarted';
 
 const MainRow4 = () => {
   return (
-    <div className="flex h-[13.75em] w-full items-center justify-between bg-[hsl(12,88%,59%)] px-6 md:px-[2em] lg:px-[5em] min-[1458px]:px-[10.3em]">
-      <h2 className="w-[12em] text-[2.4rem] font-[500] leading-[1.2em] tracking-[0.001em] text-[white]">
+    <div className="flex w-full flex-col items-center justify-center gap-[1.5em] bg-[hsl(12,88%,59%)] px-6 py-[6em] md:h-[13.75em] md:flex-row md:justify-between md:px-[2em] md:py-0 lg:px-[5em] min-[1458px]:px-[10.3em]">
+      <h2 className="text-center text-[2.4rem] font-[500] leading-[1.2em] tracking-[0.001em] text-[white] md:w-[12em] md:text-left">
         Simplify how your team works today.
       </h2>
       <ButtonGetStarted inverted />
