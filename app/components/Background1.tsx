d24bee13d303f3770b443c79da4771b25fbc0bf5
VillageR88 <villager88pl@gmail.com>
2024-02-07 12:54:59 +0100

    Update Background1 component styling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Background1.tsx
+++ app/components/Background1.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 const Background1 = () => {
   return (
     <div className="top-0 flex h-0 w-full justify-end">
-      <div className="flex h-full w-[80%] xl:w-[69%] ">
+      <div className="flex h-full w-full md:w-[80%] xl:w-[69%] ">
         <Image
-          className="z-20 ml-[18.5em] mt-[-9em] flex h-[50em] w-fit lg:ml-[27em] xl:ml-[20.5em] "
+          className="z-20 ml-[5em] mt-[-1em] flex h-fit w-fit md:ml-[18.5em] md:mt-[-9em] md:h-[50em] lg:ml-[27em] xl:ml-[20.5em] "
           src={'images/bg-tablet-pattern.svg'}
           width={10}
           height={10}
