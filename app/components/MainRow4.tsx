3a7435cd7daaf8e8079fe49148b7e707f150a435
VillageR88 <villager88pl@gmail.com>
2024-02-10 00:54:59 +0100

    Update background image and text styles in MainRow4 component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow4.tsx
+++ app/components/MainRow4.tsx
@@ -2,8 +2,10 @@ import ButtonGetStarted from './ButtonGetStarted';
 
 const MainRow4 = () => {
   return (
-    <div className="flex h-[15.6em] w-full flex-col items-center justify-center gap-[1.4em] bg-[#3A3053] bg-[url('../public/images/bg-boost-desktop.svg')] bg-center">
-      <h2 className="text-[2.5rem] font-[700] tracking-[-0.025em] text-[white]">Boost your links today</h2>
+    <div className="flex h-[15.6em] w-full flex-col items-center justify-center gap-[1.4em] bg-[#3A3053] bg-[url('../public/images/bg-boost-mobile.svg')] bg-cover bg-center md:bg-[url('../public/images/bg-boost-desktop.svg')]">
+      <h2 className="text-center text-[1.7rem] font-[700] tracking-[-0.025em] text-[white] md:text-[2.5rem]">
+        Boost your links today
+      </h2>
       <ButtonGetStarted />
     </div>
   );
