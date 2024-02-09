5f2324a74b1c4c93783a2785629310839191ac12
VillageR88 <villager88pl@gmail.com>
2024-02-09 15:18:18 +0100

    Refactor page component structure
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/components/Main.tsx
@@ -0,0 +1,12 @@
+import MainRow1 from './MainRow1';
+import MainRow2 from './MainRow2';
+
+const Main = () => {
+  return (
+    <main className="flex h-full w-full flex-col items-center">
+      <MainRow1 />
+      <MainRow2 />
+    </main>
+  );
+};
+export default Main;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import NavBar from './components/NavBar';
-import MainRow1 from './components/MainRow1';
-import MainRow2 from './components/MainRow2';
+import Main from './components/Main';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center font-poppins">
+    <div className="flex min-h-screen flex-col items-center justify-center font-poppins">
       <NavBar />
-      <MainRow1 />
-      <MainRow2 />
+      <Main />
+
       <div className="flex h-[60em] w-full items-center justify-center bg-transparent"></div>
-    </main>
+    </div>
   );
 }
