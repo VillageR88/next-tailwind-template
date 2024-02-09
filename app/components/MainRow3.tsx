b859c781464e479d0d7320b908acc03b0065aeed
VillageR88 <villager88pl@gmail.com>
2024-02-10 00:33:43 +0100

    Refactor MainRow3 component styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow3.tsx
+++ app/components/MainRow3.tsx
@@ -21,25 +21,27 @@ const MainRow3 = () => {
     },
   ];
   return (
-    <div className="flex h-full w-full flex-col items-center bg-[#F0F1F6] pb-[7.5em] md:px-[2em] lg:px-[5em] min-[1458px]:px-[10.3em]">
+    <div className="flex h-full w-full flex-col items-center bg-[#F0F1F6] px-6 pb-[5em] md:px-[2em] md:pb-[7.5em] lg:px-[5em] min-[1458px]:px-[10.3em]">
       <section className="flex flex-col items-center gap-[0.7em] ">
-        <h2 className="text-[2.5rem] font-[700] tracking-[-0.025em] text-[#34313C]">Advanced Statistics</h2>
-        <p className="max-w-[30em] text-center text-[1.12rem] leading-8 tracking-[0.008em] text-[#A5A4A9]">
+        <h2 className="text-center text-[1.7rem] font-[700] tracking-[-0.025em] text-[#34313C] md:text-[2.5rem]">
+          Advanced Statistics
+        </h2>
+        <p className="max-w-[30em] text-center leading-8 tracking-[0.008em] text-[#A5A4A9] md:text-[1.12rem]">
           Track how your links are performing across the web with our advanced statistics dashboard.
         </p>
       </section>
       <div className="h-full w-full ">
-        <div className="flex h-0 w-full">
-          <div className="mt-[15.5em] h-[0.5em] w-full bg-[#2ED1D8]"></div>
+        <div className="flex h-0 w-full justify-center">
+          <div className="mt-[15.5em] h-[40em] w-[0.5em] bg-[#2ED1D8] md:h-[0.5em] md:w-full"></div>
         </div>
-        <ul className="mt-[6.3em] flex w-full justify-center gap-[1.88em]">
+        <ul className="mt-[6.3em] flex w-full flex-col justify-center gap-[1.88em] md:flex-row">
           {listItems.map((item, index) => (
             <li
               style={{ marginTop: index * 2.8 + 'em' }}
-              className="flex w-full flex-col gap-[1em] rounded-[0.3em] bg-[white] px-[1.95em] pb-[2.75em] md:h-[27em] min-[900px]:h-[23em] min-[1100px]:h-[18em] min-[1458px]:h-fit"
+              className="flex w-full flex-col items-center gap-[1em] rounded-[0.3em] bg-[white] px-[1.95em] pb-[2.75em] md:h-[27em] md:items-start min-[900px]:h-[23em] min-[1100px]:h-[18em] min-[1458px]:h-fit"
               key={index}
             >
-              <div className="mt-[-3em] flex h-[88px] w-[88px] items-center justify-center  rounded-full bg-[#3A3053]">
+              <div className="mt-[-3em] flex h-[88px] w-[88px] items-center justify-center rounded-full bg-[#3A3053]  text-center md:text-left">
                 <Image
                   width={index !== 2 ? 40 : 50}
                   height={index !== 2 ? 40 : 50}
@@ -48,7 +50,9 @@ const MainRow3 = () => {
                 ></Image>
               </div>
               <h3 className="mt-[0.8em] text-[1.4rem] font-[700] text-[#34313C]">{item.title}</h3>
-              <p className="scale-y-105 text-[0.93rem] leading-[1.6em] text-[#A5A4A9]">{item.description}</p>
+              <p className="scale-y-105 text-center text-[0.93rem] leading-[1.6em] text-[#A5A4A9] md:text-left">
+                {item.description}
+              </p>
             </li>
           ))}
         </ul>
