2b7de57a2241bd0d676651f9e696d36ff0fc2876
VillageR88 <villager88pl@gmail.com>
2024-02-12 00:52:06 +0100

    initial MainRow4
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/components/Main/MainRow4.tsx
@@ -0,0 +1,15 @@
+const MainRow4 = () => {
+  return (
+    <div className="flex flex-col items-center bg-white pt-[7em]">
+      <section className="flex w-full flex-col items-center gap-[1.2em] text-center md:w-[33em]">
+        <h2 className="text-[1.98rem] font-[500] text-[hsl(229,31%,21%)]">Frequently Asked Questions</h2>
+        <p className="text-[0.92rem] leading-[1.6em] text-[hsl(229,8%,60%)] md:text-[1.13rem]">
+          Here are some of our FAQs. If you have any other questions you’d like answered please feel free to email us.
+        </p>
+      </section>
+      <section className="flex w-full flex-col items-center gap-[1.2em] text-center md:w-[38em]"></section>
+    </div>
+  );
+};
+
+export default MainRow4;
