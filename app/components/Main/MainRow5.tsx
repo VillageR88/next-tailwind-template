c8c3907f03a88b1e2c749ede42487fee7aeb1249
VillageR88 <villager88pl@gmail.com>
2024-02-12 21:32:12 +0100

    Refactor email input and add error handling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Main/MainRow5.tsx
+++ app/components/Main/MainRow5.tsx
@@ -1,17 +1,24 @@
 'use client';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const MainRow5 = () => {
-  const errorText = "Whoops, make sure it's an email";
-  enum EmailPhase {
-    Typing,
-    Empty,
-    Invalid,
+  enum EmailStatus {
     Valid,
+    Invalid,
+    Typing,
   }
-  const [emailPhase, setEmailPhase] = useState(EmailPhase.Typing);
-
-  const [emailText, setEmailText] = useState('');
+  const errorText = {
+    [EmailStatus.Valid]: 'Thank you for subscribing!',
+    [EmailStatus.Invalid]: "Whoops, make sure it's an email",
+    [EmailStatus.Typing]: '',
+  };
+  const [emailStatus, setEmailStatus] = useState(EmailStatus.Typing);
+  const [email, setEmail] = useState<string>('');
+  const checkEmail = (email: string) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+  };
   return (
     <div className="flex h-[22.5em] flex-col items-center bg-[#5266DF]">
       <section className="mt-[4.3em] flex w-full flex-col items-center gap-[2.15em] text-center font-[500] md:w-[33em]">
@@ -20,19 +27,60 @@ const MainRow5 = () => {
           Stay up-to-date with what we’re doing
         </h2>
         <form className="mt-[0.18em] flex gap-[1.12em]" action="">
-          <div className="mt-[-2px] h-[4.35em] w-[18.725em] rounded-[0.3em] border-[2px] border-[#DC6465] bg-[#FB5859]">
+          <div
+            className={` h-[4.35em] w-[18.725em] rounded-[0.3em]  ${
+              emailStatus === EmailStatus.Invalid &&
+              'mt-[-2px]  scale-[101.2%] border-[2px] border-[#FB5859] bg-[#FB5859]'
+            } `}
+          >
             <input
-              value={emailText}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  if (checkEmail(email)) {
+                    setEmailStatus(EmailStatus.Valid);
+                  } else {
+                    setEmailStatus(EmailStatus.Invalid);
+                  }
+                } else {
+                  setEmailStatus(EmailStatus.Typing);
+                }
+              }}
+              value={email}
               onChange={(e) => {
-                setEmailText(e.target.value);
+                setEmail(e.target.value);
               }}
               placeholder="Enter your email address"
-              className="h-[2.75em] w-full rounded-[0.2em]"
-              autoComplete=""
+              className="h-[3em] w-full rounded-[0.2em] px-[1em] text-[0.9rem] font-[400] outline-none placeholder:text-[#e2e1e1]"
+              autoComplete="email"
               type="email"
             />
+            <div className="flex h-full w-full items-start justify-between">
+              <span className="ml-2 mt-[4px] text-[0.6rem] font-[400] italic tracking-[0.04em] text-[white]">
+                {errorText[emailStatus]}
+              </span>
+              <div className="mr-[1em] mt-[-2em]">
+                <Image
+                  className={`${emailStatus === EmailStatus.Invalid ? 'block' : 'hidden'} h-fit w-fit`}
+                  src={'./images/icon-error.svg' as string}
+                  alt="error icon"
+                  width={10}
+                  height={10}
+                />
+              </div>
+            </div>
           </div>
+
           <button
+            onClick={() => {
+              if (checkEmail(email)) {
+                setEmailStatus(EmailStatus.Valid);
+              } else {
+                setEmailStatus(EmailStatus.Invalid);
+              }
+            }}
+            aria-label="Contact Us"
+            type="button"
             className={`mr-[-0.15em] h-[3.5em] w-[9.4em] rounded-[0.3em] border-2 border-[#FB5859] bg-[#FB5859] text-[0.82rem] font-[500] tracking-widest text-[white] transition hover:border-[#DC6465] hover:bg-[white] hover:text-[#DC6465]`}
           >
             Contact Us
