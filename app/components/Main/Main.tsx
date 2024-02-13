9bbb3ff3ccceee02a7b9f3e146908125cb36f765
VillageR88 <villager88pl@gmail.com>
2024-02-13 22:51:22 +0100

    Add Section1 component and update Main styling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -1,8 +1,9 @@
+import Section1 from './Section1';
 import Image from 'next/image';
 const Main = () => {
   return (
     <div className="mt-[7.68em] flex w-[46em] flex-col items-center justify-start rounded-[1.5em] bg-white">
-      <div className="flex h-[70em] w-full items-start justify-center px-[2.5em] pt-[2.5em]">
+      <div className="flex h-[70em] w-full flex-col items-start justify-start px-[2.5em] pt-[2.5em]">
         <Image
           className="h-fit w-fit rounded-[0.7em]"
           src={'./assets/images/image-omelette.jpeg'}
@@ -10,6 +11,12 @@ const Main = () => {
           width={500}
           height={500}
         />
+        <h1 className="font-youngSerif mt-[0.79em] text-[2.5rem] text-[hsl(24,5%,18%)]">Simple Omelette Recipe</h1>
+        <p className="font-outfit mt-[0.8em] text-[hsl(30,10%,34%)]">
+          An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection,
+          optionally filled with your choice of cheese, vegetables, or meats.
+        </p>
+        <Section1 />
       </div>
     </div>
   );
