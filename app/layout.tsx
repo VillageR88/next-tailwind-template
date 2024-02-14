736581619df876b3358fa3959d61ee155f948105
VillageR88 <villager88pl@gmail.com>
2024-02-14 23:18:45 +0100

    initial commit not working yet
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           .map((font) => font.variable)
           .join(' ')
           .concat(' ')
-          .concat('bg-[#0E0E0E]')}
+          .concat('bg-[#FAFAFA]')}
       >
         <div className="ml-auto mr-auto max-w-[90em] overflow-x-clip font-sans">{children}</div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex h-[50em] w-full items-center justify-center bg-blue-400"></div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
+      <Login />
+    </div>
   );
 }
