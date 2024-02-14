8e46e20752df921692e3753ec6d2daa1be5d87b5
VillageR88 <villager88pl@gmail.com>
2024-02-14 11:36:00 +0100

    mobile completed
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -6,24 +6,26 @@ import Image from 'next/image';
 
 const Main = () => {
   return (
-    <div className="mb-[7.75em] mt-[7.68em] flex w-[46em] flex-col items-center justify-start rounded-[1.5em] bg-white">
-      <div className="flex h-full w-full flex-col items-start justify-start px-[2.5em] pb-[2.5em] pt-[2.5em]">
+    <div className="flex flex-col items-center justify-start bg-white md:mb-[7.75em] md:mt-[7.68em] md:w-[46em] md:rounded-[1.5em]">
+      <div className="flex h-full w-full flex-col items-start justify-start pb-[2.5em] md:px-[2.5em] md:pt-[2.5em]">
         <Image
-          className="h-fit w-fit rounded-[0.7em]"
+          className="h-fit w-fit md:rounded-[0.7em]"
           src={'./assets/images/image-omelette.jpeg'}
           alt="image of omelette"
           width={500}
           height={500}
         />
-        <h1 className="mt-[0.79em] font-youngSerif text-[2.5rem] text-[hsl(24,5%,18%)]">Simple Omelette Recipe</h1>
-        <p className="mt-[0.8em] font-outfit text-[hsl(30,10%,34%)]">
-          An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection,
-          optionally filled with your choice of cheese, vegetables, or meats.
-        </p>
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
+        <div className="flex flex-col px-[1.6em] md:px-0">
+          <h1 className="mt-[0.79em] font-youngSerif text-[2.5rem] text-[hsl(24,5%,18%)]">Simple Omelette Recipe</h1>
+          <p className="mt-[0.8em] font-outfit text-[hsl(30,10%,34%)]">
+            An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to
+            perfection, optionally filled with your choice of cheese, vegetables, or meats.
+          </p>
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+        </div>
       </div>
     </div>
   );
--- app/components/Main/Section1.tsx
+++ app/components/Main/Section1.tsx
@@ -7,7 +7,7 @@ const Section1 = () => {
   return (
     <div className="mt-[1.9em] flex w-full flex-col items-start justify-start rounded-[0.8em] bg-[hsl(330,100%,98%)] px-[1.75em] pb-[1.75em] pt-[1.5em] font-outfit">
       <h2 className="text-[1.255rem] font-[600] text-[hsl(332,51%,32%)]">Preparation time</h2>
-      <ul className="custom-list violetLi brownLi mt-[0.7em] flex flex-col gap-[0.5em] text-[hsl(30,10%,34%)]">
+      <ul className="custom-list violetLi brownLi mt-[0.7em] flex flex-col gap-[0.5em] text-[hsl(30,10%,34%)] pr-[2em]">
         {articleItems.map((item, index) => (
           <li key={index}>
             <span className="font-[700]">{item.title}</span>
