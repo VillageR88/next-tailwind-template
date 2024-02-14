fecbe2374f736bd47790bc73fc5c059573f47738
VillageR88 <villager88pl@gmail.com>
2024-02-14 23:59:40 +0100

    Delete unused files and update page styling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+
 const Login = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
       <div className="flex h-[573px] w-[476px] flex-col items-center justify-between ">
-        <div>ff</div>
-        <div className="h-[482px] w-full bg-[white]"></div>
+        <Image
+          className="h-fit w-fit"
+          width={10}
+          height={10}
+          src={'./assets/images/logo-devlinks-large.svg' as string}
+          alt="logo"
+        />
+        <div className="flex h-[482px] w-full flex-col justify-between rounded-[12px] bg-[white] px-[40px] py-[40px]">
+          <div className="h-[80px] w-full">DIV1</div>
+          <div className="h-[282px] w-full">DIV2</div>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Login from './login/page';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
+    <div className="font-instrumentSans flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
       <Login />
     </div>
   );
