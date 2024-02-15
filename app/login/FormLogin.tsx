4eb103c98f56374f48fda59392b841a2c199e3c8
VillageR88 <villager88pl@gmail.com>
2024-02-15 13:02:25 +0100

    Update login form styling and add password input field
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/login/FormLogin.tsx
+++ app/login/FormLogin.tsx
@@ -6,7 +6,8 @@ const FormLogin = () => {
           Email address
         </label>
         <input
-          className="h-full w-full rounded-[8px] border bg-[url('../public/assets/images/icon-email.svg')] bg-[length:20px_20px] bg-[10px_center] bg-no-repeat pl-[40px]"
+          className="h-full w-full rounded-[8px] bg-[url('../public/assets/images/icon-email.svg')] bg-[length:16px_16px] bg-[16px_center] bg-no-repeat pl-[44px] outline outline-1 outline-[#D9D9D9] focus:outline-[#633CFF]"
+          placeholder="e.g. alex@email.com"
           type="email"
           name="email"
           id="email"
@@ -14,7 +15,20 @@ const FormLogin = () => {
           required
         />
       </div>
-      <div>subdiv2</div>
+      <div className="flex h-[70px] flex-col justify-between gap-[4px]">
+        <label className="bodyS h-[18px]" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="h-full w-full rounded-[8px] bg-[url('../public/assets/images/icon-password.svg')] bg-[length:16px_16px] bg-[16px_center] bg-no-repeat pl-[44px] outline outline-1 outline-[#D9D9D9] focus:outline-[#633CFF]"
+          placeholder="Enter your password"
+          type="password"
+          name="password"
+          id="password"
+          aria-required="true"
+          required
+        />
+      </div>{' '}
       <button type="submit">Login</button>
       <span>text</span>
     </form>
