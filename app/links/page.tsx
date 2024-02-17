42b2010dc1e2fa566a61521b233a1cbab6c309e0
VillageR88 <villager88pl@gmail.com>
2024-02-17 13:50:36 +0100

    Fix import paths and add user authentication check
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,21 +1,36 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import supabase from '../lib/supabaseClient';
+import { useRouter } from 'next/navigation'; // Correct import path for useRouter
+import { useEffect, useState } from 'react';
+import supabase from '../lib/supabaseClient'; // Ensure correct import for supabase
 
 export default function Links() {
+  const [userAuth, setUserAuth] = useState<boolean>(false);
   const router = useRouter();
 
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (user) setUserAuth(true);
+      else router.replace('/login');
+    };
+
+    void checkUser();
+  }, [router]);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
-      <div className="flex h-[709px] w-[476px] flex-col items-center justify-between ">
-        <div className="flex h-[618px] w-full flex-col justify-between rounded-[12px] bg-[white] px-[40px] py-[40px]">
-          <section className="flex h-[80px] w-full flex-col justify-between">
-            <h1 className="headingM">Links</h1>
-            <p className="bodyM text-[#737373]">Let’s get you started sharing your links!</p>
-          </section>
+    userAuth && (
+      <div className="flex min-h-screen flex-col items-center justify-center bg-[#FAFAFA]">
+        <div className="flex h-[709px] w-[476px] flex-col items-center justify-between ">
+          <div className="flex h-[618px] w-full flex-col justify-between rounded-[12px] bg-[white] px-[40px] py-[40px]">
+            <section className="flex h-[80px] w-full flex-col justify-between">
+              <h1 className="headingM">Links</h1>
+              <p className="bodyM text-[#737373]">Let’s get you started sharing your links!</p>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
