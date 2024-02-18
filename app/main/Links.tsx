039133fba3cde5672a33d5fef0f06a53efc5d8eb
VillageR88 <villager88pl@gmail.com>
2024-02-18 22:17:18 +0100

    Add username placeholders for social media links
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/main/Links.tsx
+++ app/main/Links.tsx
@@ -21,6 +21,23 @@ enum SocialMedia {
   stackOverflow = 'Stack Overflow',
 }
 
+const typicalUsername = 'johnappleseed';
+const urlPlaceholders = {
+  Github: `https://www.github.com/${typicalUsername}`,
+  'Frontend Mentor': `https://www.frontendmentor.io/profile/${typicalUsername}`,
+  Twitter: `https://www.twitter.com/${typicalUsername}`,
+  LinkedIn: `https://www.linkedin.com/in/${typicalUsername}`,
+  Youtube: `https://www.youtube.com/user/${typicalUsername}`,
+  Facebook: `https://www.facebook.com/${typicalUsername}`,
+  Twitch: `https://www.twitch.tv/${typicalUsername}`,
+  'Dev.to': `https://dev.to/${typicalUsername}`,
+  Codewars: `https://www.codewars.com/users/${typicalUsername}`,
+  freeCodeCamp: `https://www.freecodecamp.org/${typicalUsername}`,
+  GitLab: `https://gitlab.com/${typicalUsername}`,
+  Hashnode: `https://hashnode.com/@${typicalUsername}`,
+  'Stack Overflow': `https://stackoverflow.com/users/${typicalUsername}`,
+};
+
 interface Link {
   id: number;
   title: SocialMedia;
@@ -201,7 +218,7 @@ const Links = () => {
                         <label className="bodyS">Link</label>
                         <input
                           type="text"
-                          placeholder="https://"
+                          placeholder={urlPlaceholders[item.title as keyof typeof urlPlaceholders]}
                           value={item.url}
                           onChange={(e) => {
                             const newLinks = [...links];
