347b65af9ca0af16bb6354b2cacd370c8e01c472
VillageR88 <villager88pl@gmail.com>
2024-02-19 01:17:21 +0100

    Update Frontend Mentor icon color
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/main/Phone.tsx
+++ app/main/Phone.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const dataColors = {
   Github: ['/assets/images/icon-githubWhite.svg', 'bg-[#1A1A1A]'],
-  'Frontend Mentor': ['/assets/images/icon-frontend-mentorWhite.svg', 'bg-[#FFFFFF]'],
+  'Frontend Mentor': ['/assets/images/icon-frontend-mentorColor.svg', 'bg-[#FFFFFF]'],
   Twitter: ['/assets/images/icon-twitterWhite.svg', 'bg-[#43B7E9]'],
   LinkedIn: ['/assets/images/icon-linkedinWhite.svg', 'bg-[#2D68FF]'],
   Youtube: ['/assets/images/icon-youtubeWhite.svg', 'bg-[#EE3939]'],
@@ -28,15 +28,18 @@ const Phone = ({ socialInfo }: { socialInfo?: string[] }) => {
                 key={index}
                 className={`min-h-[44px] w-full rounded-[8px] ${dataColors[item as keyof typeof dataColors][1]}`}
               >
-                <div className="flex h-full w-full items-center fill-white *:fill-white *:text-white">
+                <div
+                  className={`bodyS flex h-full w-full items-center ${
+                    item !== 'Frontend Mentor' ? 'text-[white]' : 'rounded-[8px] border border-[#D9D9D9] text-[333333]'
+                  }`}
+                >
                   <Image
                     color="inherit"
-                    style={{ fill: 'white', color: 'inherit' }}
                     src={dataColors[item as keyof typeof dataColors][0]}
                     alt={item}
                     width={10}
                     height={10}
-                    className="fill-initial ml-[16px] mr-[16px] h-[16px] w-[16px] fill-inherit text-inherit"
+                    className="ml-[16px] mr-[16px] h-[16px] w-[16px] fill-inherit text-inherit"
                   />
 
                   <span>{item}</span>
