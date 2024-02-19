ab7323d1ce660dfc3d3313cf8f8b1a666cef261b
VillageR88 <villager88pl@gmail.com>
2024-02-19 19:56:18 +0100

    Add social media icons
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/main/Links.tsx
+++ app/main/Links.tsx
@@ -8,9 +8,33 @@ import SocialMedia from '../lib/enumSocialMedia';
 import Link from '../lib/interfaceLink';
 import urlPlaceholders from '../lib/urlPlaceholders';
 import Github from '../components/socialIcons/Github';
+import FrontendMentor from '../components/socialIcons/FrontendMentor';
+import Twitter from '../components/socialIcons/Twitter';
+import LinkedIn from '../components/socialIcons/LinkedIn';
+import Youtube from '../components/socialIcons/Youtube';
+import Facebook from '../components/socialIcons/Facebook';
+import Twitch from '../components/socialIcons/Twitch';
+import DevTo from '../components/socialIcons/DevTo';
+import Codewars from '../components/socialIcons/Codewars';
+import FreeCodeCamp from '../components/socialIcons/FreeCodeCamp';
+import GitLab from '../components/socialIcons/GitLab';
+import Hashnode from '../components/socialIcons/Hashnode';
+import StackOverflow from '../components/socialIcons/StackOverflow';
 
 const socialIconsAccess = {
   Github: <Github />,
+  'Frontend Mentor': <FrontendMentor />,
+  Twitter: <Twitter />,
+  LinkedIn: <LinkedIn />,
+  Youtube: <Youtube />,
+  Facebook: <Facebook />,
+  Twitch: <Twitch />,
+  'Dev.to': <DevTo />,
+  Codewars: <Codewars />,
+  freeCodeCamp: <FreeCodeCamp />,
+  GitLab: <GitLab />,
+  Hashnode: <Hashnode />,
+  'Stack Overflow': <StackOverflow />,
 };
 
 const Links = ({
