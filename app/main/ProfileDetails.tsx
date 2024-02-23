6d8261d4725a8626ff6ce752acf7fe90679f80f5
VillageR88 <villager88pl@gmail.com>
2024-02-24 00:44:16 +0100

    Update ProfileDetails component styling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/main/ProfileDetails.tsx
+++ app/main/ProfileDetails.tsx
@@ -1,12 +1,15 @@
 const ProfileDetails = ({ visible }: { visible: boolean }) => {
   return (
     <div className={`${visible ? 'flex' : 'hidden'}  h-full w-full flex-col items-center justify-center`}>
-      <div className="flex h-[739px] w-full flex-col justify-between p-[40px]">
+      <div className="flex h-[739px] w-full flex-col justify-start gap-[40px] p-[40px]">
         <div className="flex h-[80px] w-full flex-col justify-between">
           <h1 className="headingM text-[#333333]">Profile Details</h1>
           <p className="text-[#737373]">Add your details to create a personal touch to your profile.</p>
         </div>
-        <div className="flex h-[539px] w-full flex-col justify-between"></div>
+        <div className="flex h-[465px] w-full flex-col justify-between">
+          <div className="h-[233px] w-full rounded-[12px] bg-[#FAFAFA]"></div>
+          <div className="h-[208px] w-full rounded-[12px] bg-[#FAFAFA]"></div>
+        </div>
       </div>
       <div className="h-[95px] w-full ">
         <hr className="border-[#D9D9D9]" />
