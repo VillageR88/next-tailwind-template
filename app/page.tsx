694b0a49f3c736bca7c2891f3a746926f189ed07
VillageR88 <villager88pl@gmail.com>
2024-03-03 00:54:15 +0100

    Add FAQ section and mobile background image
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,65 @@
+'use client';
 import Image from 'next/image';
 import backgroundImageDesktop from '../public/assets/images/background-pattern-desktop.svg';
+import backgroundImageMobile from '../public/assets/images/background-pattern-mobile.svg';
 import iconStar from '../public/assets/images/icon-star.svg';
+import articleItems from './lib/articleItems';
+import iconPlus from '../public/assets/images/icon-plus.svg';
+import iconMinus from '../public/assets/images/icon-minus.svg';
+import { useState } from 'react';
+
 export default function Home() {
-  const articleItems = [
-    {
-      question: 'What is Frontend Mentor, and how will it help me?',
-      answer:
-        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
-    },
-    {
-      question: 'Is Frontend Mentor free?',
-      answer:
-        'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
-    },
-    {
-      question: 'Can I use Frontend Mentor projects in my portfolio?',
-      answer:
-        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
-    },
-    {
-      question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
-      answer:
-        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
-    },
-  ];
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [question, setQuestion] = useState<boolean[]>([false, false, false, false]);
   return (
     <main className="font-workSans flex min-h-screen flex-col items-center justify-center">
-      <Image width={1440} height={320} src={backgroundImageDesktop as string} alt="background image" />
-      <div className="flex min-h-[580px] w-full items-start justify-center bg-[hsl(275,100%,97%)]">
-        <div className="mt-[-152px] min-h-[564px] w-[600px] rounded-[15px] bg-white px-[40px] pt-[32px]">
+      <Image
+        className="min-h-[320px] w-full object-cover"
+        width={1440}
+        height={320}
+        src={backgroundImageDesktop as string}
+        alt="background image"
+        priority
+      />
+      <div className="flex min-h-[calc(100vh-320px)] w-full items-start justify-center bg-[hsl(275,100%,97%)] pb-[24px]">
+        <div className="mt-[-152px] min-h-fit w-[600px] rounded-[15px] bg-white px-[40px] py-[20px] pt-[32px]">
           <section className="flex items-center gap-[24px]">
             <Image className="h-fit w-[41px]" width={40} height={41} src={iconStar as string} alt="star icon" />
             <h1>FAQs</h1>
           </section>
-          <ul></ul>
+          <ul>
+            {articleItems.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className={`${
+                    articleItems.length !== index + 1 && 'border-b-[1px] border-[hsl(240,6%,92%)]'
+                  } flex w-full flex-col py-[24px]`}
+                >
+                  <button
+                    onClick={() => {
+                      const newQuestion = [...question];
+                      newQuestion[index] = !newQuestion[index];
+                      setQuestion(newQuestion);
+                    }}
+                    className="relative z-10 flex items-center justify-between text-left"
+                  >
+                    <h2 className="font-bold">{item.question}</h2>
+                    <Image
+                      className="h-fit w-[30px]"
+                      width={24}
+                      height={24}
+                      src={(question[index] ? iconMinus : iconPlus) as string}
+                      alt={`${question[index] ? 'minus' : 'plus'} icon`}
+                    />
+                  </button>
+                  <p className={`${!question[index] ? 'h-0 opacity-0' : 'mt-[16px] h-fit opacity-100'} transition-all`}>
+                    {question[index] ? articleItems[index].answer : articleItems[index].answer.slice(0, 50)}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </main>
