145a3ceb0dc86651fb53227c57e39a47d4101546
VillageR88 <villager88pl@gmail.com>
2024-03-03 01:44:07 +0100

    Update styles for headings and paragraphs
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,29 @@ import iconMinus from '../public/assets/images/icon-minus.svg';
 import { useState } from 'react';
 
 export default function Home() {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
   const [question, setQuestion] = useState<boolean[]>([false, false, false, false]);
   return (
-    <main className="font-workSans flex min-h-screen flex-col items-center justify-center">
-      <Image
-        className="min-h-[320px] w-full object-cover"
-        width={1440}
-        height={320}
-        src={backgroundImageDesktop as string}
-        alt="background image"
-        priority
-      />
-      <div className="flex min-h-[calc(100vh-320px)] w-full items-start justify-center bg-[hsl(275,100%,97%)] pb-[40px]">
-        <div className="mt-[-152px] min-h-fit w-[600px] rounded-[15px] bg-white px-[40px] py-[20px] pt-[32px]">
+    <main className="font-workSans flex min-h-[100dvh] flex-col items-start justify-center sm:min-h-screen sm:items-center">
+      <>
+        <Image
+          className="hidden h-fit min-h-[320px] w-full object-cover sm:block"
+          width={1440}
+          height={320}
+          src={backgroundImageDesktop as string}
+          alt="background image"
+          priority
+        />
+        <Image
+          className="block h-fit min-h-[322px] w-full object-cover object-top sm:hidden"
+          width={1440}
+          height={322}
+          src={backgroundImageMobile as string}
+          alt="background image"
+          priority
+        />
+      </>
+      <div className="flex min-h-[calc(100dvh-322px)] w-full items-start justify-center bg-[hsl(275,100%,97%)] px-[24px] pb-[40px] sm:min-h-[calc(100dvh-320px)]">
+        <div className="mt-[-152px] min-h-fit w-[600px] rounded-[15px] bg-white px-[30px] py-[20px] pt-[32px] sm:px-[40px]">
           <section className="flex items-center gap-[24px]">
             <Image className="h-fit w-[41px]" width={40} height={41} src={iconStar as string} alt="star icon" />
             <h1>FAQs</h1>
@@ -33,7 +42,7 @@ export default function Home() {
                 <li
                   key={index}
                   className={`${
-                    articleItems.length !== index + 1 && 'border-b-[1px] border-[hsl(240,6%,92%)]'
+                    articleItems.length !== index + 1 && 'border-b-2 border-[hsl(240,6%,97%)]'
                   } flex w-full flex-col py-[24px]`}
                 >
                   <button
@@ -42,7 +51,7 @@ export default function Home() {
                       newQuestion[index] = !newQuestion[index];
                       setQuestion(newQuestion);
                     }}
-                    className="relative z-10 flex items-center justify-between text-left"
+                    className="relative z-10 flex items-center justify-between gap-[24px] text-left transition duration-150 hover:text-[#B526F8]"
                   >
                     <h2 className="font-bold">{item.question}</h2>
                     <Image
