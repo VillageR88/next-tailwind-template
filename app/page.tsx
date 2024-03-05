75590acce615100977634753c108800b2d89af42
VillageR88 <villager88pl@gmail.com>
2024-03-05 14:31:47 +0100

    Add Footer component to Home page
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start bg-white font-plusJakartaSans">
       <Nav />
       <Main />
+      <Footer />
     </div>
   );
 }
