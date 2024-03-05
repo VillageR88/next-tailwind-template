0e5999eb24aabf722c25a3091b115694f8ebcc1f
VillageR88 <villager88pl@gmail.com>
2024-03-05 22:05:16 +0100

    Update MainSection1 and MainSection2 components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection1.tsx
+++ app/components/MainSection1.tsx
@@ -1,6 +1,6 @@
 const MainSection1 = () => (
   <div className="flex h-full w-full justify-between">
-    <div className="ml-[165px] mt-[186px] flex h-full w-[457px] flex-col items-start justify-between gap-[40px]">
+    <div className="ml-[39px] mt-[186px] flex h-full w-[457px] flex-col items-start justify-between gap-[40px] lg:ml-[80px] xl:ml-[165px]">
       <section className="flex flex-col gap-[29px]">
         <h1 className="HeadingXL whitespace-pre-wrap text-[#13183F]">{'Maximize skill,\nminimize budget'}</h1>
         <p className="BodyM w-full max-w-[457px] text-[#83869A]">
@@ -12,7 +12,7 @@ const MainSection1 = () => (
         Get Started
       </button>
     </div>
-    <div className="pointer-events-none mr-[-280px] mt-[-230px] h-[937px] w-[992px] bg-[url('../public/assets/images/image-hero-desktop.webp')]  bg-no-repeat"></div>
+    <div className="pointer-events-none mr-[-590px] mt-[-160px] h-[937px] w-[992px] bg-[url('../public/assets/images/image-hero-tablet.webp')] bg-no-repeat lg:mr-[-200px] lg:bg-[url('../public/assets/images/image-hero-desktop.webp')] xl:mr-[-280px]  xl:mt-[-230px]"></div>
   </div>
 );
 
--- app/components/MainSection2.tsx
+++ app/components/MainSection2.tsx
@@ -1,46 +1,16 @@
 import Image from 'next/image';
-const itemsList = [
-  {
-    description: 'Check out our most popular courses!',
-  },
-  {
-    image: '../assets/images/icon-animation.svg',
-    title: 'Animation',
-    description: 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
-  },
-  {
-    image: '../assets/images/icon-design.svg',
-    title: 'Design',
-    description: 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
-  },
-  {
-    image: '../assets/images/icon-photography.svg',
-    title: 'Photography',
-    description: 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
-  },
-  {
-    image: '../assets/images/icon-crypto.svg',
-    title: 'Crypto',
-    description:
-      'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
-  },
-  {
-    image: '../assets/images/icon-business.svg',
-    title: 'Business',
-    description:
-      'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
-  },
-];
+import itemsList from '../lib/itemList';
+
 const MainSection2 = () => (
   <div className="h-[888px] w-full bg-gradient-to-b from-white to-[#F0F1FF]">
     <ul className="grid min-h-[748px] w-full grid-cols-3 flex-col gap-x-[30px] gap-y-[56px] px-[165px]">
       {itemsList.map((item, index) =>
         index > 0 ? (
-          <li key={index} className="flex h-[346px] w-[350px] flex-col">
+          <li key={index} className="flex h-[346px] w-full max-w-[350px] flex-col">
             {item.image && (
               <Image className="z-10 mb-[-32px] ml-[32px]" width={56} height={56} src={item.image} alt={item.title} />
             )}
-            <div className="flex h-[322px] w-[350px] flex-col justify-between gap-[24px] rounded-[15px] bg-white px-[32px] pb-[40px] shadow-[0_15px_50px_1px_rgba(6,22,141,0.1)]">
+            <div className="flex h-[322px] w-full max-w-[350px] flex-col justify-between gap-[24px] rounded-[15px] bg-white px-[32px] pb-[40px] shadow-[0_15px_50px_1px_rgba(6,22,141,0.1)]">
               <section className="mt-[64px] flex flex-col gap-[24px]">
                 <h2 className="HeadingS text-[#13183F]">{item.title}</h2>
                 <p className="BodyM text-[#83869A]">{item.description}</p>
@@ -53,7 +23,7 @@ const MainSection2 = () => (
         ) : (
           <li
             key={index}
-            className="HeadingM mt-[24px] h-[322px] w-[350px] rounded-[15px] bg-gradient-to-b from-[#FF6F48] to-[#F02AA6] px-[32px] py-[64px] text-white"
+            className="HeadingM mt-[24px] h-[322px] w-full max-w-[350px] rounded-[15px] bg-gradient-to-b from-[#FF6F48] to-[#F02AA6] px-[32px] py-[64px] text-white"
           >
             {item.description}
           </li>
