52f76cf642c39486dc1104b1fe3a403bd0aca5b4
VillageR88 <villager88pl@gmail.com>
2024-03-05 13:52:42 +0100

    Refactor MainSection2 component and update global styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection2.tsx
+++ app/components/MainSection2.tsx
@@ -32,21 +32,35 @@ const itemsList = [
   },
 ];
 const MainSection2 = () => (
-  <ul className="grid min-h-[888px] w-full grid-cols-3 flex-col bg-gradient-to-b from-white to-[#F0F1FF] px-[165px]">
-    {itemsList.map((item, index) =>
-      index > 0 ? (
-        <li key={index} className="flex h-[346px] w-[350px] flex-col">
-          {item.image && <Image className="z-10 mb-[-32px]" width={56} height={56} src={item.image} alt={item.title} />}
-          <div className="h-[322px] w-[350px] rounded-[15px] bg-white shadow-xl"></div>
-        </li>
-      ) : (
-        <li
-          key={index}
-          className="mt-[24px] h-[322px] w-[322px] rounded-[15px] bg-gradient-to-b from-[#FF6F48] to-[#F02AA6]"
-        ></li>
-      ),
-    )}
-  </ul>
+  <div className="h-[888px] w-full bg-gradient-to-b from-white to-[#F0F1FF]">
+    <ul className="grid min-h-[748px] w-full grid-cols-3 flex-col gap-x-[30px] gap-y-[56px] px-[165px]">
+      {itemsList.map((item, index) =>
+        index > 0 ? (
+          <li key={index} className="flex h-[346px] w-[350px] flex-col">
+            {item.image && (
+              <Image className="z-10 mb-[-32px] ml-[32px]" width={56} height={56} src={item.image} alt={item.title} />
+            )}
+            <div className="flex h-[322px] w-[350px] flex-col justify-between gap-[24px] rounded-[15px] bg-white px-[32px] pb-[40px] shadow-[0_15px_50px_1px_rgba(6,22,141,0.1)]">
+              <section className="mt-[64px] flex flex-col gap-[24px]">
+                <h2 className="HeadingS text-[#13183F]">{item.title}</h2>
+                <p className="BodyM text-[#83869A]">{item.description}</p>
+              </section>
+              <button className="h-[28px] w-[104px] text-[18px] font-bold leading-[28px] text-[#F74780] transition hover:text-[#FFA7C3]">
+                Get Started
+              </button>
+            </div>
+          </li>
+        ) : (
+          <li
+            key={index}
+            className="HeadingM mt-[24px] h-[322px] w-[350px] rounded-[15px] bg-gradient-to-b from-[#FF6F48] to-[#F02AA6] px-[32px] py-[64px] text-white"
+          >
+            {item.description}
+          </li>
+        ),
+      )}
+    </ul>
+  </div>
 );
 
 export default MainSection2;
