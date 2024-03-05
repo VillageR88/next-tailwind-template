9481ed826bc05e5b34fe71a2dc44aa45f2f8c531
VillageR88 <villager88pl@gmail.com>
2024-03-05 22:56:07 +0100

    Update MainSection1 styling
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection1.tsx
+++ app/components/MainSection1.tsx
@@ -1,5 +1,5 @@
 const MainSection1 = () => (
-  <div className="flex h-full w-full justify-between">
+  <div className="flex h-full w-full flex-col justify-between sm:flex-row">
     <div className="ml-[39px] mt-[86px] flex h-full w-[457px] flex-col items-start justify-between gap-[40px] lg:ml-[80px] lg:mt-[240px] xl:ml-[165px] xl:mt-[186px]">
       <section className="flex flex-col gap-[29px]">
         <h1 className="HeadingXL whitespace-pre-wrap text-[#13183F]">{'Maximize skill,\nminimize budget'}</h1>
@@ -12,7 +12,7 @@ const MainSection1 = () => (
         Get Started
       </button>
     </div>
-    <div className="pointer-events-none mt-[-160px] h-[937px] w-[992px] bg-[url('../public/assets/images/image-hero-tablet.webp')] bg-no-repeat mr-[-580px] min-[1024px]:mr-[-50px] min-[1100px]:mr-[-200px] lg:bg-[url('../public/assets/images/image-hero-desktop.webp')] min-[1200px]:mr-[-300px] xl:mr-[-280px] xl:mt-[-230px]"></div>
+    <div className="pointer-events-none mt-[-160px] h-[937px] w-[992px] bg-[url('../public/assets/images/image-hero-tablet.webp')] bg-no-repeat min-[640px]:mr-[-100px] min-[700px]:mr-[-250px] min-[740px]:mr-[-300px] min-[770px]:mr-[-400px] min-[820px]:mr-[-500px] min-[860px]:mr-[-580px] lg:bg-[url('../public/assets/images/image-hero-desktop.webp')] min-[1024px]:mr-[-50px] min-[1100px]:mr-[-200px] min-[1200px]:mr-[-300px] xl:mr-[-280px] xl:mt-[-230px]"></div>
   </div>
 );
 
