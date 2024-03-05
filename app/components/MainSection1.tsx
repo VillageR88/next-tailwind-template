2845290b29edfaff43fcd5a14a614d9fdc787463
VillageR88 <villager88pl@gmail.com>
2024-03-06 00:56:05 +0100

    Refactor MainSection1 and Nav components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection1.tsx
+++ app/components/MainSection1.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+import heroMobile from '@/public/assets/images/image-hero-mobile@2x.webp';
+
 const MainSection1 = () => (
-  <div className="flex h-full w-full flex-col justify-between sm:flex-row">
-    <div className="ml-[39px] mt-[86px] flex h-full w-[457px] flex-col items-start justify-between gap-[40px] lg:ml-[80px] lg:mt-[240px] xl:ml-[165px] xl:mt-[186px]">
+  <div className="flex h-full w-full flex-col items-center justify-between  sm:flex-row sm:items-start">
+    <div className="mt-[48px] flex h-full w-fit max-w-[457px] flex-col items-start justify-between gap-[40px] px-[16px] sm:ml-[39px] sm:mt-[86px] sm:px-0 lg:ml-[80px] lg:mt-[240px] xl:ml-[165px] xl:mt-[186px]">
       <section className="flex flex-col gap-[29px]">
         <h1 className="HeadingXL whitespace-pre-wrap text-[#13183F]">{'Maximize skill,\nminimize budget'}</h1>
         <p className="BodyM w-full max-w-[457px] text-[#83869A]">
@@ -12,7 +15,14 @@ const MainSection1 = () => (
         Get Started
       </button>
     </div>
-    <div className="pointer-events-none mt-[-160px] h-[730px] w-[992px] bg-[url('../public/assets/images/image-hero-tablet.webp')] bg-no-repeat min-[640px]:mr-[-100px] min-[700px]:mr-[-250px] min-[740px]:mr-[-300px] min-[770px]:mr-[-400px] min-[820px]:mr-[-500px] min-[860px]:mr-[-580px] lg:h-[937px] lg:bg-[url('../public/assets/images/image-hero-desktop.webp')] min-[1024px]:mr-[-50px] min-[1100px]:mr-[-200px] min-[1200px]:mr-[-300px] xl:mr-[-280px] xl:mt-[-230px]"></div>
+    <div className="pointer-events-none mr-0 hidden bg-no-repeat sm:mt-[-160px] sm:block sm:h-[730px] sm:w-[992px] sm:bg-[url('../public/assets/images/image-hero-tablet.webp')] min-[640px]:mr-[-100px] min-[700px]:mr-[-250px] min-[740px]:mr-[-300px] min-[770px]:mr-[-400px] min-[820px]:mr-[-500px] min-[860px]:mr-[-580px] lg:h-[937px] lg:bg-[url('../public/assets/images/image-hero-desktop.webp')] min-[1024px]:mr-[-50px] min-[1100px]:mr-[-200px] min-[1200px]:mr-[-300px] xl:mr-[-280px] xl:mt-[-230px]"></div>
+    <Image
+      className="mt-[60px] flex h-full w-full scale-[120%] sm:hidden"
+      width={327}
+      height={301}
+      src={heroMobile}
+      alt="hero image"
+    />
   </div>
 );
 
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import logoDark from '@/public/assets/images/logo-dark.svg';
 
 const Nav = () => (
-  <nav className="mt-[24px] flex w-full items-center justify-between pl-[39px] pr-[40px] lg:pl-[80px] lg:pr-[80px] xl:mt-0 xl:pl-0 xl:pr-0">
+  <nav className="mt-[16px] flex w-full items-center justify-between pl-[16px] pr-[16px] sm:mt-[24px] sm:pl-[39px] sm:pr-[40px] lg:pl-[80px] lg:pr-[80px] xl:mt-0 xl:pl-0 xl:pr-0">
     <Image className="xl:ml-[166px] xl:mt-[39px]" width={112} height={29} src={logoDark as string} alt="skilled logo" />
     <button
       className="h-[56px] w-[167px] rounded-[28px] bg-[#13183F] text-[18px] font-semibold leading-[28px] text-white transition hover:bg-[#666CA3] xl:mr-[165px] xl:mt-[24px]"
