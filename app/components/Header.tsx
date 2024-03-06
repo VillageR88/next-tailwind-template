29fc1f78402a85b9c2e68e3b5bc9f7e5b26f4687
VillageR88 <villager88pl@gmail.com>
2024-03-06 21:41:23 +0100

    Add logo to header
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+import logoIcon from '@/public/assets/images/logo.svg';
+
 const Header = () => (
-  <header>
-    <h1>Header</h1>
+  <header className="flex w-full justify-center">
+    <Image className="ml-[1px] mt-[80px]" width={119} height={28} src={logoIcon as string} alt="meet logo" />
   </header>
 );
 
