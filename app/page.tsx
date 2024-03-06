da38cbc7a0029f8076002d7c6fc6f6f4b1d79e43
VillageR88 <villager88pl@gmail.com>
2024-03-06 22:28:23 +0100

    Add Main component to Home page
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Main from './components/Main';
 
 export default function Home() {
   return (
     <div className="font-redHatDisplay flex min-h-screen flex-col items-center justify-start">
       <Header />
+      <Main />
     </div>
   );
 }
