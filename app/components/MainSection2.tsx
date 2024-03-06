e0628042c291b831e957f88527f0b688054a00d2
VillageR88 <villager88pl@gmail.com>
2024-03-06 12:54:56 +0100

    Add image and background to MainSection2 component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection2.tsx
+++ app/components/MainSection2.tsx
@@ -1,6 +1,17 @@
+import Image from 'next/image';
+import illustrationApp from '@/public/assets/images/illustration-app.png';
+
 const Section2 = () => (
   <div className="z-10 mt-[321px] px-[165px]">
-    <div className="min-h-[600px] w-full rounded-[12px] bg-[#191826]"></div>
+    <div className="flex h-[600px] w-full rounded-[12px] bg-[#191826] bg-[url('../public/assets/images/bg-pattern-2.svg')] bg-[43.1%_-30%] bg-no-repeat">
+      <Image
+        className="ml-[110px] mt-[-213px] h-[642px] w-[312px]"
+        width={369}
+        height={642}
+        src={illustrationApp}
+        alt="illustration"
+      />
+    </div>
   </div>
 );
 
