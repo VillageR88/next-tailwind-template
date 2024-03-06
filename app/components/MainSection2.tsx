88b8741ef74392ccfa7c875a0ea12b90b039b84e
VillageR88 <villager88pl@gmail.com>
2024-03-06 13:21:11 +0100

    Refactor MainSection2 component layout and styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainSection2.tsx
+++ app/components/MainSection2.tsx
@@ -11,7 +11,21 @@ const Section2 = () => (
         src={illustrationApp}
         alt="illustration"
       />
-      <div className="mr-[95px] mt-[90px] h-[625px] w-[446px] rounded-[12px] bg-[#FA7453]"></div>
+      <div className="mr-[95px] mt-[90px] flex h-[625px] w-[446px] flex-col rounded-[12px] bg-[#FA7453] pl-[54px] pr-[59px] pt-[58px]">
+        <section className="flex flex-col gap-[22px]">
+          <h2 className="text-[40px] font-bold leading-[52px] text-[#FCFAF9]">Premium EQ</h2>
+          <p className="text-[20px] leading-[34px] text-[#FCFAF9]">
+            Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take your listening
+            experience to a whole new level and access all our incredible features!
+          </p>
+        </section>
+        <div className="flex items-center gap-[16px] pt-[39px] text-[#FCFAF9]">
+          <span className="text-[65px] font-bold leading-[52px]">$4</span>
+          <span className="text-[20px] leading-[32px]">/ month</span>
+        </div>
+        <button className="mt-[46px] h-[61px] w-[333px] rounded-[12px] bg-[#66E2DC]"></button>
+        <button className="mt-[16px] h-[61px] w-[333px] rounded-[12px] bg-[#FFB964]"></button>
+      </div>
     </div>
   </div>
 );
