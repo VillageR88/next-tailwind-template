51f1bdafbdf742f7af08aa236307e723044a73b2
VillageR88 <villager88pl@gmail.com>
2024-03-07 09:36:39 +0100

    Add Numerator component and update MainPart2 layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart2.tsx
+++ app/components/MainPart2.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import images from '../lib/images';
+import Numerator from './Numerator';
 
 const MainPart2 = () => (
   <div className="mt-[64px] flex h-full w-full flex-col items-center">
@@ -10,6 +11,21 @@ const MainPart2 = () => (
         </li>
       ))}
     </ul>
+    <div className="mt-[80px] flex min-h-[240px] w-full max-w-[540px] flex-col items-center">
+      <span className="text-[16px] font-[900] leading-[26px] tracking-[4px] text-[#4D96A9]">
+        {'Built for modern use'.toLocaleUpperCase()}
+      </span>
+      <section className="mt-[16px] flex flex-col items-center gap-[32px] text-center">
+        <h2 className="max-w-[445px] text-[40px] font-[900] leading-[44px] text-[#28283D]">
+          Smarter meetings, all in one place
+        </h2>
+        <p className="max-w-[525px] text-[18px] font-medium leading-[26px] text-[#87879D]">
+          Send messages, share files, show your screen, and record your meetings — all in one workspace. Control who can
+          join with invite-only team access, data encryption, and data export.
+        </p>
+      </section>
+    </div>
+    <Numerator number={2} additionalClass="mt-[72px]" />
   </div>
 );
 
