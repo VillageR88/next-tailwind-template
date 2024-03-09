53ae96f2547dc4d0d7b5c587e1d37aa6342604d5
VillageR88 <villager88pl@gmail.com>
2024-03-09 22:50:54 +0100

    Update MainForm styling and input validation
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainForm.tsx
+++ app/components/MainForm.tsx
@@ -4,10 +4,13 @@ import RadioButton from './RadioButton';
 
 const MainForm = () => {
   const [selected, setSelected] = useState<1 | 2>(1);
+  const [height, setHeight] = useState<string>('');
+  console.log('height', height);
+
   return (
     <form
       id="MainForm"
-      className="ml-[-266px] flex min-h-[484px] w-[564px] flex-col gap-[32px] rounded-[16px] bg-white p-[32px] shadow-[10px_25px_50px_5px_rgba(179,211,241,0.3)]"
+      className=" ml-[-266px] mt-[166px] flex max-h-[484px] w-[564px] flex-col gap-[32px] rounded-[16px] bg-white  p-[32px] shadow-[10px_25px_50px_5px_rgba(179,211,241,0.3)]"
     >
       <h2 className="Heading2">Enter your details below</h2>
       <div className="flex h-[31px] justify-between gap-[24px]">
@@ -27,9 +30,23 @@ const MainForm = () => {
           </label>
           <div className="flex">
             <input
+              value={height}
+              onChange={(e) => {
+                let newValue = e.target.value;
+                newValue = newValue
+                  .replace(/[^0-9.,]/g, '') // Remove non-numeric characters
+                  .replace(',', '.') // Replace comma with dot
+                  .replace(/(^[.,]+)/g, '') // Remove leading dots or commas
+                  .replace(/(^0+)/g, '') // Remove leading zeros
+                  .replace(/(\..*)\./g, '$1') // Remove multiple dots
+                  .replace(/(\.\d{2})./g, '$1'); // Allow only 2 decimal places
+                if (newValue.length > 3 && !newValue.includes('.')) return; // Allow only 3 digits before the dot
+                setHeight(newValue);
+              }}
               id="height"
               className="Heading2 h-[69px] w-full rounded-[12px] border border-[#D8E2E7] pl-[24px] pr-[100px] text-[#253347] outline-none placeholder:text-opacity-25 focus:border-[#345FF6]"
-              type="string"
+              min={0}
+              max={300}
               placeholder="0"
             />
             <div className="flex h-full w-0 items-center">
@@ -43,9 +60,13 @@ const MainForm = () => {
           </label>
           <div className="flex">
             <input
+              // please use text instead of number and only numbers
+              type="text"
+              inputMode="numeric"
+              // i want only numbers from 0 to 9 no other characters
+              pattern="[0-9]*"
               id="weight"
               className="Heading2 h-[69px] w-full rounded-[12px] border border-[#D8E2E7] pl-[24px] pr-[100px] text-[#253347] outline-none placeholder:text-opacity-25 focus:border-[#345FF6]"
-              type="string"
               placeholder="0"
             />
             <div className="flex h-full w-0 items-center">
@@ -54,7 +75,10 @@ const MainForm = () => {
           </div>
         </div>
       </div>
-      <div className="h-[166px] max-w-[500px] rounded-l-[16px] rounded-r-[200px] bg-gradient-to-r from-[#345FF6] to-[#587DFF]"></div>
+      <div className="flex h-[130px] max-w-[500px] flex-col justify-between rounded-l-[16px] rounded-r-[200px] bg-gradient-to-r from-[#345FF6] to-[#587DFF] p-[32px]">
+        <h2 className="Heading2 text-white">Welcome!</h2>
+        <p className="Body2 text-white">Enter your height and weight and you’ll see your BMI result here</p>
+      </div>
     </form>
   );
 };
--- app/components/MainPart1.tsx
+++ app/components/MainPart1.tsx
@@ -3,8 +3,8 @@ import logo from '@/public/assets/images/logo.svg';
 import MainForm from './MainForm';
 
 const MainPart1 = () => (
-  <div className="ml-[24px] flex min-h-[737px] w-full max-w-[1276px] items-start justify-between self-start">
-    <div className="flex size-full min-h-[737px] max-w-[978px] flex-col rounded-b-[35px] bg-gradient-to-br from-transparent to-[#D6E6FE] pl-[116px]">
+  <div className="flex min-h-[737px] w-full max-w-[1276px] items-start justify-between self-start">
+    <div className="flex size-full min-h-[737px] w-[978px] flex-col rounded-b-[35px] bg-gradient-to-br from-transparent to-[#D6E6FE] pl-[116px]">
       <Image className="mt-[75px]" width={64} height={64} src={logo as string} alt="logo" />
       <section className="mt-[127px] flex min-h-[271px] w-full max-w-[564px] flex-col justify-between">
         <h1 className="Heading1 max-w-[465px] text-[#253347]">Body Mass Index Calculator</h1>
@@ -15,9 +15,7 @@ const MainPart1 = () => (
         </p>
       </section>
     </div>
-    <div className="mt-[166px]">
-      <MainForm />
-    </div>
+    <MainForm />
   </div>
 );
 
