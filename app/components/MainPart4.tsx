dad99e39d962a7784a7c6938c8ebbc9a7eef061a
VillageR88 <villager88pl@gmail.com>
2024-03-12 00:01:35 +0100

    Add age block to MainPart4 component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart4.tsx
+++ app/components/MainPart4.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import iconGender from '@/public/assets/images/icon-gender.svg';
+import iconAge from '@/public/assets/images/icon-age.svg';
 const items = {
   gender: {
     image: iconGender as string,
@@ -7,7 +8,35 @@ const items = {
     description:
       "The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI.",
   },
+  age: {
+    image: iconAge as string,
+    title: 'Age',
+    description:
+      'In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.',
+  },
 };
+
+const Block = ({
+  title,
+  description,
+  image,
+  additionalClass,
+}: {
+  title: string;
+  description: string;
+  image: string;
+  additionalClass: string;
+}) => (
+  <div
+    className={`flex ${additionalClass} w-[365px] flex-col justify-between rounded-[16px] p-[32px] shadow-[10px_25px_50px_5px_rgba(179,211,241,0.3)]`}
+  >
+    <div className="flex items-center gap-[16px]">
+      <Image width={32} height={32} src={image} alt={title} />
+      <h3 className="Heading4 text-[#253347]">{title}</h3>
+    </div>
+    <p className="Body1 text-[#5E6E85]">{description}</p>
+  </div>
+);
 
 const MainPart4 = () => (
   <div className="mt-[120px] flex min-h-[704px] w-full max-w-[1160px] justify-between">
@@ -20,12 +49,19 @@ const MainPart4 = () => (
       </p>
     </div>
     <div className="flex h-[704px] w-[961px] flex-col items-end">
-      <div className="mr-[99px] flex h-[232px] w-[365px] flex-col justify-between rounded-[16px] p-[32px] shadow-[10px_25px_50px_5px_rgba(179,211,241,0.3)]">
-        <div className="flex items-center gap-[16px]">
-          <Image width={32} height={32} src={items.gender.image} alt={items.gender.title} />
-          <h3 className="Heading4 text-[#253347]">{items.gender.title}</h3>
-        </div>
-        <p className="Body1 text-[#5E6E85]">{items.gender.description}</p>
+      <Block
+        additionalClass={'h-[232px]'}
+        title={items.gender.title}
+        image={items.gender.image}
+        description={items.gender.description}
+      />
+      <div className="mt-[32px] flex gap-[32px]">
+        <Block
+          additionalClass={'h-[184px]'}
+          title={items.age.title}
+          image={items.age.image}
+          description={items.age.description}
+        />
       </div>
     </div>
   </div>
