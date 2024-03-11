8738633045c6c421697c6953eb0955de3dec8cfa
VillageR88 <villager88pl@gmail.com>
2024-03-11 23:51:19 +0100

    Add MainPart4 component and update styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart4.tsx
+++ app/components/MainPart4.tsx
@@ -1,6 +1,33 @@
+import Image from 'next/image';
+import iconGender from '@/public/assets/images/icon-gender.svg';
+const items = {
+  gender: {
+    image: iconGender as string,
+    title: 'Gender',
+    description:
+      "The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI.",
+  },
+};
+
 const MainPart4 = () => (
-  <div>
-    <h2>MainPart4</h2>
+  <div className="mt-[120px] flex min-h-[704px] w-full max-w-[1160px] justify-between">
+    <div className="mr-[-365px] flex h-[184px] w-[564px] flex-col justify-between">
+      <h2 className="Heading2 text-[#253347]">Limitations of BMI</h2>
+      <p className="Body1 text-[#5E6E85]">
+        Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific
+        groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial
+        to use.
+      </p>
+    </div>
+    <div className="flex h-[704px] w-[961px] flex-col items-end">
+      <div className="mr-[99px] flex h-[232px] w-[365px] flex-col justify-between rounded-[16px] p-[32px] shadow-[10px_25px_50px_5px_rgba(179,211,241,0.3)]">
+        <div className="flex items-center gap-[16px]">
+          <Image width={32} height={32} src={items.gender.image} alt={items.gender.title} />
+          <h3 className="Heading4 text-[#253347]">{items.gender.title}</h3>
+        </div>
+        <p className="Body1 text-[#5E6E85]">{items.gender.description}</p>
+      </div>
+    </div>
   </div>
 );
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import MainPart1 from './components/MainPart1';
 import MainPart2 from './components/MainPart2';
 import MainPart3 from './components/MainPart3';
+import MainPart4 from './components/MainPart4';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <MainPart1 />
       <MainPart2 />
       <MainPart3 />
+      <MainPart4 />
     </main>
   );
 }
