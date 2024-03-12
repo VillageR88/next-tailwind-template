fcf4b95e41a0e0cbaff0520d748513cc4a8e9c33
VillageR88 <villager88pl@gmail.com>
2024-03-12 21:37:18 +0100

    Update MainPart3 component styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart3.tsx
+++ app/components/MainPart3.tsx
@@ -25,15 +25,15 @@ const listItems = [
 ];
 
 const MainPart3 = () => (
-  <div className="mt-[96px] flex min-h-[503px] w-full justify-center rounded-[35px] bg-gradient-to-br from-transparent via-[rgba(214,252,254,0.2)] to-[rgba(214,230,254,0.3)] px-[24px] lg:min-h-[402px]">
-    <ul className="flex w-full max-w-[768px] flex-col justify-center gap-[32px] pl-6 lg:mt-[60px] lg:min-h-[234px] lg:max-w-[1159px] lg:flex-row lg:gap-[40px] lg:pr-6 xl:mt-[72px] xl:px-0">
+  <div className="mt-[96px] flex min-h-[878px] w-full justify-center rounded-[35px] bg-gradient-to-br from-transparent via-[rgba(214,252,254,0.2)] to-[rgba(214,230,254,0.3)] px-[24px] md:min-h-[503px] lg:min-h-[402px]">
+    <ul className="flex w-full max-w-[768px] flex-col items-center justify-center gap-[40px] pl-6 md:items-start md:gap-[32px] lg:mt-[60px] lg:min-h-[234px] lg:max-w-[1159px] lg:flex-row lg:gap-[40px] lg:pr-6 xl:mt-[72px] xl:px-0">
       {listItems.map((item, index) => (
         <li
           key={index}
-          className="flex min-h-[101px] max-w-[686px] flex-row items-center gap-[40px] lg:min-h-[234px] lg:max-w-[365px] lg:flex-col lg:items-start lg:gap-0"
+          className="flex min-h-[221px] max-w-[375px] flex-col gap-[40px] md:min-h-[101px] md:max-w-[686px] md:flex-row md:items-center lg:min-h-[234px] lg:max-w-[365px] lg:flex-col lg:items-start lg:gap-0"
         >
           <Image width={64} height={64} src={item.image} alt="icon-eating" />
-          <div className="flex max-h-[101px] flex-col justify-between gap-[24px] lg:max-h-[125px] lg:gap-0">
+          <div className="flex flex-col justify-between gap-[24px] md:max-h-[101px] lg:max-h-[125px] lg:gap-0">
             <h3 className="Heading3 text-[#253347] lg:mt-[45px]">{item.title}</h3>
             <p className="Body1 text-[#5E6E85] lg:mt-[24px]">{item.description}</p>
           </div>
