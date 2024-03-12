b09868b4f2e3ac4ff758591659fe77e3d010ef55
VillageR88 <villager88pl@gmail.com>
2024-03-12 10:40:05 +0100

    Update MainPart2 styling and responsive design
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart2.tsx
+++ app/components/MainPart2.tsx
@@ -4,7 +4,7 @@ import curvedLineLeft from '@/public/assets/images/pattern-curved-line-left.svg'
 
 const MainPart2 = () => (
   <div className="flex size-full flex-col items-center">
-    <div className=" flex h-0 w-full max-w-[1160px] justify-end pr-[27px]">
+    <div className="hidden h-0 w-full max-w-[1160px] justify-end pr-[27px] min-[1200px]:flex">
       <Image
         className="mt-[13px] h-[200px] w-[85px]"
         width={85}
@@ -13,9 +13,16 @@ const MainPart2 = () => (
         alt="pattern"
       />
     </div>
-    <div className="mt-[65px] flex min-h-[533px] w-full max-w-[1080px] justify-between xl:max-w-[1160px]">
-      <Image height={533} width={564} src={imageMan} alt="Image of a man eating sushi" priority />
-      <div className="mt-[184px] flex h-[309px] w-[465px] flex-col justify-between">
+    <div className="mt-[65px] flex min-h-[411px] w-full max-w-[841px] justify-between min-[1200px]:min-h-[533px] min-[1200px]:max-w-[1080px] xl:max-w-[1160px]">
+      <Image
+        className="h-[411px] w-[435px] min-[1200px]:h-[533px] min-[1200px]:w-[564px]"
+        height={533}
+        width={564}
+        src={imageMan}
+        alt="Image of a man eating sushi"
+        priority
+      />
+      <div className="mt-[131px] flex min-h-[342px] w-[311px] flex-col justify-between min-[1200px]:mt-[184px] min-[1200px]:h-[309px] min-[1200px]:w-[465px]">
         <h2 className="Heading2 text-[#253347]">What your BMI result means</h2>
         <p className="Body1 text-[#5E6E85]">
           {
