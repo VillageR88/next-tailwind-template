69295bb3f81aacf7a1d03c418490151e7937e6a4
VillageR88 <villager88pl@gmail.com>
2024-03-16 00:35:14 +0100

    Refactor MainBar and MainPart4 components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainBar.tsx
+++ app/components/MainBar.tsx
@@ -3,7 +3,9 @@ import logo from '@/public/assets/images/logo.svg';
 import Button1And2 from './Button1And2';
 
 const MainBar = ({ additionalClass }: { additionalClass?: string }) => (
-  <div className={`${additionalClass} flex h-[64px] w-full max-w-[1111px] items-center justify-between px-4 xl:px-0`}>
+  <div
+    className={`${additionalClass} flex h-[64px] w-full max-w-[1111px] items-center justify-between px-[39px] transition-all xl:px-0`}
+  >
     <Image height={64} width={64} src={logo as string} alt="logo" />
     <Button1And2 type={1} />
   </div>
--- app/components/MainPart4.tsx
+++ app/components/MainPart4.tsx
@@ -1,15 +1,17 @@
 import Button1And2 from './Button1And2';
 
 const MainPart4 = () => (
-  <div className="mt-[88px] flex min-h-[320px] w-full max-w-[1110px] items-center justify-between rounded-[10px] bg-black pl-[57px] pr-[103px]">
-    <section className="flex max-w-[540px] flex-col gap-[25px] text-lightCream">
-      <h2 className="HeadingM">Book a call with me</h2>
-      <p className="BodyM">
-        I&rsquo;d love to have a chat to see how I can help you. The best first step is for us to discuss your project
-        during a free consultation. Then we can move forward from there.
-      </p>
-    </section>
-    <Button1And2 type={2} />
+  <div className="flex w-full items-center justify-center px-[39px]">
+    <div className="mt-[88px] flex min-h-[352px] w-full max-w-[1110px] flex-col items-center justify-center gap-[26px] rounded-[10px] bg-black px-[57px] text-center lg:min-h-[320px] lg:flex-row lg:justify-between lg:gap-8 lg:pr-[80px] lg:text-left xl:pr-[103px]">
+      <section className="flex max-w-[540px] flex-col gap-[25px] text-lightCream">
+        <h2 className="HeadingM">Book a call with me</h2>
+        <p className="BodyM">
+          I&rsquo;d love to have a chat to see how I can help you. The best first step is for us to discuss your project
+          during a free consultation. Then we can move forward from there.
+        </p>
+      </section>
+      <Button1And2 type={2} />
+    </div>
   </div>
 );
 
