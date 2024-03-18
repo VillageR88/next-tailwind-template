7afc188109304aad6d11086c85dc56aba4c02371
VillageR88 <villager88pl@gmail.com>
2024-03-18 16:56:21 +0100

    Refactor MainPart1 component to use Button1and2
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/components/Button1and2.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import arrowRight from '@/public/assets/images/icon-arrow-right.svg';
+import arrowLeft from '@/public/assets/images/icon-arrow-left.svg';
+
+const Button1and2 = ({ type }: { type: 1 | 2 }) => {
+  const typeItems = {
+    1: {
+      text: 'OUR LOCATION',
+      arrow: arrowRight as string,
+      alt: 'arrow right',
+      direction: 'flex-row',
+    },
+    2: {
+      text: 'BACK TO HOME',
+      arrow: arrowLeft as string,
+      alt: 'arrow left',
+      direction: 'flex-row-reverse',
+    },
+  };
+  return (
+    <button className={`group ${typeItems[type].direction} flex h-[72px] w-[260px] transition`}>
+      <div
+        className={`flex h-full w-[204px] items-center justify-center bg-almostBlack font-bigShouldersDisplay text-[20px] font-extrabold tracking-[3.64px] text-white group-hover:bg-gold`}
+      >
+        {typeItems[type].text}
+      </div>
+      <div className={`flex h-full w-[56px] items-center justify-center bg-gold group-hover:bg-almostBlack`}>
+        <Image width={8} height={24} src={typeItems[type].arrow} alt="arrow right" />
+      </div>
+    </button>
+  );
+};
+
+export default Button1and2;
--- app/components/home/MainPart1.tsx
+++ app/components/home/MainPart1.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import arrowRight from '@/public/assets/images/icon-arrow-right.svg';
+import Button1and2 from '../Button1and2';
 
 const MainPart1 = () => (
   <div className="flex size-full h-[800px] w-full max-w-[1275px]">
@@ -14,14 +13,7 @@ const MainPart1 = () => (
         The arts in the collection of the Modern Art Gallery all started from a spark of inspiration. Will these pieces
         inspire you? Visit us and find out.
       </p>
-      <button className="group flex h-[72px] w-[260px] transition">
-        <div className="flex h-full w-[204px] items-center justify-center bg-almostBlack font-bigShouldersDisplay text-[20px] font-extrabold tracking-[3.64px] text-white group-hover:bg-gold">
-          OUR LOCATION
-        </div>
-        <div className="flex h-full w-[56px] items-center justify-center bg-gold group-hover:bg-almostBlack">
-          <Image width={8} height={24} src={arrowRight as string} alt="arrow right" />
-        </div>
-      </button>
+      <Button1and2 type={1} />
     </div>
   </div>
 );
