3e0e845d70f0722c4a8498fd854b55ecfe08e648
VillageR88 <villager88pl@gmail.com>
2024-03-18 19:40:55 +0100

    Refactor Facebook and Footer components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,22 +1,47 @@
 import Image from 'next/image';
 import logoLight from '@/public/assets/images/logo-light.svg';
+import logoDark from '@/public/assets/images/logo-dark.svg';
 import Facebook from './Facebook';
+import Instagram from './Instagram';
+import Twitter from './Twitter';
 
-const Footer = () => (
-  <footer className="mt-[180px] flex min-h-[244px] w-full items-center justify-center bg-almostBlack">
-    <div className="flex size-full max-w-[1110px] justify-between">
-      <div className="flex h-[84px] w-[730px] items-start justify-between">
-        <Image width={160} height={58} src={logoLight as string} alt="logo" />
-        <p className="BodyS h-[84px] w-[430px] text-white">
-          The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm. Find us at 99 King
-          Street, Newport, USA.
-        </p>
+const Footer = ({ type }: { type: 1 | 2 }) => {
+  const typeItems = {
+    1: {
+      backgroundColor: 'bg-almostBlack',
+      textColor: 'text-white',
+      fill: 'fill-[#FFF]',
+      hoverFill: 'hover:fill-gold',
+      logo: logoLight as string,
+    },
+    2: {
+      backgroundColor: 'bg-gold',
+      textColor: 'text-almostBlack',
+      fill: 'fill-[#000]',
+      hoverFill: 'hover:fill-white',
+      logo: logoDark as string,
+    },
+  };
+  return (
+    <footer
+      className={`mt-[180px] flex min-h-[244px] w-full items-center justify-center ${typeItems[type].backgroundColor}`}
+    >
+      <div className="flex size-full max-w-[1110px] justify-between">
+        <div className="flex h-[84px] w-[730px] items-start justify-between">
+          <Image width={160} height={58} src={typeItems[type].logo} alt="logo" />
+          <p className={`BodyS h-[84px] w-[430px] ${typeItems[type].textColor}`}>
+            The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm. Find us at 99
+            King Street, Newport, USA.
+          </p>
+        </div>
+        <div className="flex h-[20px] w-[100px] justify-between">
+          <Facebook hoverFill={typeItems[type].hoverFill} fill={typeItems[type].fill} />
+          <Instagram hoverFill={typeItems[type].hoverFill} fill={typeItems[type].fill} />
+          <Twitter hoverFill={typeItems[type].hoverFill} fill={typeItems[type].fill} />
+        </div>
       </div>
-      <div className="h-[20px] w-[100px]">
-        <Facebook />
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start">
       <Main />
-      <Footer />
+      <Footer type={1} />
     </div>
   );
 }
