836ecbd247c13e0e0d1213acc9226a80e204b95e
VillageR88 <villager88pl@gmail.com>
2024-03-18 11:30:34 +0100

    Add Tailwind CSS plugin and update eslint configuration
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
+        {/*eslint-disable-next-line @next/next/no-page-custom-font*/}
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
       </head>
       <body className={`bg-darkBlue ${[fontSans, fontMono].map((font) => font.variable).join(' ')}`}>
-        <div className="ml-auto mr-auto max-w-7xl overflow-x-clip font-sans">{children}</div>
+        <div className="mx-auto max-w-7xl overflow-x-clip font-sans">{children}</div>
       </body>
     </html>
   );
