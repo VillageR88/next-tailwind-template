3aca17ccb35fe0cb54801b5ab3d7d6584df07bfd
VillageR88 <villager88pl@gmail.com>
2024-03-18 21:22:50 +0100

    Add custom marker icon to LeafletMap component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/location/components/LeafletMap.tsx
+++ app/location/components/LeafletMap.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import customMarker from '@/public/assets/images/customMarker.svg';
 
 const LeafletMap = () => {
   useEffect(() => {
     // This code runs after the component has mounted, and ensures that Leaflet's CSS styles are applied correctly.
-    const L = require('leaflet');
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
@@ -16,16 +18,23 @@ const LeafletMap = () => {
     });
   }, []);
 
+  const customIcon = L.icon({
+    iconUrl: customMarker,
+    iconSize: [38, 95], // size of the icon
+    popupAnchor: [0, -20], // point from which the popup should open relative to the iconAnchor
+  });
+
   return (
-    <div className="size-full saturate-0 hover:saturate-100 duration-1000">
+    <div className="size-full saturate-0 duration-1000 ease-in-out hover:saturate-100">
       <MapContainer center={[41.4803, -71.3109]} zoom={16} style={{ height: '100%', width: '100%' }}>
         <TileLayer
           attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[41.4803, -71.3109]}>
+        <Marker position={[41.4803, -71.3109]} icon={customIcon}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            99 King Street
+            <br /> Newport RI 02840 United States of America
           </Popup>
         </Marker>
       </MapContainer>
