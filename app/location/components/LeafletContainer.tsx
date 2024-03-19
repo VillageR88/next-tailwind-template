bee71db0b86e32c6f39727871d998b0df4e1080b
VillageR88 <villager88pl@gmail.com>
2024-03-19 16:41:31 +0100

    Refactor LeafletContainer component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/location/components/LeafletContainer.tsx
+++ app/location/components/LeafletContainer.tsx
@@ -1,17 +1,11 @@
 import Button1and2 from '@/app/components/Button1and2';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
-import { useMemo } from 'react';
 
 const LeafletContainer = () => {
-  const LeafletMap = useMemo(
-    () =>
-      dynamic(() => import('@/app/location/components/LeafletMap'), {
-        //loading: () => <p>A map is loading</p>,
-        ssr: false,
-      }),
-    [],
-  );
+  const LeafletMap = dynamic(() => import('@/app/location/components/LeafletMap'), {
+    ssr: false,
+  });
   return (
     <div className="size-full">
       <div className="relative z-20 h-0 w-full">
