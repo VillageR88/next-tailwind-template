2e629245c14e5a355ea1009af3b8d7d4f5bca15f
VillageR88 <villager88pl@gmail.com>
2024-03-19 13:45:56 +0100

    Update MainPart2 styling and Footer position
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/home/MainPart2.tsx
+++ app/components/home/MainPart2.tsx
@@ -1,5 +1,5 @@
 const MainPart2 = () => (
-  <div className="mt-[120px] flex h-[1605px] w-full max-w-[1110px] flex-col gap-[30px] self-center px-4 md:h-[1131px] md:px-8 xl:px-0">
+  <div className="mt-[120px] flex  w-full max-w-[1110px] flex-col gap-[30px] self-center px-4 md:h-[1131px] md:px-8 xl:px-0">
     <div className="flex min-h-[400px] w-full flex-col-reverse items-center justify-between gap-6 md:flex-row xl:gap-0">
       <div className="flex min-h-[160px] max-w-[480px] flex-col justify-between gap-6 md:h-[299px] md:max-w-[223px] md:gap-0 lg:h-[280px] lg:max-w-[350px]">
         <h2 className="HeadingM whitespace-pre-wrap text-almostBlack md:whitespace-normal">
@@ -17,7 +17,7 @@ const MainPart2 = () => (
       <div className="flex size-full max-w-[640px] flex-col justify-between gap-[30px] md:max-w-[445px]">
         <div className="h-[313px] w-full bg-[url('../public/assets/images/mobile/image-grid-3@2x.jpg')] bg-cover bg-center md:bg-[url('../public/assets/images/tablet/image-grid-3@2x.jpg')] lg:bg-[url('../public/assets/images/desktop/image-grid-3@2x.jpg')]"></div>
         <div className="flex min-h-[377px] w-full gap-[32px] bg-almostBlack px-[47px] py-[64px] text-white">
-          <div className="flex min-h-[248px] w-[223px] flex-col justify-between lg:w-[350px]">
+          <div className="flex min-h-[248px] flex-col justify-center gap-[24px] md:max-w-[263px] lg:w-[350px]">
             <h2 className="HeadingM">COME & BE INSPIRED</h2>
             <p className="BodyM">
               We’re excited to welcome you to our gallery and see how our collections influence you.
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Main />
-      <Footer type={1} additionalClass="mt-[180px]" />
+      <Footer type={1} additionalClass="md:mt-[180px] mt-[120px]" />
     </div>
   );
 }
