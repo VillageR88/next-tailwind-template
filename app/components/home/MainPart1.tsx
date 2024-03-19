391e55f2b77a19c4f6c8208f5bd5d8c0d3b478da
VillageR88 <villager88pl@gmail.com>
2024-03-19 01:03:14 +0100

    Update MainPart1 and MainPart2 components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/home/MainPart1.tsx
+++ app/components/home/MainPart1.tsx
@@ -2,21 +2,26 @@ import Button1And2 from '@/app/components/Button1And2';
 import Link from 'next/link';
 
 const MainPart1 = () => (
-  <div className="flex size-full h-[800px] w-full max-w-[1275px]">
+  <div className="flex size-full h-[700px] w-full max-w-[1040px] justify-between self-center pr-8 min-[900px]:pl-8 min-[940px]:pr-16 min-[1000px]:pr-24 xl:h-[800px] xl:max-w-[1375px] xl:justify-start xl:pl-0 xl:pr-8">
     {/* eslint-disable-next-line tailwindcss/no-contradicting-classname*/}
-    <h1 className="HeadingXL absolute z-10 ml-[165px] mt-[189px] whitespace-pre bg-gradient-to-r from-white from-[63.6%] to-black to-[63.6%] bg-clip-text text-[2rem] text-transparent">
+    <h1 className="HeadingXL absolute z-10 ml-[165px] mt-[189px] hidden whitespace-pre bg-gradient-to-r from-white from-[63.6%] to-black to-[63.6%] bg-clip-text text-[2rem] text-transparent xl:block">
       {'MODERN\nART GALLERY'}
     </h1>
-    <div className="size-full max-w-[450px] overflow-hidden bg-almostBlack"></div>
-    <div className="h-full w-[540px] overflow-hidden bg-[url('../public/assets/images/desktop/image-hero@2x.jpg')] bg-cover"></div>
-    <div className="BodyM ml-[-65px] mt-[190px] flex h-[296px] w-[350px] flex-col justify-between text-darkGrey">
-      <p>
-        The arts in the collection of the Modern Art Gallery all started from a spark of inspiration. Will these pieces
-        inspire you? Visit us and find out.
-      </p>
-      <Link href="/location">
-        <Button1And2 type={1} />
-      </Link>
+    <div className="hidden size-full max-w-[450px] bg-almostBlack xl:block"></div>
+    <div className="h-full w-[437px] bg-[url('../public/assets/images/tablet/image-hero@2x.jpg')] bg-cover xl:w-[540px] xl:bg-[url('../public/assets/images/desktop/image-hero@2x.jpg')]"></div>
+    <div className="BodyM ml-[-65px] mt-[145px] flex h-[410px] w-[339px] flex-col justify-between gap-[48px] xl:mt-[190px] xl:h-[296px] xl:w-[350px]">
+      <div className="h-[130px] max-w-[330px] xl:hidden">
+        <h1 className="font-bigShouldersDisplay text-[70px] font-black leading-[65px] ">MODERN ART GALLERY</h1>
+      </div>
+      <div className="flex size-full flex-col justify-between gap-[48px] xl:gap-[64px]">
+        <p className="text-darkGrey">
+          The arts in the collection of the Modern Art Gallery all started from a spark of inspiration. Will these
+          pieces inspire you? Visit us and find out.
+        </p>
+        <Link href="/location">
+          <Button1And2 type={1} />
+        </Link>
+      </div>
     </div>
   </div>
 );
--- app/components/home/MainPart2.tsx
+++ app/components/home/MainPart2.tsx
@@ -1,5 +1,5 @@
 const MainPart2 = () => (
-  <div className="mt-[120px] flex h-[1131px] w-full max-w-[1110px] flex-col gap-[30px] self-center">
+  <div className="mt-[120px] flex h-[1131px] w-full max-w-[1110px] flex-col gap-[30px] self-center px-8 xl:px-0">
     <div className="flex min-h-[400px] w-full justify-between">
       <div className="flex h-[280px] w-[350px] flex-col justify-between">
         <h2 className="HeadingM text-almostBlack">{'Your Day at the Gallery'.toUpperCase()}</h2>
@@ -8,13 +8,13 @@ const MainPart2 = () => (
           their creative process.
         </p>
       </div>
-      <div className="h-[400px] w-[635px] bg-[url('../public/assets/images/desktop/image-grid-1@2x.jpg')] bg-cover"></div>
+      <div className="h-[400px] w-[635px] bg-[url('../public/assets/images/desktop/image-grid-1@2x.jpg')] bg-cover bg-center"></div>
     </div>
-    <div className="flex min-h-[720px] w-full justify-between">
-      <div className="h-full w-[635px] bg-[url('../public/assets/images/desktop/image-grid-2@2x.jpg')] bg-cover"></div>
+    <div className="flex min-h-[720px] w-full justify-between gap-6 xl:gap-0">
+      <div className="h-full w-[635px] bg-[url('../public/assets/images/desktop/image-grid-2@2x.jpg')] bg-cover bg-center"></div>
       <div className="flex h-full w-[445px] flex-col justify-between gap-[30px]">
-        <div className="h-[313px] w-full bg-[url('../public/assets/images/desktop/image-grid-3@2x.jpg')] bg-cover"></div>
-        <div className="flex h-[377px] w-full flex-col gap-[32px]  bg-almostBlack px-[47px] py-[64px] text-white">
+        <div className="h-[313px] w-full bg-[url('../public/assets/images/desktop/image-grid-3@2x.jpg')] bg-cover bg-center"></div>
+        <div className="flex h-[377px] w-full flex-col gap-[32px] bg-almostBlack px-[47px] py-[64px] text-white">
           <h2 className="HeadingM">COME & BE INSPIRED</h2>
           <p className="BodyM">
             We’re excited to welcome you to our gallery and see how our collections influence you.
