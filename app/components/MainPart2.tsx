a0528410bb7726cdc2cbc8e6ccea8fc47a36d981
VillageR88 <villager88pl@gmail.com>
2024-03-20 19:19:11 +0100

    Refactor component styles and layout
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainPart2.tsx
+++ app/components/MainPart2.tsx
@@ -2,10 +2,10 @@ import Button1n2 from './Button1n2';
 
 const MainPart2 = () => (
   <div className="flex size-full justify-center pl-8 lg:px-8 xl:pl-0">
-    <div className="mt-[83px] min-h-[480px] w-full lg:max-w-[1110px] ">
-      <div className="flex gap-[69px] lg:max-w-[1395px] lg:gap-[125px] ">
-        <div className="mt-[60px] flex min-h-[312px] w-full max-w-[445px] flex-col gap-[40px]">
-          <section className="flex w-fit flex-col gap-[24px]">
+    <div className="flex min-h-[480px] w-full justify-center sm2:mt-[83px] sm2:block lg:max-w-[1110px] ">
+      <div className="flex flex-col sm2:flex-row sm2:gap-[69px] lg:max-w-[1395px] lg:gap-[125px] ">
+        <div className="mt-[64px] flex min-h-[312px] w-full flex-col items-center gap-[40px] sm2:mt-[60px] sm2:items-start lg:max-w-[445px]">
+          <section className="flex w-full flex-col gap-[24px]">
             <h1>{'Typemaster Keyboard'.toUpperCase()}</h1>
             <p className="text-[18px]">
               Improve your productivity and gaming without breaking the bank. Upgrade to a high quality mechanical
@@ -17,9 +17,9 @@ const MainPart2 = () => (
             <span className="font-bold">{'Release on 5/27'.toUpperCase()}</span>
           </div>
         </div>
-        <div className="flex min-h-[480px] w-full justify-between lg:mr-[-285px] lg:max-w-[825px]">
-          <div className="size-full min-w-[478px] max-w-[540px] rounded-[20px] bg-[url('../public/assets/images/desktop/image-keyboard.jpg')] bg-cover"></div>
-          <div className="hidden size-full max-h-[240px] max-w-[255px] rounded-[20px] bg-[#E8EFF2] lg:block"></div>
+        <div className="flex min-h-[480px]  w-full justify-between lg:mr-[-285px] lg:max-w-[825px]">
+          <div className="size-full rounded-[20px] bg-[url('../public/assets/images/mobile/image-keyboard.jpg')] bg-cover sm2:min-w-[478px] sm2:max-w-[540px] sm2:bg-[url('../public/assets/images/desktop/image-keyboard.jpg')]"></div>
+          <div className="hidden size-full max-w-[255px] rounded-[20px] bg-[#E8EFF2] sm2:max-h-[240px] lg:block"></div>
         </div>
       </div>
     </div>
--- app/components/MainPart3.tsx
+++ app/components/MainPart3.tsx
@@ -1,14 +1,14 @@
 const MainPart3 = () => (
-  <div className=" mt-[30px] min-h-[480px] w-full px-8 min-[1150px]:max-w-[1110px] min-[1150px]:px-0">
+  <div className="mt-[30px] min-h-[480px] w-full px-8 min-[1150px]:max-w-[1110px] min-[1150px]:px-0">
     <div className="flex h-full min-h-[500px] flex-col justify-between gap-6 lg:flex-row min-[1150px]:ml-[-285px] min-[1150px]:max-w-[1395px]">
       <div className="flex h-[320px] min-h-[320px] w-full justify-between gap-[30px] min-[850px]:min-h-[400px] lg:h-[480px] lg:min-h-[480px] lg:max-w-[730px] min-[1150px]:max-w-[1015px]">
         <div className="hidden size-full max-h-[240px] rounded-[20px] bg-[#E8EFF2] lg:max-w-[255px] min-[1150px]:block"></div>
         <div className="size-full max-w-[214px]  overflow-hidden rounded-[20px] bg-[#F16718] md:max-w-[330px] lg:max-w-[255px]">
           <div className="size-full scale-125 rounded-[20px] bg-[url('../public/assets/images/desktop/image-phone-and-keyboard.jpg')] bg-cover bg-[0_-50px] opacity-80 mix-blend-multiply md:scale-100 lg:scale-100 lg:bg-center"></div>
         </div>
-        <div className="sm2:bg-[0_-30px] size-full rounded-[20px] bg-[url('../public/assets/images/desktop/image-glass-and-keyboard.jpg')] bg-cover md:min-w-[420px] md:bg-[0_-50px] lg:max-w-[480px] lg:bg-center"></div>
+        <div className="size-full rounded-[20px] bg-[url('../public/assets/images/desktop/image-glass-and-keyboard.jpg')] bg-cover sm2:bg-[0_-30px] md:min-w-[420px] md:bg-[0_-50px] lg:max-w-[480px] lg:bg-center"></div>
       </div>
-      <div className="sm2:flex-row sm2:gap-[70px] sm2:text-start mt-[75px] flex w-full flex-col items-center justify-between gap-[24px] text-center lg:mt-[112px] lg:min-h-[108px] lg:min-h-[312px] lg:max-w-[255px] lg:flex-col lg:justify-stretch  lg:gap-[50px]">
+      <div className="mt-[75px] flex w-full flex-col items-center justify-between gap-[24px] text-center sm2:flex-row sm2:gap-[70px] sm2:text-start lg:mt-[112px] lg:min-h-[108px]  lg:max-w-[255px] lg:flex-col lg:justify-stretch  lg:gap-[50px]">
         <h2 className="w-full max-w-[320px] lg:max-w-[255px]">{'mechanical wireless Keyboard'.toUpperCase()}</h2>
         <p className="max-w-[570px] lg:max-w-[255px]">
           The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and
