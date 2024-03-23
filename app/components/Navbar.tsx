ab8182601d9bea2ccdc3190a8dfbde9841d80e23
VillageR88 <villager88pl@gmail.com>
2024-03-23 11:33:54 +0100

    Update Navbar button shadow
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => (
     <div className="flex h-[80px] w-full max-w-[90em] items-center justify-between  px-8">
       <div></div>
       <div className="flex gap-4">
-        <button className="flex h-[40px] w-[110px] items-center justify-center rounded-[25px] bg-gradient-to-t from-[#9e7225] to-[#d19f47] text-[14px] font-extrabold tracking-[1px] text-[white] shadow-[0_1px_22px_-7px_rgb(209,159,71)] transition hover:opacity-80">
+        <button className="flex h-[40px] w-[110px] items-center justify-center rounded-[25px] bg-gradient-to-t from-[#9e7225] to-[#d19f47] text-[14px] font-extrabold tracking-[1px] text-[white] shadow-[0_1px_22px_-9px_rgb(209,159,71)] transition hover:opacity-80">
           <div className="flex h-[80%] w-[92%] items-center justify-center rounded-[25px] bg-gradient-to-b from-[#9e7225] to-[#d19f47]">
             Login
           </div>
