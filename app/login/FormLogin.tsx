4eb3188e7632d33d63f34b6556b07bcb7840c2a2
VillageR88 <villager88pl@gmail.com>
2024-03-24 20:46:44 +0100

    Update login form labels and placeholders
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/login/FormLogin.tsx
+++ app/login/FormLogin.tsx
@@ -3,23 +3,25 @@ const FormLogin = () => {
     <form className="flex size-full flex-col gap-6">
       <div className="flex flex-col gap-2">
         <div className="flex justify-between px-1">
-          <label className="w-fit" htmlFor="email">
+          <label className="flex w-fit items-center gap-2" htmlFor="email">
+            <span className="font-materialSymbolsOutlined">email</span>
             <span>Email</span>
           </label>
           <button className="text-[14px] text-[orange] transition hover:brightness-[117%]">Create new account</button>
         </div>
         <div>
-          <input className="w-full" id="email" type="email" />
+          <input placeholder="example@domain.com" className="w-full" id="email" type="email" />
         </div>
       </div>
       <div className="flex flex-col gap-2">
         <div className="flex justify-between px-1">
-          <label className="w-fit" htmlFor="password">
+          <label className="flex w-fit items-center gap-[6px]" htmlFor="password">
+            <span className="font-materialSymbolsOutlined">lock</span>
             <span>Password</span>
           </label>
           <button className="text-[14px] text-[orange] transition hover:brightness-[117%]">Remind my password</button>
         </div>
-        <input id="password" type="password" />
+        <input placeholder="Enter your password" id="password" type="password" />
       </div>
       <button
         className="mt-1.5 flex h-[45px] w-full items-center justify-center rounded-[6px] bg-gradient-to-b from-[#edb43a] to-[#9a6502] text-[16px] font-extrabold tracking-[1px] text-white  transition hover:brightness-[118%]"
