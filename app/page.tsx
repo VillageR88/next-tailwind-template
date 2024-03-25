994736e217863dd49883ee1d2fab916044c9ae4c
VillageR88 <villager88pl@gmail.com>
2024-03-25 16:28:41 +0100

    Refactor Navbar and add authentication check in page component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    if (!localStorage.getItem('token')) {
+      router.push('/login');
+    }
+    console.log(localStorage.getItem('token'));
+  }, [router]);
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-start font-instrumentSans">
-      <Navbar />
-      <Main />
-      <footer className="h-[200px] w-full bg-[#161616]"></footer>
-    </div>
+    localStorage.getItem('token') && (
+      <div className="flex min-h-screen w-full flex-col items-center justify-start font-instrumentSans">
+        <Navbar />
+        <Main />
+        <footer className="h-[200px] w-full bg-[#161616]"></footer>
+      </div>
+    )
   );
 }
