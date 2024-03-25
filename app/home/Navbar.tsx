7922e7a010af548b7796a8cc7c7b56fdd4800710
VillageR88 <villager88pl@gmail.com>
2024-03-25 20:41:24 +0100

    Refactor Navbar component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Navbar.tsx
+++ app/home/Navbar.tsx
@@ -7,21 +7,18 @@ const Navbar = () => {
 
   return (
     <nav className="flex w-full justify-center border-b border-[#313131] bg-[#1C1C1C] px-8">
-      <div className="flex h-[80px] w-full max-w-[90em] items-center justify-between  ">
+      <div className="flex size-full max-w-[90em] items-center justify-between">
         <Logo />
         <div className="flex gap-4">
           <button
-            className="mt-1.5 flex h-[45px] w-[120px] items-center justify-center gap-[2px] rounded-[6px] bg-gradient-to-b from-[orange] to-[#b97b08] text-[16px] font-extrabold tracking-[1px] text-white  transition hover:brightness-[118%]"
+            onClick={() => {
+              localStorage.removeItem('token');
+              router.push('/login');
+            }}
+            className="flex h-[40px] w-[110px] items-center justify-center gap-[2px] rounded-[6px] bg-gradient-to-b from-[orange] to-[#b97b08] text-[16px] font-extrabold tracking-[1px] text-white  transition hover:brightness-[118%]"
             type="submit"
           >
-            <span
-              onClick={() => {
-                localStorage.removeItem('token');
-                router.push('/login');
-              }}
-            >
-              Log out
-            </span>
+            <span>Log out</span>
             <span className="font-materialSymbolsOutlined">login</span>
           </button>
         </div>
