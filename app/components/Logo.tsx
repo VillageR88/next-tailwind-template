73395b68d56636a317a36b604747fbc65421a3eb
VillageR88 <villager88pl@gmail.com>
2024-03-25 20:05:10 +0100

    Update Logo and global styles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,9 +1,9 @@
 const Logo = () => (
   <span className="flex items-center gap-2">
-    <span className="font-materialSymbolsOutlined text-[50px] text-[orange]">database</span>
+    <span className="font-materialSymbolsOutlined text-[50px] text-[orange]">edit_note</span>
     <div>
-      <span className="text-4xl font-bold text-white">My</span>
-      <span className="text-4xl font-medium text-white">DB</span>
+      <span className="text-4xl font-bold text-[orange]">My</span>
+      <span className="text-4xl font-medium text-white">Notebook</span>
     </div>
   </span>
 );
