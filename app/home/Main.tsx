a186cdba7b70a3e3260209673b1e585086386048
VillageR88 <villager88pl@gmail.com>
2024-03-25 20:16:28 +0100

    Update max width of container div
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Main.tsx
+++ app/home/Main.tsx
@@ -2,7 +2,7 @@ import Table from './Table';
 
 const Main = () => (
   <main className="flex h-[1300px] w-full items-start justify-center bg-[#1C1C1C] p-8">
-    <div className="flex w-full max-w-[90em] justify-center">
+    <div className="flex w-full max-w-[210mm] justify-center">
       <div className="mt-[40px] size-full rounded-[6px] border border-[#313131] bg-[#232323] px-3 py-4">
         <Table />
       </div>
