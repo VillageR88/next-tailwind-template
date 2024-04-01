06b7153f17c2e6e8bbce36691cf873b8dbc645d7
VillageR88 <villager88pl@gmail.com>
2024-04-02 01:05:35 +0200

    Add save and drag-and-drop functionality to Navbar and Notebook components
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Navbar.tsx
+++ app/home/Navbar.tsx
@@ -10,17 +10,20 @@ import IconLogout from '../components/IconLogout';
 const Navbar = () => {
   const router = useRouter();
   const context = useContext(DataContext);
+  //create function checkSame and check if deep copy of JSON is the same as the original
+  const checkSame = () => {
+    return (
+      JSON.stringify(context.dataContext?.collections) ===
+      JSON.stringify(context.initialDataContext.current?.collections)
+    );
+  };
 
   return (
     <nav className="flex w-full justify-center border-b border-[#313131] bg-[#1C1C1C] px-8 py-4">
       <div className="flex size-full max-w-[90em] items-center justify-between">
         <Logo alternate />
         <div className="flex gap-4">
-          <button
-            disabled={context.dataContext === context.initialDataContext.current}
-            className="button1 flex"
-            type="button"
-          >
+          <button disabled={checkSame()} className="button1 flex" type="button">
             <span className="hidden md:block">Save</span>
             <IconSave />
           </button>
@@ -28,7 +31,7 @@ const Navbar = () => {
             onClick={() => {
               context.setDataContext(context.initialDataContext.current);
             }}
-            disabled={context.dataContext === context.initialDataContext.current}
+            disabled={checkSame()}
             className="button1 flex"
             type="button"
           >
--- app/home/Notebook.tsx
+++ app/home/Notebook.tsx
@@ -12,7 +12,43 @@ const Notebook = () => {
           ref={dragRef}
           draggable
           //create Drag and drop and include context.setDataContext
-
+          /*
+               onDragStart={(e) => {
+                      e.dataTransfer.setData('text/plain', index.toString());
+                      e.dataTransfer.effectAllowed = 'move';
+                    }}
+                    onDragOver={(e) => {
+                      e.preventDefault();
+                    }}
+                    onDrop={(e) => {
+                      e.preventDefault();
+                      const from = Number(e.dataTransfer.getData('text/plain'));
+                      const to = index;
+                      const newLinks = [...fetchLinks];
+                      const item = newLinks.splice(from, 1)[0];
+                      newLinks.splice(to, 0, item);
+                      setFetchLinks(newLinks);
+                    }}
+                    use this as inspiration
+         */
+          onDragStart={(e) => {
+            e.dataTransfer.setData('text/plain', collection.title);
+            e.dataTransfer.effectAllowed = 'move';
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDrop={(e) => {
+            e.preventDefault();
+            const from = e.dataTransfer.getData('text/plain');
+            const to = collection.title;
+            const newCollections = [...context.dataContext?.collections];
+            const fromIndex = newCollections.findIndex((c) => c.title === from);
+            const toIndex = newCollections.findIndex((c) => c.title === to);
+            const item = newCollections.splice(fromIndex, 1)[0];
+            newCollections.splice(toIndex, 0, item);
+            context.setDataContext({ ...context.dataContext, collections: newCollections });
+          }}
           className="flex flex-col gap-[6px] rounded-[6px] border border-[#313131] bg-[#232323] px-3 py-4"
           key={collection.id}
         >
