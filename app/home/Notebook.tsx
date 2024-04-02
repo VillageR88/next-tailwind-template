ffda28f8b83b8226fef3de56d63c07f2d9b2043b
VillageR88 <villager88pl@gmail.com>
2024-04-02 14:14:45 +0200

    Refactor Notebook component to handle reordering of collections and notes
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Notebook.tsx
+++ app/home/Notebook.tsx
@@ -2,23 +2,44 @@
 import { useContext } from 'react';
 import DataContext from '@/app/home/DataContext';
 import { Reorder } from 'framer-motion';
-import { Collection } from '../lib/interfaces';
+import { Collection, Note } from '../lib/interfaces';
 
 const Notebook = () => {
   const context = useContext(DataContext);
 
-  const handleReorder = (newOrder: Collection[]) => {
+  const handleReorderGroup = (newOrder: Collection[]) => {
     context.setDataContext((prevState) => ({
       ...prevState,
       collections: newOrder,
     }));
   };
 
+  const handleReorderItem = (newOrder: Note[], collectionId: number) => {
+    context.setDataContext((prevState) => {
+      // Find the index of the collection to update
+      const collectionIndex = prevState.collections.findIndex((collection) => collection.id === +collectionId);
+
+      // Make a copy of the collections array
+      const newCollections = [...prevState.collections];
+
+      // Update the Notes of the specific collection
+      newCollections[collectionIndex] = {
+        ...newCollections[collectionIndex],
+        Notes: newOrder,
+      };
+
+      // Return the updated collections array
+      return {
+        ...prevState,
+        collections: newCollections,
+      };
+    });
+  };
   return (
     <Reorder.Group
       className="flex w-full max-w-4xl flex-col gap-8 "
       values={context.dataContext.collections}
-      onReorder={handleReorder}
+      onReorder={handleReorderGroup}
     >
       {context.dataContext.collections.map((collection) => (
         <Reorder.Item
@@ -27,13 +48,20 @@ const Notebook = () => {
           className="flex flex-col gap-[6px] rounded-[6px] border border-[#313131] bg-[#232323] px-3 py-4"
         >
           <span className="pb-[8px] pl-1 text-left text-[18px] font-bold leading-6 text-white">{collection.title}</span>
-          <div className="flex flex-col gap-2">
+          <Reorder.Group
+            onReorder={(newOrder) => {
+              handleReorderItem(newOrder, collection.id);
+            }}
+            key={collection.id}
+            values={collection.Notes}
+            className="flex flex-col gap-2"
+          >
             {collection.Notes.map((note) => (
-              <div key={note.id} className="rounded-[6px] bg-[#1C1C1C] p-[10px] text-white">
+              <Reorder.Item value={note} key={note.id} className="rounded-[6px] bg-[#1C1C1C] p-[10px] text-white">
                 {note.description}
-              </div>
+              </Reorder.Item>
             ))}
-          </div>
+          </Reorder.Group>
         </Reorder.Item>
       ))}
     </Reorder.Group>
