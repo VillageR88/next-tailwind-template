2611cb40d4934cbb60e819a8e6798ccc935a5731
VillageR88 <villager88pl@gmail.com>
2024-04-11 11:19:00 +0200

    Update login page to include ButtonTheme component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,14 @@
 import FormLogin from './FormLogin';
 import Logo from '../components/Logo';
+import ButtonTheme from '../components/ButtonTheme';
 
 export default function Login() {
   return (
     <div className="flex min-h-[100dvh] w-full flex-col items-center justify-center px-8 py-10 font-instrumentSans md:min-h-screen">
       <div className={`flex w-full max-w-[500px] flex-col items-center justify-between gap-3 `}>
-        <header className="flex w-full justify-start">
+        <header className="flex w-full justify-between ">
           <Logo />
+          <ButtonTheme />
         </header>
         <main className="mt-[10px] flex min-h-full w-full flex-col gap-[48px] rounded-[6px] border border-[#313131] bg-[#232323] px-8 pb-14 pt-10">
           <div className="flex h-full items-start">
