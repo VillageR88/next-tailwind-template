6b7f65aed2f8379b85fa23982d382c4cfc4fb5ba
VillageR88 <villager88pl@gmail.com>
2024-04-17 10:21:01 +0200

    Refactor import paths and add ButtonLogin and ButtonCreateAccount components in FormLogin.tsx
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/login/ButtonCreateAccount.tsx
+++ app/login/ButtonCreateAccount.tsx
@@ -1,32 +1,30 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { Routes } from '../routes';
 
 const ButtonCreateAccount = () => {
-  const router = useRouter();
   return (
-    <button
-      onClick={() => {
-        router.push(Routes.createAccount);
-      }}
-      type="button"
-      className="group flex items-center gap-[6px] text-[14px] font-semibold tracking-[-0.17px] text-[darkorange] dark:font-normal dark:tracking-normal dark:text-[orange]"
-    >
-      <svg
-        className="fill-[darkorange] dark:fill-[orange]"
-        xmlns="http://www.w3.org/2000/svg"
-        height="18"
-        viewBox="0 -960 960 960"
-        width="18"
+    <Link href={Routes.createAccount}>
+      <button
+        type="button"
+        className="group flex items-center gap-[6px] text-[14px] font-semibold tracking-[-0.17px] text-[darkorange] dark:font-normal dark:tracking-normal dark:text-[orange]"
       >
-        <path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z" />
-      </svg>
-      <div className="flex flex-col">
-        <span className="hidden leading-[13px] md:block">Create new account</span>
-        <span className="block leading-[13px] md:hidden">new account</span>
-        <div className="h-[1px] w-full transition group-hover:bg-[darkorange] dark:group-hover:bg-[orange]"></div>
-      </div>
-    </button>
+        <svg
+          className="fill-[darkorange] dark:fill-[orange]"
+          xmlns="http://www.w3.org/2000/svg"
+          height="18"
+          viewBox="0 -960 960 960"
+          width="18"
+        >
+          <path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z" />
+        </svg>
+        <div className="flex flex-col">
+          <span className="hidden leading-[13px] md:block">Create new account</span>
+          <span className="block leading-[13px] md:hidden">new account</span>
+          <div className="h-[1px] w-full transition group-hover:bg-[darkorange] dark:group-hover:bg-[orange]"></div>
+        </div>
+      </button>
+    </Link>
   );
 };
 
