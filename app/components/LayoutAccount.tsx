d252e98bfdd6e95b22a5aba44733ceb520c69dd7
VillageR88 <villager88pl@gmail.com>
2024-04-20 13:52:51 +0200

    Refactor component props and update layout in LayoutAccount.tsx, create account page, and login page
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/LayoutAccount.tsx
+++ app/components/LayoutAccount.tsx
@@ -3,7 +3,7 @@ import ButtonTheme from '../components/ButtonTheme';
 
 import React from 'react';
 
-export default function LayoutAccount({ children }: { children: React.ReactNode }) {
+export default function LayoutAccount({ children, title }: { children: React.ReactNode; title: string }) {
   return (
     <div className="flex min-h-[100dvh] w-full flex-col items-center justify-center px-8 py-10 font-instrumentSans md:min-h-screen">
       <div className={`flex w-full max-w-[500px] flex-col items-center justify-between gap-3`}>
@@ -11,7 +11,12 @@ export default function LayoutAccount({ children }: { children: React.ReactNode 
           <Logo />
           <ButtonTheme />
         </header>
-        {children}
+        <main className="mt-[10px] flex min-h-full w-full flex-col gap-[48px] rounded-[6px] border border-[#f5f5f5] bg-[white] px-8 pb-14 pt-10 shadow-[0_10px_30px_-1px_rgba(0,0,0,0.03)] transition dark:border-[#313131] dark:bg-[#232323] dark:shadow-none">
+          <div className="flex h-full items-start">
+            <h1 className="leading-[32px]">{title}</h1>
+          </div>
+          <div className="size-full">{children}</div>
+        </main>
       </div>
     </div>
   );
--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -3,13 +3,8 @@ import LayoutAccount from '../components/LayoutAccount';
 
 export default function Login() {
   return (
-    <LayoutAccount>
-      <main className="mt-[10px] flex min-h-full w-full flex-col gap-[48px] rounded-[6px] border border-[#f5f5f5] bg-[white] px-8 pb-14 pt-10 shadow-[0_10px_30px_-1px_rgba(0,0,0,0.03)] transition dark:border-[#313131] dark:bg-[#232323] dark:shadow-none">
-        <div className="flex h-full items-start">
-          <h1 className="leading-[32px]">Create Account</h1>
-        </div>
-        <div className="size-full">{<FormCreateAccount />}</div>
-      </main>
+    <LayoutAccount title="Create Account">
+      <FormCreateAccount />
     </LayoutAccount>
   );
 }
