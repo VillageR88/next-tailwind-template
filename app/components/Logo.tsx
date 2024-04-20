baaa4a8e975277704819339701609a9f99e5c4cb
VillageR88 <villager88pl@gmail.com>
2024-04-20 15:45:02 +0200

    Refactor font sizes and headings in LayoutAccount.tsx, Logo.tsx, and globals.css
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -10,18 +10,20 @@ const Logo = ({ alternate }: { alternate?: boolean }) => (
       <path d="M160-400v-80h280v80H160Zm0-160v-80h440v80H160Zm0-160v-80h440v80H160Zm360 560v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T863-380L643-160H520Zm300-263-37-37 37 37ZM580-220h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19Z" />
     </svg>
     <div className="flex">
-      <span
-        // eslint-disable-next-line tailwindcss/classnames-order
-        className={`${alternate ? 'hidden md:block' : 'text-3xl'} font-bold text-[darkorange] dark:text-[orange] md:text-4xl`}
-      >
-        My
-      </span>
-      <span
-        // eslint-disable-next-line tailwindcss/classnames-order
-        className={`${alternate ? 'hidden md:block' : 'text-3xl'} font-medium transition dark:text-white md:text-4xl`}
-      >
-        Notebook
-      </span>
+      <h1>
+        <span
+          // eslint-disable-next-line tailwindcss/classnames-order
+          className={`${alternate && 'hidden md:block'} font-bold text-[darkorange] transition dark:text-[orange]`}
+        >
+          My
+        </span>
+        <span
+          // eslint-disable-next-line tailwindcss/classnames-order
+          className={`${alternate && 'hidden md:block'} font-medium text-black transition dark:text-white`}
+        >
+          Notebook
+        </span>
+      </h1>
     </div>
   </div>
 );
