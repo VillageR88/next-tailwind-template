c3023f6600b09fc8645507d877a807a88fa5a9e8
VillageR88 <villager88pl@gmail.com>
2024-04-20 13:15:02 +0200

    Refactor ButtonLogin component and import paths in FormLogin.tsx
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/components/ModuleLogin.tsx
@@ -0,0 +1,31 @@
+export default function ModuleLogin({ Button }: { Button(): JSX.Element }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between px-1">
+        <label className="flex w-fit items-center gap-2" htmlFor="email">
+          <svg
+            className="transition dark:fill-white"
+            xmlns="http://www.w3.org/2000/svg"
+            height="18"
+            viewBox="0 -960 960 960"
+            width="18"
+          >
+            <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
+          </svg>
+          <span>Email</span>
+        </label>
+        <Button />
+      </div>
+      <input
+        name="email"
+        required
+        autoComplete="email"
+        placeholder="example@domain.com"
+        className="regularInput"
+        id="email"
+        type="email"
+        pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+      />
+    </div>
+  );
+}
--- app/create-account/FormCreateAccount.tsx
+++ app/create-account/FormCreateAccount.tsx
@@ -1,8 +1,11 @@
 'use client';
 import { createInvoiceCreateEmail } from '../lib/functionsServer';
 import SubmitButton from '../components/SubmitButton';
-import ButtonLogin from './ButtonLogin';
 import { useFormState } from 'react-dom';
+import ModuleLogin from '../components/ModuleLogin';
+import ModulePassword from '../components/ModulePassword';
+import ModulePasswordConfirm from '../components/ModulePasswordConfirm';
+import ButtonLogin from '../components/ButtonLogin';
 
 export default function FormCreateAccount() {
   interface ErrorMessage {
@@ -11,84 +14,9 @@ export default function FormCreateAccount() {
   const [state, action] = useFormState<ErrorMessage, FormData>(createInvoiceCreateEmail, { error: '' });
   return (
     <form action={action} id="form-login" className="flex size-full flex-col gap-6">
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between px-1">
-          <label className="flex w-fit items-center gap-2" htmlFor="email">
-            <svg
-              className="transition dark:fill-white"
-              xmlns="http://www.w3.org/2000/svg"
-              height="18"
-              viewBox="0 -960 960 960"
-              width="18"
-            >
-              <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
-            </svg>
-            <span>Email</span>
-          </label>
-          <ButtonLogin />
-        </div>
-        <input
-          name="email"
-          required
-          autoComplete="email"
-          placeholder="example@domain.com"
-          className="regularInput"
-          id="email"
-          type="email"
-          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between px-1">
-          <label className="flex w-fit items-center gap-[6px]" htmlFor="password">
-            <svg
-              className="transition dark:fill-white"
-              xmlns="http://www.w3.org/2000/svg"
-              height="18"
-              viewBox="0 -960 960 960"
-              width="18"
-            >
-              <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z" />
-            </svg>
-            <span>Password</span>
-          </label>
-        </div>
-        <input
-          className="regularInput"
-          name="password"
-          required
-          minLength={8}
-          autoComplete="current-password"
-          placeholder="Enter your password"
-          id="password"
-          type="password"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between px-1">
-          <label className="flex w-fit items-center gap-[6px]" htmlFor="passwordConfirm">
-            <svg
-              className="transition dark:fill-white"
-              xmlns="http://www.w3.org/2000/svg"
-              height="18"
-              viewBox="0 -960 960 960"
-              width="18"
-            >
-              <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z" />
-            </svg>
-            <span>Confirm</span>
-          </label>
-        </div>
-        <input
-          className="regularInput"
-          name="passwordConfirm"
-          required
-          minLength={8}
-          placeholder="Confirm your password"
-          id="passwordConfirm"
-          type="password"
-        />
-      </div>
+      <ModuleLogin Button={ButtonLogin} />
+      <ModulePassword />
+      <ModulePasswordConfirm />
       {<SubmitButton state={state.error} type="createAccount" />}
     </form>
   );
