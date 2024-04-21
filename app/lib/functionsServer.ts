dbe829a9c0be47a45db32928b27c0355e50b0601
VillageR88 <villager88pl@gmail.com>
2024-04-21 23:39:52 +0200

    Refactor createInvoiceCreateEmail function and handle server errors in functionsServer.ts
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/lib/functionsServer.ts
+++ app/lib/functionsServer.ts
@@ -61,74 +61,41 @@ export const handleSaveCollectionGroup = async ({ data, token }: { data: Collect
   }
 };
 
-export const handleCreateAccount = async ({
-  email,
-  password,
-  passwordConfirm,
-}: {
-  email: string;
-  password: string;
-  passwordConfirm: string;
-}) => {
+interface ErrorMessage {
+  error: string;
+}
+
+export async function createInvoiceCreateEmail(prev: ErrorMessage, formData: FormData): Promise<ErrorMessage> {
+  if (formData.get('password') !== formData.get('passwordConfirm'))
+    return {
+      error: 'Passwords do not match',
+    };
+  let response;
   try {
-    const response = await fetch(`${server}create-account`, {
+    response = await fetch(`${server}create-account`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email, password, passwordConfirm }),
+      body: JSON.stringify({
+        email: formData.get('email'),
+        password: formData.get('password'),
+        passwordConfirm: formData.get('passwordConfirm'),
+      }),
     });
 
     if (response.ok) {
       const { token } = (await response.json()) as { token: string };
-      return token;
-    } else {
-      const error = await response.text();
-      console.error(error);
-      if (error === 'User already exists') return 'exists';
-      else return 'unsuccessful';
+      cookies().set({ name: 'token', value: token, httpOnly: true });
+      return { error: '' };
     }
   } catch (error) {
-    console.error(error);
+    return { error: 'Server error' };
+  } finally {
+    if (response?.status === 200) redirect(Routes.home);
+    else if (response?.status === 400) return { error: 'User already exists' };
+    else return { error: 'Server error' };
   }
-};
-interface ErrorMessage {
-  error: string;
-}
-export async function createInvoiceCreateEmail(prev: ErrorMessage, formData: FormData): Promise<ErrorMessage> {
-  const rawFormData = {
-    email: formData.get('email'),
-    password: formData.get('password'),
-    passwordConfirm: formData.get('passwordConfirm'),
-  };
-
-  if (rawFormData.password !== rawFormData.passwordConfirm)
-    return {
-      error: 'Passwords do not match',
-    };
-
-  const response = await handleCreateAccount({
-    email: rawFormData.email as string,
-    password: rawFormData.password as string,
-    passwordConfirm: rawFormData.passwordConfirm as string,
-  })
-    .then((e) => {
-      if (!e) return 'Server error';
-      if (e === 'unsuccessful') {
-        return { error: 'Failed to create account' };
-      } else if (e === 'exists') {
-        return { error: 'User already exists' };
-      } else {
-        cookies().set({ name: 'token', value: e, httpOnly: true });
-        return 'pass';
-      }
-    })
-    .catch((e) => {
-      console.error(e);
-      return 'Server error';
-    });
-  if (response !== 'pass') return response as ErrorMessage;
-  redirect(Routes.home);
 }
 
 export async function createInvoiceLogin(prev: ErrorMessage, formData: FormData): Promise<ErrorMessage> {
